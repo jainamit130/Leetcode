6fc91deca61357c6b0e5eb25d625820639faeddd
Amit Choraria <jainamit130@gmail.com>
2024-03-09 00:37:40 +0530

    Time: 139 ms (48.36%), Space: 134 MB (23.57%) - LeetHub

--- 2918-minimum-equal-sum-of-two-arrays-after-replacing-zeros/2918-minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
+++ 2918-minimum-equal-sum-of-two-arrays-after-replacing-zeros/2918-minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
     long long minSum(vector<int>& nums1, vector<int>& nums2) {
-        int c1=0;
-        int c2=0;
-        long int t1=0;
-        long int t2=0;
-        for(int i=0;i<nums1.size();i++){
-            if(nums1[i]==0)
-           {      t1+=1;c1++;}
-            
-            t1+=nums1[i];
+        int c1 = 0;
+        int c2 = 0;
+        long int t1 = 0;
+        long int t2 = 0;
+        for (int i = 0; i < nums1.size(); i++) {
+            if (nums1[i] == 0) {
+                t1 += 1;
+                c1++;
+            }
+
+            t1 += nums1[i];
         }
-        for(int i=0;i<nums2.size();i++){
-            if(nums2[i]==0)
-             {      t2+=1;c2++;}
-            t2+=nums2[i];
+        for (int i = 0; i < nums2.size(); i++) {
+            if (nums2[i] == 0) {
+                t2 += 1;
+                c2++;
+            }
+            t2 += nums2[i];
         }
-        if(t1<t2){
-            if(c1==0)
+        if (t1 < t2) {
+            if (c1 == 0)
                 return -1;
         }
-        if(t1>t2){
-            if(c2==0)
+        if (t1 > t2) {
+            if (c2 == 0)
                 return -1;
         }
-        return max(t1,t2);
+        return max(t1, t2);
     }
 };
