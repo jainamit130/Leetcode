f471c1a91fc87dfb49453a105d2c55bcb4a16bd2
Amit Choraria <jainamit130@gmail.com>
2024-03-08 17:40:04 +0530

    Time: 256 ms (38.31%), Space: 81.6 MB (25.79%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.cpp
+++ 1462-course-schedule-iv/1462-course-schedule-iv.cpp
@@ -1,6 +1,6 @@
 //Topological Sort
 
-//DFS
+//BFS
 class Solution {
 public:
     vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
@@ -11,31 +11,76 @@ public:
             adjacencyList[prerequisites[i][0]].push_back(prerequisites[i][1]);
         
         //Topological Sort
-        unordered_map<int,unordered_set<int>> topoSort;
+        unordered_map<int,unordered_set<int>> prereqMap;
         for(int i=0;i<numCourses;i++)
-            dfs(adjacencyList,i,topoSort);
+            bfs(adjacencyList,i,prereqMap);
 
         for(int i=0;i<queries.size();i++){
             int start=queries[i][0];
             int dst=queries[i][1];
-            result[i]=topoSort[start].find(dst)!=topoSort[start].end();
+            result[i]=prereqMap[start].find(dst)!=prereqMap[start].end();
         }
         return result;
     }
 
-    unordered_set<int> dfs(vector<vector<int>>& adj,int course,unordered_map<int,unordered_set<int>>& prereqMap){
-        if(prereqMap.find(course)!=prereqMap.end())
-            return prereqMap[course];
-
-        unordered_set<int> topoSet;
-        for(int i=0;i<adj[course].size();i++){
-            unordered_set<int> temp=dfs(adj,adj[course][i],prereqMap);
-            prereqMap[course].insert(temp.begin(),temp.end());
+    void bfs(vector<vector<int>>& adj,int course,unordered_map<int,unordered_set<int>>& prereqMap){
+        queue<int> q;
+        q.push(course);
+        while(!q.empty()){
+            int size=q.size();
+            while(size){
+                int top=q.front();
+                q.pop();
+                size--;
+                if(prereqMap[course].find(top)!=prereqMap[course].end())
+                    continue;
+                prereqMap[course].insert(top);
+                for(int i=0;i<adj[top].size();i++){
+                    if(prereqMap[course].find(adj[top][i])==prereqMap[course].end())
+                        q.push(adj[top][i]);
+                }
+            }
         }
-        prereqMap[course].insert(course);
-        return prereqMap[course];
+        return;
     }
 };
+
+//DFS
+// class Solution {
+// public:
+//     vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
+//         vector<vector<int>> adjacencyList(numCourses);
+//         vector<bool> result(queries.size());
+//         //Constructing Adjacency List
+//         for(int i=0;i<prerequisites.size();i++)
+//             adjacencyList[prerequisites[i][0]].push_back(prerequisites[i][1]);
+        
+//         //Topological Sort
+//         unordered_map<int,unordered_set<int>> prereqMap;
+//         for(int i=0;i<numCourses;i++)
+//             dfs(adjacencyList,i,prereqMap);
+
+//         for(int i=0;i<queries.size();i++){
+//             int start=queries[i][0];
+//             int dst=queries[i][1];
+//             result[i]=prereqMap[start].find(dst)!=prereqMap[start].end();
+//         }
+//         return result;
+//     }
+
+//     unordered_set<int> dfs(vector<vector<int>>& adj,int course,unordered_map<int,unordered_set<int>>& prereqMap){
+//         if(prereqMap.find(course)!=prereqMap.end())
+//             return prereqMap[course];
+
+//         unordered_set<int> topoSet;
+//         for(int i=0;i<adj[course].size();i++){
+//             unordered_set<int> temp=dfs(adj,adj[course][i],prereqMap);
+//             prereqMap[course].insert(temp.begin(),temp.end());
+//         }
+//         prereqMap[course].insert(course);
+//         return prereqMap[course];
+//     }
+// };
 
 //Brute Force
 
