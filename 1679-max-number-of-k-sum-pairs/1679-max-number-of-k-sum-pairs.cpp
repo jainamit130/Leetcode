eac9055badb56f2bb7292ec7a26a25e0504aa803
Amit Choraria <jainamit130@gmail.com>
2024-03-09 18:17:05 +0530

    Time: 141 ms (14.48%), Space: 65.3 MB (35.45%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -3,24 +3,20 @@ public:
     int maxOperations(vector<int>& nums, int k) {
         sort(nums.begin(),nums.end());
         unordered_map<int,int> memory;
-        for(auto n:nums)
-            memory[n]++;
+        for(auto n:nums){
+            if(n<=k)
+                memory[n]++;
+        }
         int count=0;
         for(auto n:nums){
-            if(n>=k)
-                break;
-            if((memory.find(n)!=memory.end() && memory.find(k-n)!=memory.end())){
+            if((n!=k-n && memory.find(n)!=memory.end() && memory.find(k-n)!=memory.end()) || (n==k-n && memory.find(n)!=memory.end() && memory[n]>=2)){
                 memory[n]--;
                 memory[k-n]--;
-                if(n==k-n && memory[k-n]<=0){
-                    memory.erase(n);
-                    continue;
-                }
                 count++;
-                if(memory[n]<=0){
+                if(memory[n]<=0)
                     memory.erase(n);
+                if(memory[k-n]<=0)
                     memory.erase(k-n);
-                }
             }
             if(memory.size()==0)
                 break;
