87c74edfd447f705ff6e1079efe39b811dacbcbf
Amit Choraria <jainamit130@gmail.com>
2024-03-09 14:39:49 +0530

    Time: 51 ms (5.13%), Space: 136.1 MB (5.02%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        string pattern="";
+        string ans="";
+        int i=0;
+        while(i<str1.length() && i<str2.length()){
+            if(str1[i]==str2[i]){
+                pattern+=str1[i];
+            } else {
+                return "";
+            }
+            string a="";
+            string b="";
+            while(a.length()<str1.length()){
+                a+=pattern;
+            }
+            while(b.length()<str2.length()){
+                b+=pattern;
+            }
+            if(a==str1 && b==str2)
+                ans=ans.length()<pattern.length()?pattern:ans;
+            i++;
+        }
+        return ans;
+    }
+};
