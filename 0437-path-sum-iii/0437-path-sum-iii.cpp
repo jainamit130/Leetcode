ce54b7100f3a8833b1cad8db31e5d83d364a6e8e
Amit Choraria <jainamit130@gmail.com>
2024-03-16 17:41:47 +0530

    Time: 14 ms (75.52%), Space: 21.6 MB (17.89%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.cpp
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -1,14 +1,36 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+//Brute Force
+// class Solution {
+// public:
+//     int count=0;
+//     int pathSum(TreeNode* root, int targetSum) {
+//         dfsSolve(root,targetSum);
+//         return count/2;
+//     }
+
+//     void dfsSolve(TreeNode* root,int targetSum){
+//         if(!root)
+//             return;
+
+//         solve(root->left,targetSum-root->val);
+//         solve(root->right,targetSum-root->val);
+//         dfsSolve(root->left,targetSum);
+//         dfsSolve(root->right,targetSum);
+//         return;
+//     }
+
+//     void solve(TreeNode* root,long long targetSum){
+//         if(targetSum==0)
+//             count++;
+
+//         if(!root)
+//             return;
+
+//         solve(root->left,targetSum-root->val);
+//         solve(root->right,targetSum-root->val);
+//         return;
+//     }
+// };
+
 class Solution {
 public:
     int solve(TreeNode* root, int k, long long sum, unordered_map<long long, int>& mpp){
