eb5a1f460c679b73c1b477db72ab0dc914a3ef45
Amit Choraria <jainamit130@gmail.com>
2024-03-22 23:50:09 +0530

    Time: 34 ms (5.23%), Space: 25.3 MB (7.52%) - LeetHub

---
+++ 1377-frog-position-after-t-seconds/1377-frog-position-after-t-seconds.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    double ans=0.0;
+    double frogPosition(int n, vector<vector<int>>& edges, int t, int target) {
+        vector<unordered_set<int>> adj(n+1);
+        for(auto e:edges){
+            adj[e[0]].insert(e[1]);
+            adj[e[1]].insert(e[0]);
+        }
+        unordered_set<int> visited;
+        dfs(adj,1,t,visited,target,1.0);
+        return ans;
+    }
+
+    void dfs(vector<unordered_set<int>>& adj,int currentVertex,int time,unordered_set<int>& visited,int target,double prob) {
+        if(visited.find(currentVertex)!=visited.end()){
+            return;
+        }
+
+        if(time<0){
+            return;
+        }
+
+        if(currentVertex==target && (adj[currentVertex].size()==0 || time==0)){ 
+            ans=prob;
+            return;
+        }
+        visited.insert(currentVertex);
+        for(auto a:adj[currentVertex]){
+            adj[a].erase(currentVertex);
+            dfs(adj,a,time-1,visited,target,prob*(1.0/adj[currentVertex].size()));
+            adj[a].insert(currentVertex);
+        }
+        return;
+    }
+};
