206e2195b3ff7486d62e728879a0816d60b690fe
Amit Choraria <jainamit130@gmail.com>
2024-03-28 14:29:58 +0530

    Time: 264 ms (11.64%), Space: 82.5 MB (80.73%) - LeetHub

--- 2905-find-indices-with-index-and-value-difference-ii/2905-find-indices-with-index-and-value-difference-ii.cpp
+++ 2905-find-indices-with-index-and-value-difference-ii/2905-find-indices-with-index-and-value-difference-ii.cpp
@@ -1,6 +1,42 @@
 class Solution {
 public:
     vector<int> findIndices(vector<int>& nums, int indexDifference, int valueDifference) {
+        int i=indexDifference,j1=-1,j2=-1,maxValue=INT_MIN,minValue=INT_MAX;
+        int index=0;
+        while(i<nums.size()){
+            int addedDiff=nums[i]+valueDifference;
+            int subDiff = nums[i]-valueDifference;
+            if(maxValue<nums[index]){
+                maxValue=nums[index];
+                j1=index;
+            }
+            if(minValue>nums[index]){
+                minValue=nums[index];
+                j2=index;
+            }
+            if(maxValue>=addedDiff)
+                return {j1,i};
+            if(minValue<=subDiff)
+                return {j2,i};
+            cout<<maxValue<<" "<<minValue<<endl;
+            index++;
+            i++;
+        }
+        return {-1,-1};
+    }
+};
+
+
+
+/*
+A solution for question like to find two indices i and j, both in the range [0, n - 1], that satisfy the following conditions:
+
+abs(i - j) = indexDifference, and
+abs(nums[i] - nums[j]) = valueDifference
+
+class Solution {
+public:
+    vector<int> findIndices(vector<int>& nums, int indexDifference, int valueDifference) {
         unordered_map<int,unordered_set<int>> mp;
         for(int i=0;i<nums.size();i++){
             mp[nums[i]].insert(i);
@@ -22,27 +58,4 @@ public:
         return {-1,-1};
     }
 };
-
-
-
-
-/*
-Map
-5 -> 0
-1 -> 1  3
-4 -> 2
-
-indexDifference = 2    valueDifference = 4
-0   1   2   3
-5   1   4   1
-i       j
-
-possibleValues  -> >9    1>
-possibleIndexes -> >=2   
-
-
-5   1   4   1
-i
-        j
-
 */
