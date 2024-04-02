3dc62dcdc9435357acae0ad7885836f2b55f9f2c
Amit Choraria <jainamit130@gmail.com>
2024-04-02 17:18:18 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
@@ -1,20 +1,37 @@
 class Solution {
 public:
     int maxSubarraySumCircular(vector<int>& nums) {
+        if(nums.size()==1)
+            return nums[0];
+        int runningSum=nums[0];
         int globalMax=nums[0];
         int globalMin=nums[0];
-        int currentMax=0;
-        int currentMin=0;
-        int total=0;
-        for(auto n:nums){
-            total+=n;
-            currentMax=max(n,currentMax+n);
-            globalMax=max(globalMax,currentMax);
-            currentMin=min(n,currentMin+n);
-            globalMin=min(globalMin,currentMin);
+        int globalMaxSum=nums[0];
+        int globalMinSum=nums[0];
+        for(int i=1;i<nums.size();i++){
+            globalMax+=nums[i];
+            globalMin+=nums[i];
+            runningSum+=nums[i];
+            globalMax=max(globalMax,nums[i]);
+            globalMin=min(0,nums[i]);
+            globalMaxSum=max(globalMaxSum,globalMax);
+            globalMinSum=min(globalMinSum,globalMin);
         }
-        if(globalMax<0)
-            return globalMax;
-        return max(total-globalMin,globalMax);
+        return max(globalMaxSum,runningSum-globalMinSum);
     }
 };
+
+
+
+
+/*
+
+
+-3  -2  -3
+
+amns=max(-2,);
+
+
+*/
+
+
