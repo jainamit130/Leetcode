d1e988ea4cf50f83e694c36ba7e8ccc0a97fd5dc
Amit Choraria <jainamit130@gmail.com>
2024-04-04 20:10:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0773-sliding-puzzle/0773-sliding-puzzle.cpp
@@ -0,0 +1,82 @@
+class Solution {
+public:
+    int ans=INT_MAX;
+    vector<vector<int>> directions={{-1,0},
+                              {0,-1},     {0,1},
+                                    {1,0}};
+    vector<vector<int>> goal={{1,2,3},{4,5,0}};
+    int slidingPuzzle(vector<vector<int>>& board) {
+        vector<vector<int>> visited(2,vector<int>(3,0));
+        //Get Position of Zero
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board[0].size();j++){
+                if(board[i][j]==0){
+                    dfs(i,j,board,0,visited);
+                }
+            }
+        }
+        if(ans==INT_MAX)
+            return -1;
+        return ans;
+    }
+
+    bool isValid(int r,int c){
+        if(r<0 || r>=2 || c>=3 || c<0)
+            return false;
+        return true;
+    }
+
+    void dfs(int r,int c,vector<vector<int>>& board,int moves,vector<vector<int>>& visited){
+        if(!isValid(r,c))
+            return;
+
+        if(visited[r][c]==1)
+            return;
+
+        if(board==goal){
+            ans=min(ans,moves);
+            return;
+        }
+
+        for(auto d:directions){
+            int nr=r+d[0];
+            int nc=c+d[1];
+            //swap
+            if(isValid(nr,nc)){
+                board[r][c]=board[nr][nc];
+                board[nr][nc]=0;
+                visited[r][c]=1;
+            }
+            dfs(nr,nc,board,moves+1,visited);
+            if(isValid(nr,nc)){
+                board[nr][nc]=board[r][c];
+                board[r][c]=0;
+                visited[r][c]=0;
+            }
+        }
+        return;
+    }
+};
+
+
+/*
+
+1   2   3
+5   4
+...
+1   2   3
+5       4
+
+1   3   4
+5       2
+
+1   4   2
+5   3
+
+4   2   3
+    5   1
+
+2   3   1
+4   5    
+
+*/
