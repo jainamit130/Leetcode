b82d2a6349289d9b55b5279a3333b6e8a3e8c6af
Amit Choraria <jainamit130@gmail.com>
2024-04-09 14:27:17 +0530

    Time: 5 ms (22.66%), Space: 9.5 MB (41.74%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -1,26 +1,48 @@
+// # Approach 1
+// class Solution {
+// public:
+//     int timeRequiredToBuy(vector<int>& tickets, int k) {
+//         queue<pair<int,int>> q;
+//         for(int i=0;i<tickets.size();i++)
+//             q.push({tickets[i],i});
+//         int time=0;
+//         while(1){
+//             int t=q.front().first;
+//             int ti=q.front().second;
+//             if(t>1){
+//                 q.pop();
+//                 time++;
+//                 q.push({t-1,ti});
+//             } else if(t==1 && ti==k)
+//                 return ++time;
+//             else {
+//                 q.pop();
+//                 time++;
+//             }
+//         }
+//         return time;
+//     }
+// };
+
+
+// #Approach 2
 class Solution {
-public:
-    int timeRequiredToBuy(vector<int>& tickets, int k) {
-        queue<pair<int,int>> q;
-        for(int i=0;i<tickets.size();i++)
-            q.push({tickets[i],i});
-        int time=0;
-        while(1){
-            int t=q.front().first;
-            int ti=q.front().second;
-            if(t>1){
-                q.pop();
-                time++;
-                q.push({t-1,ti});
-            } else if(t==1 && ti==k)
-                return ++time;
-            else {
-                q.pop();
-                time++;
+    public:
+        int timeRequiredToBuy(vector<int>& tickets, int k) {
+            int ans=0;
+            int i=0;
+            while(1){
+                if(tickets.size()==i)
+                    i=0;
+                if(tickets[i]>0)
+                    ans++;
+                tickets[i]=tickets[i]-1;
+                if(tickets[k]==0)
+                    break;
+                i++;
             }
+            return ans;
         }
-        return time;
-    }
 };
 
 
