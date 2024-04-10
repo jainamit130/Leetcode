74dd68ae415db6cc9166ed734e7a631bfae06fb3
Amit Choraria <jainamit130@gmail.com>
2024-04-10 11:42:37 +0530

    Time: 7 ms (18.53%), Space: 10.9 MB (34.66%) - LeetHub

--- 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.cpp
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.cpp
@@ -1,32 +1,54 @@
+// #Approach 1
+// class Solution {
+// public:
+//     vector<int> deckRevealedIncreasing(vector<int>& deck) {
+//         deque<int> q;
+//         sort(deck.begin(),deck.end(),[](auto lhs,auto rhs){
+//             return lhs>rhs;
+//         });
+//         vector<int> ans;
+//         for(auto d:deck){
+//             lastToFirst(q);
+//             q.push_front(d);
+//         }   
+//         while(!q.empty()){
+//             ans.push_back(q.front());
+//             q.pop_front();
+//         }
+//         return ans;
+//     }
+
+//     void lastToFirst(deque<int>& q){
+//         if(q.empty() || q.size()==1)
+//             return;
+//         int temp=q.back();
+//         q.pop_back();
+//         q.push_front(temp);
+//         return;
+//     }
+// };
+
+
+// #Approach 2
 class Solution {
 public:
     vector<int> deckRevealedIncreasing(vector<int>& deck) {
-        deque<int> q;
-        sort(deck.begin(),deck.end(),[](auto lhs,auto rhs){
-            return lhs>rhs;
-        });
-        vector<int> ans;
+        queue<int> q;
+        for(int i=0;i<deck.size();i++)
+            q.push(i);
+        sort(deck.begin(),deck.end());
+        vector<int> ans(deck.size());
         for(auto d:deck){
-            lastToFirst(q);
-            q.push_front(d);
-        }   
-        while(!q.empty()){
-            ans.push_back(q.front());
-            q.pop_front();
+            int index=q.front();
+            q.pop();
+            ans[index]=d;
+            int skip=q.front();
+            q.pop();
+            q.push(skip);
         }
         return ans;
     }
-
-    void lastToFirst(deque<int>& q){
-        if(q.empty() || q.size()==1)
-            return;
-        int temp=q.back();
-        q.pop_back();
-        q.push_front(temp);
-        return;
-    }
 };
-
 
 
 
