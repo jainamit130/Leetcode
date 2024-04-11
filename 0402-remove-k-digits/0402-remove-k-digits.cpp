6a8998e9519e6af65a2fc1cb8c8886ba5a0f4733
Amit Choraria <jainamit130@gmail.com>
2024-04-11 15:42:30 +0530

    Time: 19 ms (5.4%), Space: 12.7 MB (5.32%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -2,36 +2,33 @@ class Solution {
 public:
     string removeKdigits(string num, int k) {
         stack<int> st;
-        if(k==num.length())
-            return "0";
-        int lock=0;
         for(auto c:num){
             int n=c-'0';
-            if(st.size()==k && lock==0)
-                lock=1;
-            if(lock==0){
-                while(!st.empty() && st.top()>n){
-                    st.pop();
-                }
-            } else {
-                while(st.size()==k && st.top()>n){
-                    st.pop();
-                }
+            while(k>0 && !st.empty() && st.top()>n){
+                k--;
+                st.pop();
             }
             st.push(n);
+
         }
         string ans="";
         while(!st.empty()){
-            ans+=to_string(st.top());
-            st.pop();
+            if(k>0){
+                st.pop();
+                k--;
+                continue;
+            } else {
+                ans+=to_string(st.top());
+                st.pop();
+            }
         }
         reverse(ans.begin(),ans.end());
         string finalAns="";
         for(auto c:ans){
-            if(finalAns.length()==0 && c=='0'){
+            if(finalAns.length()==0 && c=='0')
                 continue;
-            }
-            finalAns+=c;
+            else 
+                finalAns+=c;
         }
         if(finalAns.length()==0)
             return "0";
