5f412cc5ed8617a0b6f88f83d453d7d21aab4759
Amit Choraria <jainamit130@gmail.com>
2024-04-15 19:33:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.java
+++ 0264-ugly-number-ii/0264-ugly-number-ii.java
@@ -23,17 +23,35 @@
 // }
 
 // #Approach 2
-class Solution {
-    public int nthUglyNumber(int n) {
-        ArrayList<Integer> dp=new ArrayList<Integer>(n);
-        dp.add(1);
-        int p2=0,p3=0,p5=0;
-        for(int i=1;i<n;i++){
-            dp.add(Math.min(dp.get(p2)*2,Math.min(dp.get(p3)*3,dp.get(p5)*5)));
-            if(dp.get(p2)*2==dp.get(i)) p2++;
-            if(dp.get(p3)*3==dp.get(i)) p3++;
-            if(dp.get(p5)*5==dp.get(i)) p5++;
+// class Solution {
+//     public int nthUglyNumber(int n) {
+//         ArrayList<Integer> dp=new ArrayList<Integer>(n);
+//         dp.add(1);
+//         int p2=0,p3=0,p5=0;
+//         for(int i=1;i<n;i++){
+//             dp.add(Math.min(dp.get(p2)*2,Math.min(dp.get(p3)*3,dp.get(p5)*5)));
+//             if(dp.get(p2)*2==dp.get(i)) p2++;
+//             if(dp.get(p3)*3==dp.get(i)) p3++;
+//             if(dp.get(p5)*5==dp.get(i)) p5++;
+//         }
+//         return dp.get(n-1);
+//     }
+// }
+
+// #Approach 3
+
+public int nthUglyNumber(int n) {
+        TreeSet<Long> st = new TreeSet<>();
+        st.add((long)1);
+        int cnt = 0;
+        long ans = 1;
+        for(;cnt != n;){
+            long ugly = st.pollFirst();
+            ans = ugly;
+            ++cnt;
+            st.add(2 * ugly);
+            st.add(3 * ugly);
+            st.add(5 * ugly);
         }
-        return dp.get(n-1);
+        return (int)ans;
     }
-}
