7e0af43b5ec6988398eefee87c17850dbee11028
Amit Choraria <jainamit130@gmail.com>
2024-04-15 16:01:50 +0530

    Time: 50 ms (69.49%), Space: 48.7 MB (64.39%) - LeetHub

---
+++ 2621-sleep/2621-sleep.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) { 
+             return new Promise(function (resolve) { 
+                setTimeout(function () { 
+                    resolve(); 
+                }, millis); 
+            }); 
+}
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
