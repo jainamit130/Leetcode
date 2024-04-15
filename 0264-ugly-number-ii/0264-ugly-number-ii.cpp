d4c2d473968273a3db34f5d4049780cb76153c8b
Amit Choraria <jainamit130@gmail.com>
2024-04-15 18:35:22 +0530

    Time: 79 ms (17.68%), Space: 30.2 MB (22.66%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -20,18 +20,44 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        vector<int> dp(n);
-        dp[0]=1;
-        int p2=0,p3=0,p5=0;
-        for(int i=1;i<n;i++){
-            dp[i]=min(dp[p2]*2,min(dp[p3]*3,dp[p5]*5));
-            if(dp[p2]*2==dp[i]) p2++;
-            if(dp[p3]*3==dp[i]) p3++;
-            if(dp[p5]*5==dp[i]) p5++;
+        unordered_set<long long> visited;
+        priority_queue<long long,vector<long long>,greater<long long>> pq;
+        pq.push(1);
+        long long top=1;
+        int count=0;
+        while(count!=n){
+            top=pq.top();
+            pq.pop();
+            if(visited.find(top)!=visited.end())
+                continue;
+            else {
+                count++;
+                visited.insert(top);
+            }
+            pq.push(top*2);
+            pq.push(top*3);
+            pq.push(top*5);
         }
-        return dp[n-1];
+        return top;
     }
 };
+
+// #Approach 3
+// class Solution {
+// public:
+//     int nthUglyNumber(int n) {
+//         vector<int> dp(n);
+//         dp[0]=1;
+//         int p2=0,p3=0,p5=0;
+//         for(int i=1;i<n;i++){
+//             dp[i]=min(dp[p2]*2,min(dp[p3]*3,dp[p5]*5));
+//             if(dp[p2]*2==dp[i]) p2++;
+//             if(dp[p3]*3==dp[i]) p3++;
+//             if(dp[p5]*5==dp[i]) p5++;
+//         }
+//         return dp[n-1];
+//     }
+// };
 
 
 /*
