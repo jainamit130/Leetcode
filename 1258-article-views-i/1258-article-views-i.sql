dae7b6bcb56ace9fb4a297b8e90debe88380b33b
Amit Choraria <jainamit130@gmail.com>
2024-04-26 00:51:42 +0530

    Time: 950 ms (28.64%), Space: 0B (100%) - LeetHub

--- 1258-article-views-i/1258-article-views-i.sql
+++ 1258-article-views-i/1258-article-views-i.sql
@@ -1,2 +1,3 @@
 # Write your MySQL query statement below
-SELECT author_id AS ID FROM VIEWS GROUP BY author_id,viewer_id HAVING author_id=viewer_id ORDER BY AUTHOR_ID; 
+SELECT author_id AS ID FROM VIEWS WHERE author_id=viewer_id GROUP BY author_id ORDER BY AUTHOR_ID; 
+
