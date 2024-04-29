8bf23da89e5ebe7043ae794d9f7960aa5bb3f2a9
Amit Choraria <jainamit130@gmail.com>
2024-04-29 12:38:15 +0530

    Time: 1736 ms (10%), Space: 8.6 MB (70%) - LeetHub

--- 3133-minimum-array-end/3133-minimum-array-end.cpp
+++ 3133-minimum-array-end/3133-minimum-array-end.cpp
@@ -23,3 +23,40 @@ public:
         return num;
     }
 };
+
+
+// class Solution {
+// public:
+//     long long minEnd(int n, int x) {
+//         n--;
+//         if(n==0)
+//             return x;
+//         vector<int> unsetBits(64,0);
+//         long long t=1;
+//         for(int i=63;i>=0;i--){
+//             if((t<<(63-i)) & x){
+//                 unsetBits[i]=1;
+//             }
+//         }
+//         vector<int> unsetBits1=unsetBits;
+//         long long numberOfZeros=1;
+//         while(n>-2){
+//             unsetBits1=unsetBits;
+//            for(int i=63;i>=0;i--){
+//                 if(unsetBits1[i]==0){
+//                     unsetBits1[i]=(numberOfZeros>>(63-i)) & 1;
+//                 }
+//            }
+//            for(int i=0;i<64;i++)
+//                 cout<<unsetBits1[i]<<" ";
+//             cout<<" ============="<<n<<endl;
+//            numberOfZeros++;
+//             n--;
+//         }
+//         long long ans=0;
+//         for(int i=0;i<64;i++){
+//             ans= (ans<<1) | unsetBits1[i];
+//         }
+//         return ans;
+//     }
+// };
