0f69e2878b3014fb353cd8007cce99ff5f4fc0d5
Amit Choraria <jainamit130@gmail.com>
2024-05-08 12:02:46 +0530

    Time: 58 ms (7.41%), Space: 92.5 MB (5.21%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.cpp
+++ 0506-relative-ranks/0506-relative-ranks.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
     vector<string> findRelativeRanks(vector<int>& score) {
-        vector<string> ans; 
-        bool gold=false,silver=false,bronze=false;
-        for(auto n:score){
-            if(n==1)
-                ans.push_back("Gold Medal");
-            else if(n==2){
-                ans.push_back("Silver Medal");
-            }
-            else if(n==3){
-                ans.push_back("Bronze Medal");
-            } else {
-                ans.push_back(to_string(i));
+        vector<int> st(1000001,-1);
+        for(int i=0;i<score.size();i++){
+            st[score[i]]=i;
+        }
+        int k=1;
+        vector<string> ans(score.size());
+        for(int i=1000000;i>=0;i--){
+            if(st[i]!=-1){
+                if(k==1)
+                    ans[st[i]]="Gold Medal";
+                else if(k==2)
+                    ans[st[i]]="Silver Medal";
+                else if(k==3)
+                    ans[st[i]]="Bronze Medal";
+                else {
+                    ans[st[i]]=to_string(k);
+                }
+                k++;
             }
         }
         return ans;
