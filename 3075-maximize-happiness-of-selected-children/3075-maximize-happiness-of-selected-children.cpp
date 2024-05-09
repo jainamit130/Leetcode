6eedb34bffc702f0fa6ce98b5055478a426fe20c
Amit Choraria <jainamit130@gmail.com>
2024-05-09 17:31:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
@@ -2,16 +2,14 @@ class Solution {
 public:
     long long maximumHappinessSum(vector<int>& happiness, int k) {
         sort(happiness.begin(),happiness.end());
-        int decrement=0;
-        long long ans=0;
-        while(happiness.size() && k>0 && happiness.back()>0){
-            int t=happiness.back()-decrement;
-            if(t<=0)
+        int ans=0;
+        int t=0;
+        for(int i=happiness.size()-1;i>=0 && k>0;i--){
+            if((happiness[i]-t)<0)
                 break;
-            ans+=t;
+            ans+=happiness[i]-t;
+            t++;
             k--;
-            decrement++;
-            happiness.pop_back();
         }
         return ans;
     }
