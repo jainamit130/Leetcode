1a469bf010cc8160eed0f2549f2cbc2b2155de36
Amit Choraria <jainamit130@gmail.com>
2024-05-20 13:29:02 +0530

    Time: 0 ms (100%), Space: 8.2 MB (88.36%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
-    int ans=0;
     int subsetXORSum(vector<int>& nums) {
-        takeItOrLeaveIt(nums,0,0);
-        return ans;
-    }
-
-    void takeItOrLeaveIt(vector<int>& nums,int index,int currXor){
-        if(index>=nums.size()){
-            ans+=currXor;
-            return;
-        }
-
-        //take it
-        takeItOrLeaveIt(nums,index+1,currXor^nums[index]);
-        //leave it
-        takeItOrLeaveIt(nums,index+1,currXor);
-        return;
+        int ors=0;
+        for(auto n:nums)
+            ors=ors|n;
+        return ors<<nums.size()-1;
     }
 };
+
+// class Solution {
+// public:
+//     int ans=0;
+//     int subsetXORSum(vector<int>& nums) {
+//         takeItOrLeaveIt(nums,0,0);
+//         return ans;
+//     }
+
+//     void takeItOrLeaveIt(vector<int>& nums,int index,int currXor){
+//         if(index>=nums.size()){
+//             ans+=currXor;
+//             return;
+//         }
+
+//         //take it
+//         takeItOrLeaveIt(nums,index+1,currXor^nums[index]);
+//         //leave it
+//         takeItOrLeaveIt(nums,index+1,currXor);
+//         return;
+//     }
+// };
