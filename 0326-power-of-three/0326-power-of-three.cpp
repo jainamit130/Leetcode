b6f845a1ef0a6d7c94984b7c64240fd5192b921c
Amit Choraria <jainamit130@gmail.com>
2024-05-26 13:33:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,15 +1,5 @@
 class Solution {
-public:
     bool isPowerOfThree(int n) {
-        n=abs(n);
-        if(n==1 || n==0)
-            return false;
-        while(n>1){
-            if(n%3!=0)
-                return false;
-            else
-                n/=3;
-        }
-        return true;
+        return n > 0 && (1162261467 % n == 0);
     }
 };
