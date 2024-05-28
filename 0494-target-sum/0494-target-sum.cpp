3fc6905b391a855af2d5395457ea0ae47238ba2a
Amit Choraria <jainamit130@gmail.com>
2024-05-28 19:12:16 +0530

    Time: 16 ms (55.47%), Space: 24.8 MB (27.43%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        return solve(nums,target,0,0);
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        vector<vector<int>> cache(nums.size()+1, vector<int>(2 * sum + 1, INT_MIN));
+        return solve(nums,target,0,0,sum,cache);
     }
 
-    int solve(vector<int>& nums,int target,int currSum,int index){
+    int solve(vector<int>& nums,int target,int currSum,int index,int sum,vector<vector<int>>& cache){
         if(index>=nums.size()){
             if(currSum==target)
                 return 1;
             else
                 return 0;
         }
+
+        int adjustedSum = currSum + sum;
+
+        if(cache[index][adjustedSum]!=INT_MIN)
+            return cache[index][adjustedSum];
     
-        int subtract = solve(nums,target,currSum-nums[index],index+1);
-        int add = solve(nums,target,currSum+nums[index],index+1);
-        return subtract+add;
+        int subtract = solve(nums,target,currSum-nums[index],index+1,sum,cache);
+        int add = solve(nums,target,currSum+nums[index],index+1,sum,cache);
+        return cache[index][adjustedSum]=subtract+add;
     }
 };
 
