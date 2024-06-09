9b32081fe03e46aa27fa7b5d87e9908b6136c295
Amit Choraria <jainamit130@gmail.com>
2024-06-09 16:59:57 +0530

    Time: 2 ms (58.89%), Space: 7.2 MB (24.11%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.cpp
+++ 0319-bulb-switcher/0319-bulb-switcher.cpp
@@ -1,9 +1,9 @@
-// class Solution {
-// public:
-//     int bulbSwitch(int n) {
-        
-//     }
-// };
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        return sqrt(n);
+    }
+};
 
 
 /*
@@ -11,7 +11,12 @@
 
 2   4   6   8   10  12  14  16  18  20  22  24  26  28  30  32  34  36  38  40  42  44  46  48  50
 
+8
 
+2   4   6   8   10
+
+
+16
 
 
 
@@ -19,31 +24,30 @@
 */
 
 
-class Solution {
-public:
-    unordered_set<int> st;
-    int bulbSwitch(int n) {
-        for(int i=1;i<=n;i++){
-            if(i%2!=0)
-                st.insert(i);
-        }
-        solve(3,n,st);
-        return st.size();
-    }
+// class Solution {
+// public:
+//     unordered_set<int> st;
+//     int bulbSwitch(int n) {
+//         for(int i=1;i<=n;i++){
+//             st.insert(i);
+//         }
+//         solve(2,n,st);
+//         return st.size();
+//     }
 
-    void solve(int currRound,int& n,unordered_set<int>& st){
-        if(currRound>n){
-            return;
-        }
+//     void solve(int currRound,int& n,unordered_set<int>& st){
+//         if(currRound>n){
+//             return;
+//         }
 
-        for(int i=currRound;i<=n;i+=currRound){
-            if(st.find(i)!=st.end()){
-                st.erase(i);
-            } else {
-                st.insert(i);
-            }
-        }
-        solve(currRound+1,n,st);
-        return;
-    }
-};
+//         for(int i=currRound;i<=n;i+=currRound){
+//             if(st.find(i)!=st.end()){
+//                 st.erase(i);
+//             } else {
+//                 st.insert(i);
+//             }
+//         }
+//         solve(currRound+1,n,st);
+//         return;
+//     }
+// };
