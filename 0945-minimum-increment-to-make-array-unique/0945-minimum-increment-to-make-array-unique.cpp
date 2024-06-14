f17f60cc84b20837095031d6c96243566aafd25b
Amit Choraria <jainamit130@gmail.com>
2024-06-14 17:03:32 +0530

    Time: 118 ms (65.94%), Space: 69 MB (93.9%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
@@ -2,15 +2,12 @@ class Solution {
 public:
     int minIncrementForUnique(vector<int>& nums) {
        sort(nums.begin(),nums.end());
-       unordered_set<int> st;
-       st.insert(nums[0]);
        int ans=0;
        for(int i=1;i<nums.size();i++){
-            if(st.find(nums[i])!=st.end()){
+            if(nums[i]<=nums[i-1]){
                 ans+=(nums[i-1]+1)-nums[i];
                 nums[i]=nums[i-1]+1;
             }
-            st.insert(nums[i]);
        } 
        return ans;
     }
