67a220665876d8d0c7cc7721c534df73b830d85e
Amit Choraria <jainamit130@gmail.com>
2024-06-25 12:06:17 +0530

    Time: 4 ms (35.79%), Space: 10.2 MB (42.71%) - LeetHub

--- 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.cpp
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.cpp
@@ -9,22 +9,13 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
     TreeNode* bstToGst(TreeNode* root) {
-        int sum=findSum(root);
+        int sum=0;
         solve(root,sum);   
         return root;
-    }
-
-    int findSum(TreeNode* root){
-        if(!root){
-            return 0;
-        }
-
-        int sum=0;
-        sum+=root->val + findSum(root->left) + findSum(root->right);
-        return sum;
     }
 
     void solve(TreeNode* root,int& sum){
@@ -32,11 +23,43 @@ public:
             return;
         }
 
-        solve(root->left,sum);
-        int temp=root->val;
-        root->val=sum;
-        sum-=temp;
         solve(root->right,sum);
+        sum+=root->val;
+        root->val=sum;
+        solve(root->left,sum);
         return;
     }
 };
+
+
+// class Solution {
+// public:
+//     TreeNode* bstToGst(TreeNode* root) {
+//         int sum=findSum(root);
+//         solve(root,sum);   
+//         return root;
+//     }
+
+//     int findSum(TreeNode* root){
+//         if(!root){
+//             return 0;
+//         }
+
+//         int sum=0;
+//         sum+=root->val + findSum(root->left) + findSum(root->right);
+//         return sum;
+//     }
+
+//     void solve(TreeNode* root,int& sum){
+//         if(!root){
+//             return;
+//         }
+
+//         solve(root->left,sum);
+//         int temp=root->val;
+//         root->val=sum;
+//         sum-=temp;
+//         solve(root->right,sum);
+//         return;
+//     }
+// };
