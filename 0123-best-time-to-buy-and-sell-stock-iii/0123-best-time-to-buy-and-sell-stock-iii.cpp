c7d15a5bc5a05694c8f93cac2fed6f9da39f3eb7
Amit Choraria <jainamit130@gmail.com>
2024-06-26 23:37:32 +0530

    Time: 451 ms (35.24%), Space: 222.1 MB (20.89%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        return solve(prices,0,0,false,2);
+        vector<vector<vector<int>>> cache(prices.size()+1,vector<vector<int>>(2,vector<int>(3,-1)));
+        return solve(prices,0,false,2,cache);
     }
 
-    int solve(vector<int>& prices,int index,int currProfit,bool isBought,int transactions){
-        if(index>=prices.size()){
-            return currProfit;
+    int solve(vector<int>& prices,int index,bool isBought,int transactions,vector<vector<vector<int>>>& cache){
+        if(index>=prices.size() || transactions<=0){
+            return 0;
         }
 
-        if(transactions<0){
-            return INT_MIN;
+        if(cache[index][isBought][transactions]!=-1){
+            return cache[index][isBought][transactions];
         }
 
-        if(transactions==0 && !isBought){
-            return currProfit;
-        }
-
-        int ans=0;
+        int ans=INT_MIN;
         if(isBought){
-            ans=max(ans,max(currProfit,solve(prices,index+1,currProfit+prices[index],false,transactions-1)));
+            ans=max(ans,prices[index]+solve(prices,index+1,false,transactions-1,cache));
         } else {
-            ans=max(ans,max(currProfit,solve(prices,index+1,currProfit-prices[index],true,transactions)));
+            ans=max(ans,-prices[index]+solve(prices,index+1,true,transactions,cache));
         }
-        ans=max(ans,max(currProfit,solve(prices,index+1,currProfit,isBought,transactions)));
-        return ans;
+        ans=max(ans,solve(prices,index+1,isBought,transactions,cache));
+        return cache[index][isBought][transactions]=ans;
 
     }
 };
