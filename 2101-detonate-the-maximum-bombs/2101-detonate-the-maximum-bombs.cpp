ebcdcf4907bc0c1da1a6613c6f5b8c25ad8d2396
Amit Choraria <jainamit130@gmail.com>
2024-07-01 22:35:56 +0530

    Time: 53 ms (88.3%), Space: 21.3 MB (52.35%) - LeetHub

---
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int maximumDetonation(vector<vector<int>>& bombs) {
+        int n=bombs.size();
+        vector<vector<int>> adj(n);
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if(i!=j){
+                double distance = sqrt(abs(pow(bombs[i][0] - bombs[j][0], 2)) + pow(abs(bombs[i][1] - bombs[j][1]), 2));
+                    if(distance<=(double)bombs[i][2]){
+                        adj[i].push_back(j);
+                    }
+                    if(distance<=(double)bombs[j][2]){
+                        adj[j].push_back(i);
+                    }
+                }
+            }
+        }
+
+        int ans=1;
+        for(int i=0;i<n;i++){
+            vector<int> visited(n,-1);
+            int a=countDetonation(adj,i,visited);
+            ans=max(ans,a);
+        }
+        return ans;
+    }
+
+    int countDetonation(vector<vector<int>>& adj, int node, vector<int>& visited) {
+        visited[node] = 1; // Mark the current node as visited
+        int count = 1; // Initialize count with 1 (counting the current node itself)
+        
+        for (int neighbor : adj[node]) {
+            if (visited[neighbor] == -1) { // If neighbor node is not visited
+                count += countDetonation(adj, neighbor, visited); // Recursively count nodes reachable from neighbor
+            }
+        }
+        
+        return count; // Return the total count of reachable nodes from the starting node
+    }
+};
