5e930995b977b29a9e44ffa458572bbeb7bffa48
Amit Choraria <jainamit130@gmail.com>
2024-07-03 00:28:52 +0530

    Time: 0 ms (100%), Space: 8.9 MB (37.22%) - LeetHub

--- 0843-guess-the-word/0843-guess-the-word.cpp
+++ 0843-guess-the-word/0843-guess-the-word.cpp
@@ -7,27 +7,35 @@
  * };
  */
 class Solution {
-    int findMatches(string w1, string w2){
-        int count = 0;
-        for(int i =0; i < 6; ++i)
-            if(w1[i] == w2[i]) count++;
+    int match(string w1,string w2){
+        int count=0;
+        for(int i=0;i<6;i++){
+            if(w1[i]==w2[i])
+                count++;
+        }
         return count;
     }
 public:
     void findSecretWord(vector<string>& words, Master& master) {
         srand(time(0));
         vector<string> candidates;
-        for(int i =0; i < words.size(); ++i)
-            candidates.push_back(words[i]);
-        
+        for(auto s:words){
+            candidates.push_back(s);
+        }
+
         while(!candidates.empty()){
-            string word = candidates[rand()%candidates.size()];
-            int match = master.guess(word);
-            if(match == 6) return;
+            string word=candidates[rand()%candidates.size()];
+            int g=master.guess(word);
+            if(g==6){
+                return;
+            }
             vector<string> temp;
-            for(string w: candidates)
-                if(findMatches(w, word) == match) temp.push_back(w);
-            candidates = temp;
+            for(int i=0;i<candidates.size();i++){
+                if(match(candidates[i],word)==g){
+                    temp.push_back(candidates[i]);
+                }
+            }
+            candidates=temp;
         }
     }
 };
