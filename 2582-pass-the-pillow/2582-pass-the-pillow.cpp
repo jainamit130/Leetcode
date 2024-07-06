05c4ad54a90d4766c86611a22ac50aef318646d2
Amit Choraria <jainamit130@gmail.com>
2024-07-06 10:38:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2582-pass-the-pillow/2582-pass-the-pillow.cpp
+++ 2582-pass-the-pillow/2582-pass-the-pillow.cpp
@@ -3,12 +3,9 @@ public:
     int passThePillow(int n, int time) {
         int round=time/(n-1);
         time=time%(n-1);
-        int start=1;
-        int ans=start+time;
         if(round%2!=0){
-            start=n;
-            ans=start-time;
+            return n-time;
         }
-        return ans;
+        return n+time;
     }
 };
