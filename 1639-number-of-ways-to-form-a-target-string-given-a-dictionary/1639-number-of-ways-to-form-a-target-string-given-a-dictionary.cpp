de40d51dae181c6b01f3ecbcb12ee63c67e6da2b
Amit Choraria <jainamit130@gmail.com>
2024-07-06 17:26:05 +0530

    Time: 340 ms (15.25%), Space: 58.4 MB (43.43%) - LeetHub

--- 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
+++ 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
@@ -1,37 +1,53 @@
 class Solution {
 public:
-    int mod=1e9+7;
+    int mod = 1e9 + 7;
+
     int numWays(vector<string>& words, string target) {
-        vector<vector<int>> cache(words[0].length()+1,vector<int>(target.length()+1,-1));
-    map<pair<int, char>, int> mp;
-    for (int i = 0; i < words.size(); i++) {
-        for (int j = 0; j < words[i].length(); j++) {
-            mp[{j, words[i][j]}]++;
+        int n = words[0].length();
+        int m = target.length();
+
+        // Create a map to count occurrences of (position, character) in words
+        vector<map<char, int>> mp(n);
+        for (auto& word : words) {
+            for (int j = 0; j < n; ++j) {
+                mp[j][word[j]]++;
+            }
         }
-    }
-        return solve(words,0,0,target,cache,mp);
+
+        // Memoization table
+        vector<vector<int>> cache(n, vector<int>(m, -1));
+
+        return solve(words, 0, 0, target, cache, mp);
     }
 
-    int solve(vector<string>& words,int usableIndexes,int index,string target,vector<vector<int>>& cache,map<pair<int, char>, int>& mp){
-        if(index>=target.size()){
+    int solve(vector<string>& words, int usableIndexes, int index, string& target,
+              vector<vector<int>>& cache, vector<map<char, int>>& mp) {
+        if (index >= target.size()) {
             return 1;
         }
-        
-        if(usableIndexes>=words[0].length()){
+
+        if (usableIndexes >= words[0].length()) {
             return 0;
         }
 
-        if(cache[usableIndexes][index]!=-1){
+        if (cache[usableIndexes][index] != -1) {
             return cache[usableIndexes][index];
         }
-        int ans=0;
-        int ch=target[index];
-        ans=ans+(long long)solve(words,usableIndexes+1,index,target,cache,mp)%mod;
-            ans=(ans+mp[{usableIndexes,ch}]*(long long)solve(words,usableIndexes+1,index+1,target,cache,mp))%mod;
-        return cache[usableIndexes][index]=ans;
+
+        char ch = target[index];
+        long long ans = 0;
+
+        // Case 1: Do not pick character from words[usableIndexes]
+        ans = (ans + solve(words, usableIndexes + 1, index, target, cache, mp)) % mod;
+
+        // Case 2: Pick character ch from words[usableIndexes]
+        if (mp[usableIndexes].count(ch) > 0) {
+            ans = (ans + (long long) mp[usableIndexes][ch] * solve(words, usableIndexes + 1, index + 1, target, cache, mp)) % mod;
+        }
+
+        return cache[usableIndexes][index] = ans;
     }
 };
-
 /*
 
 ch, i
