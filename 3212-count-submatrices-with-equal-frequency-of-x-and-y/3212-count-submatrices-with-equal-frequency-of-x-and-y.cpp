a4627f088030622efa660090188611066dd05a39
Amit Choraria <jainamit130@gmail.com>
2024-07-07 23:59:16 +0530

    Time: 931 ms (16.67%), Space: 282.1 MB (16.67%) - LeetHub

--- 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.cpp
+++ 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.cpp
@@ -3,33 +3,33 @@ public:
     int numberOfSubmatrices(vector<vector<char>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
-        vector<vector<vector<int>>> prefix(grid.size(),vector<vector<int>>(grid[0].size(),vector<int>(2)));
-        //Building 1st Col
+        vector<vector<vector<int>>> prefix(m,vector<vector<int>>(n,vector<int>(2)));
+        prefix[0][0]={0,0};
+        //first row 
         int count1=0;
         int count2=0;
         for(int i=0;i<m;i++){
             if(grid[i][0]=='X'){
-                count1++;
+                count1+=1;
             } else if(grid[i][0]=='Y'){
-                count2++;
+                count2+=1;
             }
-            prefix[i][0][0]=count1;
-            prefix[i][0][1]=count2;
+            prefix[i][0]={count1,count2};
         }
-        
-        //Building 1st Row
+
+        //first col 
         count1=0;
         count2=0;
         for(int j=0;j<n;j++){
             if(grid[0][j]=='X'){
-                count1++;
+                count1+=1;
             } else if(grid[0][j]=='Y'){
-                count2++;
+                count2+=1;
             }
-            prefix[0][j][0]=count1;
-            prefix[0][j][1]=count2;
+            prefix[0][j]={count1,count2};
         }
-        
+
+        //build remaining prefix
         for(int i=1;i<m;i++){
             for(int j=1;j<n;j++){
                 count1=0,count2=0;
@@ -42,12 +42,13 @@ public:
                 prefix[i][j][1]=prefix[i-1][j][1]+prefix[i][j-1][1]-prefix[i-1][j-1][1]+count2;
             }
         }
+
         int ans=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 count1=prefix[i][j][0];
                 count2=prefix[i][j][1];
-                if(count1>=1 && count2==count1){
+                if(count1>=1 && count1==count2){
                     ans++;
                 }
             }
@@ -55,3 +56,13 @@ public:
         return ans;
     }
 };
+
+/*
+
+
+
+
+
+
+
+*/
