7ae8c9032055610af3370ed7fda4e07a0e0a0522
Amit Choraria <jainamit130@gmail.com>
2024-07-10 01:00:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0818-race-car/0818-race-car.cpp
+++ 0818-race-car/0818-race-car.cpp
@@ -1,47 +1,30 @@
 class Solution {
 public:
     int racecar(int target) {
-        vector<int> dp(target + 1, -1);
-        return racecarHelper(target, dp);
+        vector<int> dp(target+1,-1);
+        return solve(target,dp);
     }
 
-private:
-    int racecarHelper(int target, vector<int>& dp) {
-        if (dp[target] >= 0) {
+    int solve(int target,vector<int>& dp){
+        if(dp[target]!=-1){
             return dp[target];
         }
 
-        int n = floor(log2(target)) + 1;
-        if (target == (1 << n) - 1) {
-            dp[target] = n;
-        } else {
-            dp[target] = n + 1 + racecarHelper((1 << n) - 1 - target, dp);
-            for (int i = 0; i < n - 1; ++i) {
-                dp[target] = min(dp[target], n + i + 1 + racecarHelper(target - (1 << (n - 1)) + (1 << i), dp));
-            }
+        int n=floor(log2(target))+1;
+        if((1<<n)-1==target){
+            return n;
         }
-        return dp[target];
+
+        dp[n]=n+1+solve((1<<n)-1-target,dp);
+        for(int i=0;i<n-1;i++){
+            dp[n]=min(dp[n],n+i+1+solve(target-(1<<(n-1))+(1<<i),dp));
+        }
+        return dp[n];
     }
 };
 
 
-
 /*
-
-
-0   1   2   3   4   
-0   1   3   7   15  
-
-
-    A   A   A
-0   1   3   7
-
-Diff = 2
-
-
-    A   A   A   A
-0   1   3   7   15  
-
 
 
 
