296769fce3005fb04e8e4f60173b0e1dab2c6965
Amit Choraria <jainamit130@gmail.com>
2024-07-12 15:03:40 +0530

    Time: 1902 ms (5.29%), Space: 393 MB (5.28%) - LeetHub

---
+++ 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.cpp
@@ -0,0 +1,98 @@
+class Solution {
+public:
+    int maximumGain(string s, int x, int y) {
+        int ans=0;
+        stack<char> st;
+        for(int i=0;i<s.length();i++){
+            if(s[i]!='b' && s[i]!='a'){
+                stack<char> copy=st;
+                ans+=max(consumeStack(copy,x,y,"ab","ba"),consumeStack(st,y,x,"ba","ab"));
+            } else {
+                st.push(s[i]);
+            }
+        }
+        stack<char> copy=st;
+        ans+=max(consumeStack(copy,x,y,"ab","ba"),consumeStack(st,y,x,"ba","ab"));
+        return ans;
+    }
+
+    int consumeStack(stack<char>& st,int imp,int nimp,string impStr,string nimpStr){
+        stack<char> st2;
+        int ans=0;
+        while(!st.empty()){
+            st2.push(st.top());
+            st.pop();
+            while(!st.empty() && !st2.empty()){
+            char secondChar=st2.top();
+            char firstChar=st.top();
+            string word(1, firstChar);
+            word += secondChar;
+            if(word==impStr){
+                ans+=imp;
+                st2.pop();
+                st.pop();
+            } else{
+                st2.push(st.top());
+                st.pop();
+            }
+            }
+        }
+        // printStack(st2);
+        stack<char> st3;
+        while(!st2.empty()){
+            st3.push(st2.top());
+            st2.pop();
+            while(!st2.empty() && !st3.empty()){
+            char secondChar=st2.top();
+            char firstChar=st3.top();
+            string word(1, firstChar);
+            word += secondChar;
+            // cout<<"---------st2-----"<<endl;
+            // printStack(st2);
+            // cout<<"----------st3----"<<endl;
+            // printStack(st3);
+            // cout<<word<<" "<<nimpStr<<endl;
+            if(word==nimpStr){
+                ans+=nimp;
+                st3.pop();
+                st2.pop();
+            } else{
+                st3.push(st2.top());
+                st2.pop();
+            }
+            }
+        }
+        return ans;
+    }
+
+
+// void printStack(std::stack<char> st) {
+//     std::stack<char> temp;
+
+//     while (!st.empty()) {
+//         temp.push(st.top());
+//         st.pop();
+//     }
+
+//     while (!temp.empty()) {
+//         std::cout << temp.top() << " ";
+//         st.push(temp.top());
+//         temp.pop();
+//     }
+
+//     std::cout << std::endl;
+// }
+};
+
+
+
+/*
+
+c   d   b   c   b   b   a   a   a   b   a   b
+                                            i
+st: b   b   a   a   a      
+point: 18
+
+st2: b  
+
+*/
