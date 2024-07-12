738bad4548f28b585dc76ec1a0e838adea04d0fd
Amit Choraria <jainamit130@gmail.com>
2024-07-12 15:59:35 +0530

    Time: 1950 ms (5.29%), Space: 391.9 MB (5.28%) - LeetHub

--- 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.cpp
+++ 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.cpp
@@ -17,43 +17,36 @@ public:
     }
 
     int consumeStack(stack<char>& st,int imp,int nimp,string impStr,string nimpStr){
-        stack<char> st2;
         int ans=0;
-        while(!st.empty()){
-            st2.push(st.top());
-            st.pop();
-            while(!st.empty() && !st2.empty()){
-            char secondChar=st2.top();
-            char firstChar=st.top();
-            string word(1, firstChar);
-            word += secondChar;
-            if(word==impStr){
-                ans+=imp;
-                st2.pop();
-                st.pop();
-            } else{
-                st2.push(st.top());
-                st.pop();
-            }
-            }
-        }
+        stack<char> st2;
+        ans+=process(st,st2,0,impStr,imp);
         stack<char> st3;
-        while(!st2.empty()){
-            st3.push(st2.top());
-            st2.pop();
-            while(!st2.empty() && !st3.empty()){
-            char secondChar=st2.top();
-            char firstChar=st3.top();
-            string word(1, firstChar);
-            word += secondChar;
-            if(word==nimpStr){
-                ans+=nimp;
-                st3.pop();
-                st2.pop();
-            } else{
-                st3.push(st2.top());
-                st2.pop();
-            }
+        ans+=process(st2,st3,1,nimpStr,nimp);
+        return ans;
+    }
+
+    int process(stack<char>& st1,stack<char>& st2,int flag,string str,int score) {
+        int ans=0;
+        while(!st1.empty()){
+            st2.push(st1.top());
+            st1.pop();
+            while(!st1.empty() && !st2.empty()){
+                char secondChar=st2.top();
+                char firstChar=st1.top();
+                if(flag){
+                    secondChar=st1.top();
+                    firstChar=st2.top();
+                }
+                string word(1, firstChar);
+                word += secondChar;
+                if(word==str){
+                    ans+=score;
+                    st2.pop();
+                    st1.pop();
+                } else{
+                    st2.push(st1.top());
+                    st1.pop();
+                }
             }
         }
         return ans;
