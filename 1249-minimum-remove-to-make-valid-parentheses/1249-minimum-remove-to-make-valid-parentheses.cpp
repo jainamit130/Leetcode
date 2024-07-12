eed5f512e6bab4eec2c56017d0840b6340b01369
Amit Choraria <jainamit130@gmail.com>
2024-07-12 23:00:48 +0530

    Time: 15 ms (92.44%), Space: 13.9 MB (22.61%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     string minRemoveToMakeValid(string s) {
-        stack<int> st;
+        int count=0;
         string ans="";
         for(int i=0;i<s.length();i++){
             if(s[i]=='('){
-                st.push(i);
+                count++;
                 ans+=s[i];
             } else if(s[i]==')') {
-                if(!st.empty()){
-                    st.pop();
+                if(count>0){
+                    count--;
                     ans+=s[i];
                 }  
             } else {
@@ -17,11 +17,10 @@ public:
             }
         }
 
-        int size=st.size();
         string t="";
         for(int i=ans.length()-1;i>=0;i--){
-            if(ans[i]=='(' && size){
-                size--;
+            if(ans[i]=='(' && count){
+                count--;
                 continue;
             }
             t+=ans[i];
