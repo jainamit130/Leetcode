5979458f5030b8b99b810c975f92351d2bf7dc90
Amit Choraria <jainamit130@gmail.com>
2024-07-14 20:26:31 +0530

    Time: 4 ms (75%), Space: 29.4 MB (100%) - LeetHub

--- 3218-minimum-cost-for-cutting-cake-i/3218-minimum-cost-for-cutting-cake-i.cpp
+++ 3218-minimum-cost-for-cutting-cake-i/3218-minimum-cost-for-cutting-cake-i.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
     int minimumCost(int m, int n, vector<int>& horizontalCut, vector<int>& verticalCut) {
-        sort(horizontalCut.begin(),horizontalCut.end());
-        sort(verticalCut.begin(),verticalCut.end());
-        int i=m-2;
         int hFactor=1;
         int vFactor=1;
-        int ans=0;
+        int i=m-2;
         int j=n-2;
+        int ans=0;
+        sort(horizontalCut.begin(),horizontalCut.end());
+        sort(verticalCut.begin(),verticalCut.end());
         while(i>=0 && j>=0){
-            if(horizontalCut[i]>verticalCut[j]){
+            if(horizontalCut[i]<verticalCut[j]){
+                ans+=verticalCut[j]*hFactor;
                 vFactor+=1;
-                ans+=horizontalCut[i]*hFactor;
-                i--;  
-            } else if(horizontalCut[i]<=verticalCut[j]) {
-                hFactor+=1;
-                ans+=verticalCut[j]*vFactor; 
                 j--;
-            } 
+            } else {
+                ans+=horizontalCut[i]*vFactor;
+                hFactor+=1;
+                i--;
+            }
         }
 
         while(i>=0){
-            ans+=horizontalCut[i]*hFactor;
-            i--;  
+            ans+=horizontalCut[i]*vFactor;
+            i--;
         }
 
         while(j>=0){
-            ans+=verticalCut[j]*vFactor; 
-            j--; 
+            ans+=verticalCut[j]*hFactor;
+            j--;
         }
         return ans;
     }
