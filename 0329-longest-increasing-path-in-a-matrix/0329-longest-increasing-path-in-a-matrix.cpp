b7de85b3c80108cbc642ba584a757d641894030c
Amit Choraria <jainamit130@gmail.com>
2024-07-19 22:54:20 +0530

    Time: 30 ms (63.97%), Space: 19.7 MB (48.57%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -2,11 +2,12 @@ class Solution {
 public:
     vector<vector<int>> dir={{0,1},{1,0},{-1,0},{0,-1}};
     int longestIncreasingPath(vector<vector<int>>& matrix) {
+        vector<vector<int>> cache(matrix.size()+1,vector<int>(matrix[0].size()+1,-1));
         vector<vector<int>> adj;
         int ans=1;
         for(int i=0;i<matrix.size();i++){
             for(int j=0;j<matrix[0].size();j++){
-                ans=max(ans,dfs(i,j,matrix));
+                ans=max(ans,dfs(i,j,matrix,cache));
             }
         }
         return ans;
@@ -18,19 +19,23 @@ public:
         return true;
     }
 
-    int dfs(int row,int col,vector<vector<int>>& matrix){
+    int dfs(int row,int col,vector<vector<int>>& matrix,vector<vector<int>>& cache){
         if(!isValid(row,col,matrix.size(),matrix[0].size())){
             return 0;
         }
 
-        int maxi=1;
+        if(cache[row][col]!=-1){
+            return cache[row][col];
+        }
+
+         cache[row][col]=1;
         for(int i=0;i<dir.size();i++){
             int newr=row+dir[i][0];
             int newc=col+dir[i][1];
             if(isValid(newr,newc,matrix.size(),matrix[0].size()) && matrix[newr][newc]>matrix[row][col]){
-                maxi=max(maxi,1+dfs(newr,newc,matrix));
+                 cache[row][col]=max( cache[row][col],1+dfs(newr,newc,matrix,cache));
             }
         }
-        return maxi;
+        return  cache[row][col];
     }
 };
