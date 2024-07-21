8e89b6ba9821fed0b798878f96855f04626264f8
Amit Choraria <jainamit130@gmail.com>
2024-07-21 17:53:00 +0530

    Time: 145 ms (25%), Space: 108.6 MB (75%) - LeetHub

--- 3224-minimum-array-changes-to-make-differences-equal/3224-minimum-array-changes-to-make-differences-equal.cpp
+++ 3224-minimum-array-changes-to-make-differences-equal/3224-minimum-array-changes-to-make-differences-equal.cpp
@@ -1,46 +1,25 @@
 class Solution {
 public:
     int minChanges(vector<int>& nums, int k) {
-        map<int,int> mp;
+        unordered_map<int,int> mp;
+        vector<int> maxDiff;
         int n=nums.size();
         for(int i=0;i<n/2;i++){
-            int diff=abs(nums[i]-nums[n-i-1]);
+            int a=nums[i];
+            int b=nums[n-i-1];
+            int diff=abs(a-b);
             mp[diff]++;
-        }
-        int result=INT_MAX;
-        
-        vector<vector<int>> freqMp;
-        for(auto [a,b]:mp){
-            cout<<a<<" "<<b<<endl;
-            freqMp.push_back({b,a});
+            int maxAtt=max(max(a,b),max(k-b,k-a));
+            maxDiff.push_back(maxAtt);
         }
 
-        sort(freqMp.begin(),freqMp.end());
-        int i=freqMp.size()-1;
-        int freq=-1;
-        while(i>=0){
-            if(freq==-1){
-                freq=freqMp[i][0];
-            } else {
-                if(freqMp[i][0]!=freq && freqMp[i][1]!=15){
-                    break;
-                }
-            }
-            int targetDiff=freqMp[i][1];
-            int ans=0;
-            for(int i=0;i<n/2;i++){
-                int diff=abs(nums[i]-nums[n-i-1]);
-                if(diff!=targetDiff){
-                    if(nums[n-i-1]-targetDiff>=0 || nums[n-i-1]+targetDiff<=k || nums[i]-targetDiff>=0 || nums[i]+targetDiff<=k){
-                        ans+=1;
-                    } else {
-                        ans+=2;
-                    }
-                }
-            }
-            result=min(ans,result);
-            i--;
+        sort(maxDiff.begin(),maxDiff.end());
+        int ans=INT_MAX;
+        for(auto [a,b]:mp){
+            int lesser=lower_bound(maxDiff.begin(),maxDiff.end(),a)-maxDiff.begin();
+            int greater=(n/2)-lesser;
+            ans=min(ans,greater-b+2*lesser);
         }
-        return result;
+        return ans;
     }
 };
