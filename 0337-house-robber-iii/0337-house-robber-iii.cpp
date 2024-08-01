45430aaa863e53b008e4ada88fb6420d62a049e1
Amit Choraria <jainamit130@gmail.com>
2024-08-01 19:48:23 +0530

    Time: 17 ms (34.45%), Space: 32.1 MB (5%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.cpp
+++ 0337-house-robber-iii/0337-house-robber-iii.cpp
@@ -6,33 +6,33 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     int rob(TreeNode* root) {
-        unordered_map<TreeNode*,unordered_map<bool,int>> cache;
-        return max(solve(root,0,cache),solve(root,1,cache));
+        vector<int> options = travel(root);
+        return max(options[0], options[1]);
     }
 
-    int solve(TreeNode* root,bool chosen,unordered_map<TreeNode*,unordered_map<bool,int>>& cache){
-        if(!root){
-            return 0;
-        }
+    vector<int> travel(TreeNode* root) {
+        // Base case. just return {0,0} as you cannot rob anything
+        if (root == NULL)
+            return {0,0};
 
-        if(cache.find(root)!=cache.end() && cache[root].find(chosen)!=cache[root].end()){
-            return cache[root][chosen];
-        }
+        vector<int> left_node_choices = travel(root->left);
+        vector<int> right_node_choices = travel(root->right);
+        vector<int> options(2);
 
-        int ans=0;
-        if(chosen){
-            ans=max(ans,root->val+solve(root->left,false,cache)+solve(root->right,false,cache));
-            ans=max(ans,solve(root->left,true,cache)+solve(root->right,true,cache));
-        } else {
-            ans=max(ans,solve(root->left,true,cache)+solve(root->right,true,cache));
-        }
-        
-        return cache[root][chosen]=ans;
+        // Store value if looted in [0]
+        options[0] = root->val + left_node_choices[1] + right_node_choices[1];
+
+        // Store value if skipped in [1]
+        options[1] = max(left_node_choices[0], left_node_choices[1]) +
+                     max(right_node_choices[0], right_node_choices[1]);
+
+        return options;
     }
 };
