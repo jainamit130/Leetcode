8c8657b11319e9d87eba16ad3e37e8a39a71391f
Amit Choraria <jainamit130@gmail.com>
2024-08-02 23:40:16 +0530

    Time: 5 ms (44.06%), Space: 29.9 MB (7.18%) - LeetHub

--- 0593-valid-square/0593-valid-square.cpp
+++ 0593-valid-square/0593-valid-square.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool validSquare(vector<int>& p1, vector<int>& p2, vector<int>& p3, vector<int>& p4) {
         unordered_map<double,int> mp;
-        //p1 p2
+
         mp[findDistance(p1,p2)]++;
         mp[findDistance(p1,p3)]++;
         mp[findDistance(p1,p4)]++;
@@ -16,6 +16,11 @@ public:
             if(a==0){
                 return false;
             }
+            if(b==4 || b==2){
+                continue;
+            } else {
+                return false;
+            }
         }
         return true;
     }
@@ -27,7 +32,6 @@ public:
         int bx=b[0];
         int by=b[1];
 
-        //distance
         double dist=sqrt(pow(abs(ax-bx),2)+pow(abs(ay-by),2));
         return dist;
     }
