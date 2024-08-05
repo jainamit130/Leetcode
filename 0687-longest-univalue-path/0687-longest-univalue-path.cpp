5d533f7d32d7dae6a30fbf84a61b071835ab93a4
Amit Choraria <jainamit130@gmail.com>
2024-08-05 18:39:27 +0530

    Time: 103 ms (23.39%), Space: 70.5 MB (6.14%) - LeetHub

--- 0687-longest-univalue-path/0687-longest-univalue-path.cpp
+++ 0687-longest-univalue-path/0687-longest-univalue-path.cpp
@@ -13,32 +13,29 @@ class Solution {
 public:
     int ans=0;
     int longestUnivaluePath(TreeNode* root) {
-        solve(root,INT_MAX);
-        return max(0,ans-1);
+        solve(root);
+        return ans;
     }
 
-    pair<int,int> solve(TreeNode* root,int val){
+    pair<int,int> solve(TreeNode* root){
         if(!root){
             return {INT_MAX,0};
         }
 
-        auto leftPair=solve(root->left,val);
-        auto rightPair=solve(root->right,val);
-        int count=1;
+        auto leftPair=solve(root->left);
+        auto rightPair=solve(root->right);
+        int count=0;
         if(leftPair.first==root->val){
-            count+=leftPair.second;
+            count=max(count,1+leftPair.second);
         }
         if(rightPair.first==root->val){
-            count+=rightPair.second;
+            count=max(count,1+rightPair.second);
+        }
+
+        if(rightPair.first==root->val && leftPair.first==root->val){
+            ans=max(ans,2+leftPair.second+rightPair.second);
         }
         ans=max(ans,count);
         return {root->val,count};
     }
 };
-
-
-//Values can be anything
-//Root can be Null
-//10000
-//Number of Nodes: 0 - 10000
-//
