6e794823d4b696c0f72a288587349a85f3781d52
Amit Choraria <jainamit130@gmail.com>
2024-08-05 14:43:32 +0530

    Time: 280 ms (68%), Space: 160.5 MB (64.25%) - LeetHub

--- 2925-maximum-score-after-applying-operations-on-a-tree/2925-maximum-score-after-applying-operations-on-a-tree.cpp
+++ 2925-maximum-score-after-applying-operations-on-a-tree/2925-maximum-score-after-applying-operations-on-a-tree.cpp
@@ -5,24 +5,23 @@ public:
         vector<vector<int>> adj(n);
         for (int i = 0; i < edges.size(); i++) {
             adj[edges[i][0]].push_back(edges[i][1]);
+            adj[edges[i][1]].push_back(edges[i][0]);
         }
         long long total=0;
         for(int i=0;i<n;i++){
             total+=values[i];
         }
-        return total-minValRem(adj, 0, values);
+        return total-minValRem(adj, 0, -1,values);
     }
 
-    long long minValRem(vector<vector<int>>& adj, int node, vector<int>& values) {
-        if(adj[node].size()==0){
-            return values[node];
-        }
-
+    long long minValRem(vector<vector<int>>& adj, int node, int parent, vector<int>& values) {
         long long val=values[node];
         long long sumChild=0;
         for(int i=0;i<adj[node].size();i++){
-            sumChild+=minValRem(adj, adj[node][i],values);
+            if(parent!=adj[node][i])
+                sumChild+=minValRem(adj, adj[node][i],node,values);
         }
+        if(sumChild==0) return val;
         return min(sumChild,val);
     }   
 };
