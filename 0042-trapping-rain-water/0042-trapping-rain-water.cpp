43cbeac33b792c01c7988c0bae9a4661ddafb823
Amit Choraria <jainamit130@gmail.com>
2024-08-08 22:10:13 +0530

    Time: 68 ms (5.05%), Space: 40.8 MB (5.01%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,51 +1,54 @@
-// class Solution {
-// public:
-//     int trap(vector<int>& height) {
-//         if(height.size()==1)
-//             return 0;
-//         int temp=INT_MIN;
-//         vector<pair<int,int>> leftrightheights(height.size(),{INT_MIN,INT_MIN});
-//         for(int i=0;i<height.size();i++){
-//             temp=max(height[i],temp);
-//             leftrightheights[i].first=temp;
-//         }
-//         temp=INT_MIN;
-//         for(int i=height.size()-1;i>=0;i--){
-//             temp=max(height[i],temp);
-//             leftrightheights[i].second=temp;
-//         }
-//         int ans=0;
-//         for(int i=0;i<height.size();i++){
-//             int collectWater=min(leftrightheights[i].first,leftrightheights[i].second)-height[i];
-//             collectWater=(collectWater<=0)?0:collectWater;
-//             ans+=collectWater;
-//         }
-//         return ans;
-//     }
-// };
-
 class Solution {
 public:
     int trap(vector<int>& height) {
-        if(height.size()==1)
-            return 0;
-        int maxL=height[0],maxR=height.back();
-        int i=0,j=height.size()-1;
-        int ans=0;
-        while(i<=j){
-            if(maxL<=maxR){
-                int t=maxL-height[i];
-                ans+=(t<=0)?0:t;
-                maxL=max(maxL,height[i]);
-                i++;
+        stack<vector<int>> st;
+        int n=height.size();
+        vector<vector<int>> nextMaxHeight(n,{-1,-1});
+        for(int i=height.size()-1;i>=0;i--){
+            if(height[i]==0){
+                continue;
             } else {
-                int t=maxR-height[j];
-                ans+=(t<=0)?0:t;
-                maxR=max(maxR,height[j]);
-                j--;
+                vector<int> nextGreater={-1,-1};
+                while(!st.empty() && st.top()[0]<height[i]){   
+                    if(st.top()[0]>nextGreater[0]){
+                        nextGreater=st.top();
+                    }
+                    st.pop();
+                }
+                if(!st.empty() && st.top()[0]>nextGreater[0]){
+                    nextGreater=st.top();
+                }
+                nextMaxHeight[i]=nextGreater;
+                st.push({height[i],i});
+            }
+        }
+        bool noSnowMode=false;
+        int endActivation=-1;
+        int ans=0;
+        for(int i=0;i<nextMaxHeight.size();){
+            if(nextMaxHeight[i][0]==-1){
+                if(i==endActivation){
+                    noSnowMode=false;
+                    endActivation=-1;
+                }
+                i++;
+                continue;
+            }
+            if(noSnowMode){
+                if(i==endActivation){
+                    noSnowMode=false;
+                    endActivation=-1;
+                } else {
+                    ans-=height[i];
+                    i++;
+                }
+            } else {
+                ans+=abs(nextMaxHeight[i][1]-i-1)*min(height[i],nextMaxHeight[i][0]);
+                noSnowMode=true;
+                endActivation=nextMaxHeight[i][1];
+                i++;
             }
         }
         return ans;
     }
 };
-
