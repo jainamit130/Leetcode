34365a996cc09dffc409d3d7ba9dd5006d0b4ed7
Amit Choraria <jainamit130@gmail.com>
2024-08-11 15:50:33 +0530

    Time: 278 ms (25%), Space: 58.7 MB (41.67%) - LeetHub

--- 3250-find-the-count-of-monotonic-pairs-i/3250-find-the-count-of-monotonic-pairs-i.cpp
+++ 3250-find-the-count-of-monotonic-pairs-i/3250-find-the-count-of-monotonic-pairs-i.cpp
@@ -2,26 +2,31 @@ class Solution {
 public:
     int mod=1e9+7;
     int countOfPairs(vector<int>& nums) {
-        vector<vector<vector<int>>> cache(nums.size()+1,vector<vector<int>>(52,vector<int>(52,-1)));
-        return solve(nums,0,0,51,cache); // nums, last element of arr1, last element of arr2
+        vector<vector<int>> cache(nums.size()+1,vector<int>(52,-1));
+        return solve(nums,0,0,cache); // nums, last element of arr1, last element of arr2
     }
 
     // arr1 is increasing and arr2 is decreasing
-    int solve(vector<int>& nums,int index,int arr1B,int arr2B,vector<vector<vector<int>>>& cache){
+    int solve(vector<int>& nums,int index,int arr1B,vector<vector<int>>& cache){
         if(index>=nums.size()){
             return 1;
         }
 
-        if(cache[index][arr1B][arr2B]!=-1){
-            return cache[index][arr1B][arr2B];
+        int arr2B=52;
+        if(index>0){
+            arr2B=nums[index-1]-arr1B;
         }
 
-        cache[index][arr1B][arr2B]=0;
+        if(cache[index][arr1B]!=-1){
+            return cache[index][arr1B];
+        }
+
+        cache[index][arr1B]=0;
         for(int i=0;i<=nums[index];i++){
             if(i>=arr1B && (nums[index]-i)<=arr2B)
-                cache[index][arr1B][arr2B]=(cache[index][arr1B][arr2B]+solve(nums,index+1,i,nums[index]-i,cache))%mod;
+                cache[index][arr1B]=(cache[index][arr1B]+solve(nums,index+1,i,cache))%mod;
         }
-        return cache[index][arr1B][arr2B];
+        return cache[index][arr1B];
     }
 };
 
