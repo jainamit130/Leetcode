b11d2b9ede61b06087040f92eb732f0e79e66000
Amit Choraria <jainamit130@gmail.com>
2024-08-12 13:47:19 +0530

    Time: 548 ms (41.67%), Space: 400.3 MB (33.33%) - LeetHub

--- 3251-find-the-count-of-monotonic-pairs-ii/3251-find-the-count-of-monotonic-pairs-ii.cpp
+++ 3251-find-the-count-of-monotonic-pairs-ii/3251-find-the-count-of-monotonic-pairs-ii.cpp
@@ -1,24 +1,132 @@
+// class Solution {
+// public:
+//     int mod=1e9+7;
+//     int countOfPairs(vector<int>& nums) {
+//         int n=nums.size();
+//         vector<vector<int>> cache(n+1,vector<int>(1001,-1)); 
+//         int ans=solve(nums,0,0,cache);
+//         return cache[0][0];
+//     }
+
+//     int solve(vector<int>& nums,int index,int arr1B,vector<vector<int>> & cache){
+//         if(index>=nums.size()){
+//             return 1;
+//         }
+
+//         int arr2B=1000;
+//         if(index>0){
+//             arr2B=nums[index-1]-arr1B;
+//         }
+
+//         if(cache[index][arr1B]!=-1){
+//             return cache[index][arr1B];
+//         }
+
+//         int ans=0;
+//         for(int newArr1B=arr1B;newArr1B<=nums[index];newArr1B++){
+//             int newArr2B=nums[index]-newArr1B;
+//             if(newArr2B<=arr2B){
+//                 ans+=solve(nums,index+1,newArr1B,cache)%mod;
+//                 ans=ans%mod;
+//             }
+//         }
+//         return cache[index][arr1B]=ans;
+//     }
+// };
+
+// class Solution {
+// public:
+//     int mod=1e9+7;
+//     int countOfPairs(vector<int>& nums) {
+//         int n=nums.size();
+//         vector<vector<int>> dp(n+1,vector<int>(1001,-1)); 
+//         vector<int> base(1001,1);
+//         dp[n]=base;
+//         for(int index=n-1;index>=0;index--){
+//             for(int arr1B=0;arr1B<1001;arr1B++){
+//                 int arr2B=1000;
+//                 if(index>0){
+//                     arr2B=nums[index-1]-arr1B;
+//                 }
+//                 int ans=0;
+//                 for(int newArr1B=arr1B;newArr1B<=nums[index];newArr1B++){
+//                     int newArr2B = nums[index]-newArr1B;
+//                     if(newArr2B<=arr2B){
+//                         ans+=dp[index+1][newArr1B];
+//                     }
+//                 }
+//                 dp[index][arr1B]=ans;
+//             }
+//         }
+//         return dp[0][0];
+//     }
+// };
+
+
+// class Solution {
+// public:
+//     int mod=1e9+7;
+//     int countOfPairs(vector<int>& nums) {
+//         int n=nums.size();
+//         vector<int> dp(1001,1); 
+//         for(int index=n-1;index>=0;index--){
+//             vector<int> nextDp(1001);
+//             vector<int> prefix(1002);
+//             prefix[1]=dp[0];
+//             for(int i=1;i<1001;i++){
+//                 prefix.push_back(prefix.back()+dp[i]);
+//             }
+//             for(int arr1B=0;arr1B<1001;arr1B++){
+//                 int arr2B=1000;
+//                 if(index>0){
+//                     arr2B=nums[index-1]-arr1B;
+//                 }
+//                 if(arr2B<0){
+//                     break;
+//                 }
+//                 int l=max(nums[index]-arr2B,nums[index]);
+//                 int r=nums[index];
+//                 if(l<=r){
+//                     nextDp[arr1B]=prefix[r+1]-prefix[l];
+//                 }
+//             }
+//             dp=nextDp;
+//         }
+//         return dp[0];
+//     }
+// };
+
 class Solution {
 public:
-    int mod=1e9+7;
+    int mod = 1e9 + 7;
     int countOfPairs(vector<int>& nums) {
-        int n=nums.size();
-        vector<vector<int>> dp(n+1,vector<int>(2000,0));
-        vector<int> base(2000,1);
-        dp[n]=base;
-        for(int index=n-1;index>=0;index--){
-            for(int arr1B=0;arr1B<=nums[index];arr1B++){
-                int arr2B=(index>0)?nums[index-1]-arr1B:2000;
-                int ans=0;
-                for(int i=arr1B;i<=nums[index];i++){
-                    if(nums[index]-i<=arr2B){
-                        ans=ans%mod+dp[index+1][i]%mod;
-                    }
-                }
-                dp[index][arr1B]=ans;
+        const int N = 1001;  
+        int n = nums.size();
+        vector<int> dp(N, 1);  
+        
+        for (int index = n - 1; index >= 0; index--) {
+            vector<int> newDp(N, 0); 
+            vector<int> prefix(N, 0); 
+            
+            prefix[0] = dp[0];
+            for (int j = 1; j < N; j++) 
+                prefix[j] = (prefix[j - 1] + dp[j]) % mod;
+            
+            for (int arr1B = 0; arr1B <= 1000; arr1B++) {
+                int arr2B = (index > 0) ? nums[index - 1] - arr1B : 1000;
+                
+                if (arr2B < 0) 
+                    break;
+                
+                int l = max(arr1B, nums[index] - arr2B);
+                int r = nums[index];
+                
+                if (l <= r)
+                    newDp[arr1B] = (prefix[r] - (l > 0 ? prefix[l - 1] : 0) + mod) % mod;
             }
+            dp = newDp;
         }
 
-        return dp[0][0]; 
+        return dp[0];
     }
 };
