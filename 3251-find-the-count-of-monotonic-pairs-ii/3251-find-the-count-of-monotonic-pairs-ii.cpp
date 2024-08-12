396022d67bfe36a69eeab295d297c9aa70abd000
Amit Choraria <jainamit130@gmail.com>
2024-08-12 13:49:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3251-find-the-count-of-monotonic-pairs-ii/3251-find-the-count-of-monotonic-pairs-ii.cpp
+++ 3251-find-the-count-of-monotonic-pairs-ii/3251-find-the-count-of-monotonic-pairs-ii.cpp
@@ -63,70 +63,70 @@
 // };
 
 
-// class Solution {
-// public:
-//     int mod=1e9+7;
-//     int countOfPairs(vector<int>& nums) {
-//         int n=nums.size();
-//         vector<int> dp(1001,1); 
-//         for(int index=n-1;index>=0;index--){
-//             vector<int> nextDp(1001);
-//             vector<int> prefix(1002);
-//             prefix[1]=dp[0];
-//             for(int i=1;i<1001;i++){
-//                 prefix.push_back(prefix.back()+dp[i]);
-//             }
-//             for(int arr1B=0;arr1B<1001;arr1B++){
-//                 int arr2B=1000;
-//                 if(index>0){
-//                     arr2B=nums[index-1]-arr1B;
-//                 }
-//                 if(arr2B<0){
-//                     break;
-//                 }
-//                 int l=max(nums[index]-arr2B,nums[index]);
-//                 int r=nums[index];
-//                 if(l<=r){
-//                     nextDp[arr1B]=prefix[r+1]-prefix[l];
-//                 }
-//             }
-//             dp=nextDp;
-//         }
-//         return dp[0];
-//     }
-// };
-
 class Solution {
 public:
-    int mod = 1e9 + 7;
+    int mod=1e9+7;
     int countOfPairs(vector<int>& nums) {
-        const int N = 1001;  
-        int n = nums.size();
-        vector<int> dp(N, 1);  
-        
-        for (int index = n - 1; index >= 0; index--) {
-            vector<int> newDp(N, 0); 
-            vector<int> prefix(N, 0); 
-            
-            prefix[0] = dp[0];
-            for (int j = 1; j < N; j++) 
-                prefix[j] = (prefix[j - 1] + dp[j]) % mod;
-            
-            for (int arr1B = 0; arr1B <= 1000; arr1B++) {
-                int arr2B = (index > 0) ? nums[index - 1] - arr1B : 1000;
-                
-                if (arr2B < 0) 
-                    break;
-                
-                int l = max(arr1B, nums[index] - arr2B);
-                int r = nums[index];
-                
-                if (l <= r)
-                    newDp[arr1B] = (prefix[r] - (l > 0 ? prefix[l - 1] : 0) + mod) % mod;
+        int n=nums.size();
+        vector<int> dp(1001,1); 
+        for(int index=n-1;index>=0;index--){
+            vector<int> nextDp(1001);
+            vector<int> prefix;
+            prefix.push_back(0);
+            for(int i=0;i<1001;i++){
+                prefix.push_back((prefix.back()+dp[i])%mod);
             }
-            dp = newDp;
+            for(int arr1B=0;arr1B<1001;arr1B++){
+                int arr2B=1000;
+                if(index>0){
+                    arr2B=nums[index-1]-arr1B;
+                }
+                if(arr2B<0){
+                    break;
+                }
+                int l=max(nums[index]-arr2B,arr1B);
+                int r=nums[index];
+                if(l<=r){
+                    nextDp[arr1B]=(prefix[r+1]%mod-prefix[l]%mod)%mod;
+                }
+            }
+            dp=nextDp;
         }
-
         return dp[0];
     }
 };
+
+// class Solution {
+// public:
+//     int mod = 1e9 + 7;
+//     int countOfPairs(vector<int>& nums) {
+//         const int N = 1001;  
+//         int n = nums.size();
+//         vector<int> dp(N, 1);  
+        
+//         for (int index = n - 1; index >= 0; index--) {
+//             vector<int> newDp(N, 0); 
+//             vector<int> prefix(N, 0); 
+            
+//             prefix[0] = dp[0];
+//             for (int j = 1; j < N; j++) 
+//                 prefix[j] = (prefix[j - 1] + dp[j]) % mod;
+            
+//             for (int arr1B = 0; arr1B <= 1000; arr1B++) {
+//                 int arr2B = (index > 0) ? nums[index - 1] - arr1B : 1000;
+                
+//                 if (arr2B < 0) 
+//                     break;
+                
+//                 int l = max(arr1B, nums[index] - arr2B);
+//                 int r = nums[index];
+                
+//                 if (l <= r)
+//                     newDp[arr1B] = (prefix[r] - (l > 0 ? prefix[l - 1] : 0) + mod) % mod;
+//             }
+//             dp = newDp;
+//         }
+
+//         return dp[0];
+//     }
+// };
