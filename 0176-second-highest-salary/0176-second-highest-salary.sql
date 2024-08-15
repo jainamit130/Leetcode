7c0c703a7feb191a703251aee7d0059213ee2d2e
Amit Choraria <jainamit130@gmail.com>
2024-08-15 11:10:13 +0530

    Time: 227 ms (80.76%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-Select max(salary) as SecondHighestSalary  from Employee where salary < (select max(salary) from Employee);
+select max(salary) as SecondHighestSalary from Employee where salary<(Select max(salary) from Employee);
