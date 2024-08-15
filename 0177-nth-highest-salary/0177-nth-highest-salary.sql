43525c36161fe715df43ca7d55457db4f6903eb7
Amit Choraria <jainamit130@gmail.com>
2024-08-15 11:31:23 +0530

    Time: 458 ms (31.54%), Space: 0B (100%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -1,9 +1,7 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
-SET N = N-1;
-  RETURN (
-      SELECT DISTINCT(salary) from Employee order by salary DESC
-      LIMIT 1 OFFSET N
-      
-  );
+   DECLARE nth_salary INT;
+   with t as (Select Salary,ROW_NUMBER() OVER (ORDER BY SALARY DESC) as row_num From (Select Distinct Salary from Employee) AS DistinctSalaries)
+   Select salary into nth_salary From t where row_num=N;
+  RETURN nth_salary;
 END
