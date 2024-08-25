8c451f86db2e098ce19e0141e23df213d168eea6
Amit Choraria <jainamit130@gmail.com>
2024-08-25 18:13:07 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3266-final-array-state-after-k-multiplication-operations-ii/3266-final-array-state-after-k-multiplication-operations-ii.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <cmath>
+using namespace std;
+
+long long modExp(int base, int exp, int mod) {
+    if (exp == 0) {
+        return 1;  
+    }
+    
+    long long half = modExp(base, exp / 2, mod);
+    half = (half * half) % mod;
+    
+    if (exp % 2 != 0) {
+        half = (half * base) % mod;  
+    }
+    
+    return half;
+}
+
+
+class Solution {
+public:
+    vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
+        if (multiplier == 1) {
+            return nums;  
+        }
+
+        int mod = 1e9 + 7;
+        int m = *max_element(nums.begin(), nums.end());
+        int n = nums.size();
+        
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minHeap;
+
+        for (int i = 0; i < n; i++) {
+            minHeap.push({nums[i], i});
+        }
+
+        while (k > 0 && minHeap.top().first * multiplier <= m) {
+            auto [value, index] = minHeap.top();
+            minHeap.pop();
+            value = (value * multiplier) % mod;
+            minHeap.push({value, index});
+            k--;
+        }
+
+        vector<pair<int, int>> vals;
+        while (!minHeap.empty()) {
+            vals.push_back(minHeap.top());
+            minHeap.pop();
+        }
+
+        int q = k / n;
+        int remK = k % n;
+
+        for (int i = 0; i < n; i++) {
+            vals[i].first = (long long)vals[i].first * modExp(multiplier, q, mod) % mod;
+        }
+
+        for (int i = 0; i < remK; i++) {
+            vals[i].first = (long long)vals[i].first * multiplier % mod;
+        }
+
+        vector<int> result(n);
+        for (auto& [value, index] : vals) {
+            result[index] = value;
+        }
+
+        return result;
+    }
+};
