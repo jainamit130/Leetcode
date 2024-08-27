76120ff4b7effabf1348679071bd1a0bb6d6891d
Amit Choraria <jainamit130@gmail.com>
2024-08-27 09:06:40 +0530

    Time: 134 ms (52.98%), Space: 71.3 MB (38.46%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
-    using pii=pair<double,int>;
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
-        vector<vector<pii>> adj(n);
+        vector<vector<pair<int,double>>> adj(n);
         for(int i=0;i<edges.size();i++){
-            adj[edges[i][0]].push_back({succProb[i],edges[i][1]});
-            adj[edges[i][1]].push_back({succProb[i],edges[i][0]});
+            adj[edges[i][0]].push_back({edges[i][1],succProb[i]});
+            adj[edges[i][1]].push_back({edges[i][0],succProb[i]});
         }
-        priority_queue<pii> pq;
-        pq.push({1,start_node});
-        vector<double> probabilities(n,1);
+
         vector<int> visited(n);
+        priority_queue<pair<double,int>> pq;
+        pq.push({1,start_node});
         while(!pq.empty()){
             int node=pq.top().second;
-            double prob=pq.top().first;
+            double currProb=pq.top().first;
             pq.pop();
-            if(visited[node]){
-                continue;
-            }
             visited[node]=1;
-            probabilities[node]*=prob;
+            if(node==end_node){
+                return currProb;
+            }
             for(int i=0;i<adj[node].size();i++){
-                pq.push({adj[node][i].first*prob,adj[node][i].second});
+                if(visited[adj[node][i].first]){
+                    continue;
+                }
+                pq.push({currProb*adj[node][i].second,adj[node][i].first});
             }
         }
-        if(probabilities[end_node]==1){
-            return 0;
-        }
-        return probabilities[end_node];
+        return 0;
     }
 };
