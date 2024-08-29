70518311b7fad5bc592a5e8ac740f6478e90cd2a
Amit Choraria <jainamit130@gmail.com>
2024-08-29 18:26:38 +0530

    Time: 68 ms (30.83%), Space: 42.7 MB (5.07%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,51 +1,67 @@
 class Solution {
 public:
+    void dfs(int node, const unordered_map<int, unordered_set<int>>& adj, unordered_set<int>& visited, vector<int>& component) {
+        stack<int> s;
+        s.push(node);
+        while (!s.empty()) {
+            int curr = s.top();
+            s.pop();
+            if (visited.find(curr) == visited.end()) {
+                visited.insert(curr);
+                component.push_back(curr);
+                for (int neighbor : adj.at(curr)) {
+                    if (visited.find(neighbor) == visited.end()) {
+                        s.push(neighbor);
+                    }
+                }
+            }
+        }
+    }
+
     int removeStones(vector<vector<int>>& stones) {
-        int n=stones.size();
-        unordered_map<int,vector<int>> row;
-        unordered_map<int,vector<int>> col;
-        for(int i=0;i<n;i++){
+        int n = stones.size();
+        unordered_map<int, vector<int>> row;
+        unordered_map<int, vector<int>> col;
+
+        for (int i = 0; i < n; i++) {
             row[stones[i][0]].push_back(i);
             col[stones[i][1]].push_back(i);
         }
-        vector<vector<int>> indegrees(n,{0,0});
-        unordered_map<int,unordered_set<int>> adj;
-        for(int k=0;k<=1;k++){
-            unordered_map<int,vector<int>> mp;
-            if(k==0){
-                mp=row;
+
+        unordered_map<int, unordered_set<int>> adj;
+        for (int k = 0; k <= 1; k++) {
+            unordered_map<int, vector<int>> mp;
+            if (k == 0) {
+                mp = row;
             } else {
-                mp=col;
+                mp = col;
             }
-            for(auto [a,b]:mp){
-                for(int i=0;i<b.size()-1;i++){
-                    for(int j=i+1;j<b.size();j++){
+            for (auto [a, b] : mp) {
+                for (int i = 0; i < b.size() - 1; i++) {
+                    for (int j = i + 1; j < b.size(); j++) {
                         adj[b[i]].insert(b[j]);
                         adj[b[j]].insert(b[i]);
-                        indegrees[b[i]]={indegrees[b[i]][0]+1,b[i]};
-                        indegrees[b[j]]={indegrees[b[j]][0]+1,b[j]};
                     }
                 }
-            }   
+            }
         }
-        int ans=0;
-        while(indegrees.size()>0){
-            sort(indegrees.begin(),indegrees.end(),greater<vector<int>>());
-            while(indegrees.size()>0 && indegrees.back()[0]==0){
-                indegrees.pop_back();
+
+        unordered_set<int> visited;
+        vector<int> components_sizes;
+        
+        for (const auto& [node, _] : adj) {
+            if (visited.find(node) == visited.end()) {
+                vector<int> component;
+                dfs(node, adj, visited, component);
+                components_sizes.push_back(component.size());
             }
-            if(indegrees.size()==0){
-                break;
-            }
-            int node=indegrees.back()[1];
-            ans++;
-            indegrees.pop_back();
-            for(int i=0;i<indegrees.size();i++){
-                if(adj[node].find(indegrees[i][1])!=adj[node].end()){
-                    indegrees[i][0]--;
-                }
-            }
-        }    
-        return ans; 
+        }
+
+        int total_removals = 0;
+        for (int size : components_sizes) {
+            total_removals += size - 1; 
+        }
+
+        return total_removals;
     }
 };
