0789e9c1beb7c146bdb114e4fd05d369c664a81f
Amit Choraria <jainamit130@gmail.com>
2024-09-08 18:37:46 +0530

    Time: 216 ms (20%), Space: 171.5 MB (80%) - LeetHub

--- 3282-reach-end-of-array-with-max-score/3282-reach-end-of-array-with-max-score.cpp
+++ 3282-reach-end-of-array-with-max-score/3282-reach-end-of-array-with-max-score.cpp
@@ -1,29 +1,15 @@
 class Solution {
 public:
     long long findMaximumScore(vector<int>& nums) {
-        stack<pair<int,int>> st;
-        int n=nums.size();
-        vector<int> nextGreaterIndex(n);
-        nextGreaterIndex[n-1]=n-1;
-        st.push({n-1,nums.back()});
-        int i=n-2;
-        while(i>=0){
-            while(!st.empty() && st.top().second<nums[i]){
-                st.pop();
-            }
-            if(st.empty()){
-                nextGreaterIndex[i]=n-1;
-            } else {
-                nextGreaterIndex[i]=st.top().first;
-            }
-            st.push({i,nums[i]});
-            i--;
-        }
+        int last=0;
         long long ans=0;
-        i=0;
-        while(i<n-1){
-            ans+=(long long)nums[i]*(long long)(nextGreaterIndex[i]-i);
-            i=nextGreaterIndex[i];
+        for(int i=1;i<nums.size() && last<nums.size()-1;i++)
+        {
+            ans+=nums[last];
+            if(nums[last]<nums[i])
+            {
+                last=i;
+            }
         }
         return ans;
     }
