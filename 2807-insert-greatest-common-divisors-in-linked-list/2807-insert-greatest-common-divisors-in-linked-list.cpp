58c43f5bfd54cd6a3c0724b4edfaa57f3eecf9c7
Amit Choraria <jainamit130@gmail.com>
2024-09-10 13:31:47 +0530

    Time: 55 ms (17.53%), Space: 35.5 MB (82.75%) - LeetHub

--- 2807-insert-greatest-common-divisors-in-linked-list/2807-insert-greatest-common-divisors-in-linked-list.cpp
+++ 2807-insert-greatest-common-divisors-in-linked-list/2807-insert-greatest-common-divisors-in-linked-list.cpp
@@ -1,29 +1,10 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* insertGreatestCommonDivisors(ListNode* head) {
         ListNode* ptr = head;
         while(ptr && ptr->next){
-            // is middle?
                 int num1= ptr->val;
                 int num2= ptr->next->val;
-                // find GCD of num1 and num2
-                // What is GCD?
-                /*
-                    18 =  
-                    10 =
-                    10 -> 1 which ever divides both
-                
-                */
                 int gcd = min(num1,num2);
                 for(;gcd>=1;gcd--){
                     if(num1%gcd==0 && num2%gcd==0){
