c497fefb8c67422d211e704fea853d0819e9d672
Amit Choraria <jainamit130@gmail.com>
2024-09-15 13:58:37 +0530

    Time: 794 ms (36.84%), Space: 584.2 MB (15.79%) - LeetHub

--- 3287-find-the-maximum-sequence-value-of-array/3287-find-the-maximum-sequence-value-of-array.cpp
+++ 3287-find-the-maximum-sequence-value-of-array/3287-find-the-maximum-sequence-value-of-array.cpp
@@ -1,62 +1,41 @@
 class Solution {
 public:
     int maxValue(vector<int>& nums, int k) {
-        int n = nums.size();
+        int n=nums.size();
+        vector<vector<vector<int>>> left(n,vector<vector<int>>(k+1,vector<int>(129,0)));
+        vector<vector<vector<int>>> right(n,vector<vector<int>>(k+1,vector<int>(129,0)));
 
-        vector<vector<vector<bool>>> left(n, vector<vector<bool>>(k + 1, vector<bool>(128, false)));
-        vector<vector<vector<bool>>> right(n, vector<vector<bool>>(k + 1, vector<bool>(128, false)));
-
-        // Initialize the bitmask arrays
-        left[0][0][0] = true;
-        left[0][1][nums[0]] = true;
-
-        // Fill left DP table with OR results using bitmasking
-        for (int i = 1; i < n - k; ++i) {
-            left[i][0][0] = true;
-            for (int j = 1; j <= k; ++j) {
-                for (int v = 0; v < 128; ++v) {
-                    // Carry over previous OR results
-                    if (left[i - 1][j][v]) left[i][j][v] = true;
-
-                    // Compute new OR results by including nums[i]
-                    if (left[i - 1][j - 1][v]) {
-                        left[i][j][v | nums[i]] = true;
+        left[0][1][nums[0]]=1;
+        for(int i=1;i<n;i++){
+            left[i][1][nums[i]]=1;
+            for(int subSize = 1; subSize<=k; subSize++){
+                for(int vals = 0;vals<=128;vals++){
+                    if(left[i-1][subSize][vals]){
+                        left[i][subSize][vals]=1;
+                    }
+                    if(left[i-1][subSize-1][vals]){
+                        left[i][subSize][vals|nums[i]]=1;
                     }
                 }
             }
         }
 
-        // Initialize right DP table with OR results
-        right[n - 1][0][0] = true;
-        right[n - 1][1][nums[n - 1]] = true;
-
-        int result = 0;
-
-        // Special case for k = 1
-        if (k == 1) {
-            for (int l = 0; l < 128; ++l) {
-                if (left[n - 2][k][l]) {
-                    result = max(result, l ^ nums[n - 1]);
-                }
-            }
-        }
-
-        // Fill right DP table and compute the max XOR
-        for (int i = n - 2; i >= k; --i) {
-            right[i][0][0] = true;
-            for (int j = 1; j <= k; ++j) {
-                for (int v = 0; v < 128; ++v) {
-                    // Carry over previous OR results
-                    if (right[i + 1][j][v]) right[i][j][v] = true;
-
-                    // Compute new OR results by including nums[i]
-                    if (right[i + 1][j - 1][v]) {
-                        right[i][j][v | nums[i]] = true;
+        right[n-1][1][nums[n-1]]=1;
+        for(int i=n-2;i>=0;i--){
+            right[i][1][nums[i]]=1;
+            for(int subSize = 1;subSize<=k ; subSize++){
+                for(int vals=0;vals<=128;vals++){
+                    if(right[i+1][subSize][vals]){
+                        right[i][subSize][vals]=1;
+                    }
+                    if(right[i+1][subSize-1][vals]){
+                        right[i][subSize][vals|nums[i]]=1;
                     }
                 }
             }
         }
 
+        int result=0;
         for(int i=1;i<n;i++){
             for (int g = 0; g < 128; ++g) {
                 if(left[i-1][k][g]){
@@ -71,3 +50,29 @@ public:
         return result;
     }
 };
+
+/*
+
+
+log2 (2^7) = 7
+
+k = 2
+
+    4      2      5         6           7
+0   0      0      0         0           0       
+1   4      4,2    4,2,5     4,2,5,6    4,2,5,6,7
+2          2,6    2,6,5,7   2,6,5,7     2,6,5,7
+
+
+4      2      5      6        7
+
+5,6,7   6,7     7       7
+
+            
+
+
+
+
+
+
+*/
