7c63fefb4a5bb3d50e158407e3fc6dfcf509e5e2
Amit Choraria <jainamit130@gmail.com>
2024-09-15 15:14:42 +0530

    Time: 1939 ms (5.39%), Space: 489.7 MB (5.42%) - LeetHub

--- 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
@@ -8,26 +8,45 @@ public:
             return lhs[0]<rhs[0];
         });
 
-        return findLis(envelopes);
+        // return findLis(envelopes);
+        return findLisBS(envelopes);
     }
 
-    int findLis(vector<vector<int>>& envelopes){
+    int findLisBS(vector<vector<int>>& envelopes){
         int n=envelopes.size();
-        vector<int> dp(n);
-        dp[n-1]=1;
-        int ans=1;
-        for(int i=n-2;i>=0;i--){
-            int subAns=1;
-            for(int j=i+1;j<n;j++){
-                if(envelopes[j][1]>envelopes[i][1]){
-                    subAns=max(subAns,dp[j]+1);
-                }
+        vector<int> sorted;
+        sorted.push_back(envelopes[0][1]);
+        for(int i=1;i<n;i++){
+            auto itr=lower_bound(sorted.begin(),sorted.end(),envelopes[i][1]);
+            if(itr==sorted.end()){
+                sorted.push_back(envelopes[i][1]);
+            } else {
+                int index=itr-sorted.begin();
+                sorted[index]=envelopes[i][1];
             }
-            dp[i]=subAns;
-            ans=max(ans,subAns);
         }
-        return ans;
+        return sorted.size();
     }
+
+    // int findLis(vector<vector<int>>& envelopes){
+    //     int n=envelopes.size();
+    //     vector<int> dp(n);
+    //     dp[n-1]=1;
+    //     int ans=1;
+    //     for(int i=n-2;i>=0;i--){
+    //         int subAns=1;
+    //         for(int j=i+1;j<n;j++){
+    //             if(envelopes[j][1]>envelopes[i][1]){
+    //                 subAns=max(subAns,dp[j]+1);
+    //             }
+    //         }
+    //         dp[i]=subAns;
+    //         ans=max(ans,subAns);
+    //     }
+    //     return ans;
+    // }
+
+    
 };
 
 
