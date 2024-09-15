422aa5d38b7b533da0eeb741d1db372909db779d
Amit Choraria <jainamit130@gmail.com>
2024-09-15 23:52:23 +0530

    Time: 412 ms (90.91%), Space: 147.2 MB (100%) - LeetHub

--- 3288-length-of-the-longest-increasing-path/3288-length-of-the-longest-increasing-path.cpp
+++ 3288-length-of-the-longest-increasing-path/3288-length-of-the-longest-increasing-path.cpp
@@ -1,40 +1,43 @@
 class Solution {
 public:
     int maxPathLength(vector<vector<int>>& coordinates, int k) {
-        int n = coordinates.size();
-        int startX = coordinates[k][0];
-        int startY = coordinates[k][1];
-        
-        sort(coordinates.begin(), coordinates.end(), [](const vector<int>& lhs, const vector<int>& rhs) {
-            if (lhs[0] == rhs[0]) return lhs[1] > rhs[1];
-            return lhs[0] < rhs[0];
+        int n=coordinates.size();
+
+        int a=coordinates[k][0];
+        int b=coordinates[k][1];
+
+        sort(coordinates.begin(),coordinates.end(),[](const vector<int>& lhs, const vector<int>& rhs){
+            if(lhs[0]==rhs[0]){
+                return lhs[1]>rhs[1];
+            }
+            return lhs[0]<rhs[0];
         });
 
-        int ans = 0;
-        vector<int> lowerLIS, upperLIS;
-        
-        for (int i = 0; i < n; i++) {
-            if (coordinates[i][0] == startX && coordinates[i][1] == startY) continue;
-            
-            if (coordinates[i][0] < startX && coordinates[i][1] < startY) {
-                updateLIS(lowerLIS, coordinates[i][1]);
-            } else if (coordinates[i][0] > startX && coordinates[i][1] > startY) {
-                updateLIS(upperLIS, coordinates[i][1]);
+        vector<int> leftLis;
+        vector<int> rightLis;
+
+        for(int i=0;i<n;i++){
+            if(a==coordinates[i][0] || b==coordinates[i][1]) continue;
+
+            if(coordinates[i][0]<a && coordinates[i][1]<b){
+                updateLis(leftLis,coordinates[i][1]);
+            }
+
+            if(coordinates[i][0]>a && coordinates[i][1]>b){
+                updateLis(rightLis,coordinates[i][1]);
             }
         }
 
-        ans += lowerLIS.size();  
-        ans += upperLIS.size();  
-        
-        return ans + 1;  
+        return leftLis.size()+1+rightLis.size();
     }
 
-    void updateLIS(vector<int>& lis, int value) {
-        auto it = lower_bound(lis.begin(), lis.end(), value);
-        if (it == lis.end()) {
-            lis.push_back(value);
+    void updateLis(vector<int>& lis,int val){
+        auto it = lower_bound(lis.begin(),lis.end(),val);
+        if(it==lis.end()){
+            lis.push_back(val);
         } else {
-            *it = value;
+            *it=val;
         }
+        return;
     }
 };
