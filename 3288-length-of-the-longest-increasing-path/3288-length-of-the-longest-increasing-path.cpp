499fb4eaf649c9bc6e4a5c1b1668a0a802dc3d4f
Amit Choraria <jainamit130@gmail.com>
2024-09-15 23:50:22 +0530

    Time: 393 ms (100%), Space: 147.1 MB (100%) - LeetHub

--- 3288-length-of-the-longest-increasing-path/3288-length-of-the-longest-increasing-path.cpp
+++ 3288-length-of-the-longest-increasing-path/3288-length-of-the-longest-increasing-path.cpp
@@ -5,46 +5,36 @@ public:
         int startX = coordinates[k][0];
         int startY = coordinates[k][1];
         
-        sort(coordinates.begin(), coordinates.end(), [](auto lhs, auto rhs) {
-            if(lhs[0] == rhs[0]) {
-                return lhs[1] > rhs[1]; 
-            }
-            return lhs[0] < rhs[0]; 
+        sort(coordinates.begin(), coordinates.end(), [](const vector<int>& lhs, const vector<int>& rhs) {
+            if (lhs[0] == rhs[0]) return lhs[1] > rhs[1];
+            return lhs[0] < rhs[0];
         });
-        
+
         int ans = 0;
-        vector<vector<int>> lowerHalf, upperHalf;
+        vector<int> lowerLIS, upperLIS;
         
         for (int i = 0; i < n; i++) {
             if (coordinates[i][0] == startX && coordinates[i][1] == startY) continue;
+            
             if (coordinates[i][0] < startX && coordinates[i][1] < startY) {
-                lowerHalf.push_back({coordinates[i][0], coordinates[i][1]});
+                updateLIS(lowerLIS, coordinates[i][1]);
             } else if (coordinates[i][0] > startX && coordinates[i][1] > startY) {
-                upperHalf.push_back({coordinates[i][0], coordinates[i][1]});
+                updateLIS(upperLIS, coordinates[i][1]);
             }
         }
+
+        ans += lowerLIS.size();  
+        ans += upperLIS.size();  
         
-        ans += findLisBS(lowerHalf);
-        ans += findLisBS(upperHalf);
-        
-        return ans + 1; 
+        return ans + 1;  
     }
 
-    int findLisBS(vector<vector<int>>& coordinates) {
-        if (coordinates.empty()) return 0;
-        
-        vector<int> sortedLIS;
-        sortedLIS.push_back(coordinates[0][1]);
-        
-        for (int i = 1; i < coordinates.size(); i++) {
-            auto it = lower_bound(sortedLIS.begin(), sortedLIS.end(), coordinates[i][1]);
-            if (it == sortedLIS.end()) {
-                sortedLIS.push_back(coordinates[i][1]);
-            } else {
-                *it = coordinates[i][1]; 
-            }
+    void updateLIS(vector<int>& lis, int value) {
+        auto it = lower_bound(lis.begin(), lis.end(), value);
+        if (it == lis.end()) {
+            lis.push_back(value);
+        } else {
+            *it = value;
         }
-        
-        return sortedLIS.size(); 
     }
 };
