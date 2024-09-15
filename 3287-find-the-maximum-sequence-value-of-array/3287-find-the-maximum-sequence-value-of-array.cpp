ece206e8f59d6d0a64f82eb930b8f06572a1834f
Amit Choraria <jainamit130@gmail.com>
2024-09-16 00:13:21 +0530

    Time: 783 ms (36.84%), Space: 584.2 MB (15.79%) - LeetHub

--- 3287-find-the-maximum-sequence-value-of-array/3287-find-the-maximum-sequence-value-of-array.cpp
+++ 3287-find-the-maximum-sequence-value-of-array/3287-find-the-maximum-sequence-value-of-array.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     int maxValue(vector<int>& nums, int k) {
-        int n=nums.size();
+        int n= nums.size();
         vector<vector<vector<int>>> left(n,vector<vector<int>>(k+1,vector<int>(129,0)));
         vector<vector<vector<int>>> right(n,vector<vector<int>>(k+1,vector<int>(129,0)));
-
+        
         left[0][1][nums[0]]=1;
         for(int i=1;i<n;i++){
             left[i][1][nums[i]]=1;
-            for(int subSize = 1; subSize<=k; subSize++){
-                for(int vals = 0;vals<=128;vals++){
-                    if(left[i-1][subSize][vals]){
-                        left[i][subSize][vals]=1;
+            for(int subsize = 1;subsize <=k; subsize++){
+                for(int vals=0; vals<=128; vals++){
+                    if(left[i-1][subsize][vals]){
+                        left[i][subsize][vals]=1;
                     }
-                    if(left[i-1][subSize-1][vals]){
-                        left[i][subSize][vals|nums[i]]=1;
+
+                    if(left[i-1][subsize-1][vals]){
+                        left[i][subsize][vals|nums[i]]=1;
                     }
                 }
             }
@@ -23,52 +24,41 @@ public:
         right[n-1][1][nums[n-1]]=1;
         for(int i=n-2;i>=0;i--){
             right[i][1][nums[i]]=1;
-            for(int subSize = 1;subSize<=k ; subSize++){
-                for(int vals=0;vals<=128;vals++){
-                    if(right[i+1][subSize][vals]){
-                        right[i][subSize][vals]=1;
+            for(int subsize = 1;subsize <=k; subsize++){
+                for(int vals=0; vals<=128; vals++){
+                    if(right[i+1][subsize][vals]){
+                        right[i][subsize][vals]=1;
                     }
-                    if(right[i+1][subSize-1][vals]){
-                        right[i][subSize][vals|nums[i]]=1;
+
+                    if(right[i+1][subsize-1][vals]){
+                        right[i][subsize][vals|nums[i]]=1;
                     }
                 }
             }
         }
 
         int result=0;
-        for(int i=1;i<n;i++){
-            for (int g = 0; g < 128; ++g) {
-                if(left[i-1][k][g]){
-                    for (int v = 0; v < 128; ++v) {
-                        if(right[i][k][v])
-                            result=max(result,g^v);
+        for(int i=0;i<n-1;i++){
+            for(int vals1=0;vals1<=128;vals1++){
+                if(left[i][k][vals1]){
+                    for(int vals2=0;vals2<=128;vals2++){
+                        if(right[i+1][k][vals2]){
+                            result=max(result,vals1^vals2);
+                        }
                     }
                 }
             }
         }
-
         return result;
     }
 };
 
+
+
+
+
 /*
 
-
-log2 (2^7) = 7
-
-k = 2
-
-    4      2      5         6           7
-0   0      0      0         0           0       
-1   4      4,2    4,2,5     4,2,5,6    4,2,5,6,7
-2          2,6    2,6,5,7   2,6,5,7     2,6,5,7
-
-
-4      2      5      6        7
-
-5,6,7   6,7     7       7
-
-            
 
 
 
