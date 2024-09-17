1647c3d3d6e1281a44838d84237d6978d2e5981c
Amit Choraria <jainamit130@gmail.com>
2024-09-17 23:28:21 +0530

    Time: 5 ms (15.91%), Space: 9 MB (45.08%) - LeetHub

---
+++ 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<string> uncommonFromSentences(string s1, string s2) {
+        unordered_map<string,int> mp1;
+        string word="";
+        for(int i=0;i<=s1.length();i++){
+            if(i==s1.length() || s1[i]==' '){
+                mp1[word]++;
+                word="";
+            } else {
+                word+=s1[i];
+            }
+        }
+        word="";
+        unordered_map<string,int> mp2;
+        vector<string> ans;
+        for(int i=0;i<=s2.length();i++){
+            if(i==s2.length() || s2[i]==' '){
+                mp2[word]++;
+                word="";
+            } else {
+                word+=s2[i];
+            }
+        }
+
+        for(auto [w,c]:mp1){
+            if(c==1 && mp2.find(w)==mp2.end()){
+                ans.push_back(w);
+            }
+        }
+
+        for(auto [w,c]:mp2){
+            if(c==1 && mp1.find(w)==mp1.end()){
+                ans.push_back(w);
+            }
+        }
+        return ans;
+    }
+};
