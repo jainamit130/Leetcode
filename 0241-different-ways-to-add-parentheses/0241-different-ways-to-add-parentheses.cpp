397dc45b1c071818e00138fc500946ab52e466a8
Amit Choraria <jainamit130@gmail.com>
2024-09-19 15:42:31 +0530

    Time: 4 ms (46.04%), Space: 14.4 MB (8.2%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.cpp
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.cpp
@@ -1,18 +1,48 @@
 class Solution {
 public:
-    vector<int> ans;
+    unordered_set<char> st = {'*', '+', '-'};
     vector<int> diffWaysToCompute(string expression) {
-        solve(expression,0,index,0);
+        return solve(expression, 0, expression.length() - 1);
+    }
+
+    vector<int> solve(string expression, int start, int end) {
+        if (start > end) {
+            return {};
+        }
+        vector<int> ans;
+        bool flag=0;
+        int num=0;
+        for (int i = start; i <= end; i++) {
+            if (st.find(expression[i]) != st.end()) {
+                flag=1;
+                vector<int> left = solve(expression, start, i - 1);
+                vector<int> right = solve(expression, i + 1, end);
+                if (expression[i] == '*') {
+                    for(int j=0;j<left.size();j++){
+                        for(int k=0;k<right.size();k++){
+                            ans.push_back(left[j]*right[k]);
+                        }
+                    }
+                } else if (expression[i] == '-') {
+                    for(int j=0;j<left.size();j++){
+                        for(int k=0;k<right.size();k++){
+                            ans.push_back(left[j]-right[k]);
+                        }
+                    }
+                } else if (expression[i] == '+') {
+                    for(int j=0;j<left.size();j++){
+                        for(int k=0;k<right.size();k++){
+                            ans.push_back(left[j]+right[k]);
+                        }
+                    }
+                }
+            } else {
+                num = num * 10 + (expression[i] - 48);
+            }
+        }
+        if(flag==0){
+            return {num};
+        }
         return ans;
     }
-
-    int solve(string expression,int count,int index,int result){
-        if(count==expression.length()/2){
-            ans.push_back(result);
-            return;
-        }
-
-        
-    }
-
 };
