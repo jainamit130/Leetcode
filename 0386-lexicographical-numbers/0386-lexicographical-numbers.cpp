8837a38808ae2096ed35bd858961c6656710391d
Amit Choraria <jainamit130@gmail.com>
2024-09-21 15:28:04 +0530

    Time: 55 ms (28.97%), Space: 34.4 MB (5.32%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -1,22 +1,46 @@
+class TrieNode{
+    public:
+        map<char,TrieNode*> children;
+        bool end;
+        TrieNode(){end=false;}
+};
+
+class Trie{
+    public:
+        TrieNode* root;
+        Trie(){
+            root=new TrieNode();
+        }
+        void insert(string s){
+            TrieNode* current=root;
+            for(auto ch:s){
+                if(current->children.find(ch)==current->children.end())
+                    current->children[ch]=new TrieNode();
+                current=current->children[ch];
+            }
+            current->end=true;
+        }
+};
+
 class Solution {
 public:
     vector<int> ans;
     vector<int> lexicalOrder(int n) {
-        int digits = min(n,9);
-        for(int i=1;i<=digits;i++){
-            solve(i,n);
+        Trie* data = new Trie();
+        for(int i=1;i<=n;i++){
+            data->insert(to_string(i));
         }
+        dfs(data->root,0);
         return ans;
     }
 
-    void solve(int num,int n){
-        if(num>n){
-            return;
-        }
+    void dfs(TrieNode* root,int current){     
+        if(root->end)
+            ans.push_back(current);
 
-        ans.push_back(num);
-        for(int i=0;i<=9;i++){
-            solve(num*10+i,n);
+        for(char ch = '0';ch <='9';ch++){
+            if(root->children.find(ch)!=root->children.end())
+                dfs(root->children[ch],(current*10 )+ (ch - '0'));
         }
         return;
     }
