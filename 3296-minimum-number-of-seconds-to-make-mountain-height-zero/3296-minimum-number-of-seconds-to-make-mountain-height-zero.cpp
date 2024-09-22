e543efa220f8835488b93d0f0a83a147de069249
Amit Choraria <jainamit130@gmail.com>
2024-09-22 16:24:32 +0530

    Time: 73 ms (100%), Space: 27.9 MB (100%) - LeetHub

--- 3296-minimum-number-of-seconds-to-make-mountain-height-zero/3296-minimum-number-of-seconds-to-make-mountain-height-zero.cpp
+++ 3296-minimum-number-of-seconds-to-make-mountain-height-zero/3296-minimum-number-of-seconds-to-make-mountain-height-zero.cpp
@@ -1,24 +1,65 @@
 class Solution {
 public:
-    long long minNumberOfSeconds(int mountainHeight, vector<int>& wt) {
-    long long l = 1, r = 100000000000LL * mountainHeight / wt.size();
-
-    while (l < r) {
-        long long m = (l + r) / 2;
-        long long h = 0; // Total height reduction
-
-        // Accumulate height reductions using a loop
-        for (int t : wt) {
-            h += static_cast<long long>(sqrt(m / static_cast<double>(t) * 2 + 0.25) - 0.5);
-        }
-
-        if (h < mountainHeight)
-            l = m + 1;
-        else
-            r = m;
+    using ll=long long;
+    ll minNumberOfSeconds(int mountainHeight, vector<int>& workerTimes) {
+        ll start=1;
+        ll end=1e17;
+        ll ans=end;
+        while(start<=end){
+            ll mid=start+(end-start)/2;
+            if(isValid(mid,workerTimes,mountainHeight)){
+                ans=mid;
+                end=mid-1;
+            } else {
+                start=mid+1;
+            }
+        } 
+        return ans;
     }
 
-    return l;
-}
+    bool isValid(ll maxTime,vector<int>& rate,int height){
+        long long totalReduction=0;
+        for(auto n: rate){
+            totalReduction+=binarySearchOnHeight(maxTime,n,height);
+            if(totalReduction>=height){
+                return true;
+            }
+        }
+        return false;
+    }
 
+    int binarySearchOnHeight(ll maxTime,int n,int height){
+        int start=0;
+        int end=height;
+        int ans=0;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            ll maxTimeForMidHeight = (ll)n*((ll)mid*(ll)(mid+1)/2);
+            if(maxTimeForMidHeight<=maxTime){
+                ans=mid;
+                start=mid+1;
+            } else {
+                end=mid-1;
+            }
+        }
+        return ans;
+    }
 };
+
+/*
+
+[10^6]
+
+[10^5]
+
+1 - 10^17
+
+mid
+
+((10^6*10^6)/2)*10^5 = 10^12+5 = 10^17
+
+workerTimes[i] + workerTimes[i] * 2 + ... + workerTimes[i] * x
+
+n*(mid*(mid+1)/2);
+
+*/
