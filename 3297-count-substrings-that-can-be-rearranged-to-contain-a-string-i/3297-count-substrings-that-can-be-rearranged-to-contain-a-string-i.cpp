6ab6e07477aeaebc48ac87fbaa0c59bfed28ec95
Amit Choraria <jainamit130@gmail.com>
2024-09-23 00:27:34 +0530

    Time: 530 ms (36.36%), Space: 176.9 MB (27.27%) - LeetHub

--- 3297-count-substrings-that-can-be-rearranged-to-contain-a-string-i/3297-count-substrings-that-can-be-rearranged-to-contain-a-string-i.cpp
+++ 3297-count-substrings-that-can-be-rearranged-to-contain-a-string-i/3297-count-substrings-that-can-be-rearranged-to-contain-a-string-i.cpp
@@ -3,31 +3,30 @@ public:
     long long validSubstringCount(string word1, string word2) {
         int m=word1.length();
         int n=word2.length();
-        vector<vector<int>> prefixFreq(m+1,vector<int>(26));
-        for(int i=0;i<m;i++){
-            prefixFreq[i+1]=prefixFreq[i];
-            prefixFreq[i+1][word1[i]-'a']++;
-        }
-
+        vector<vector<int>> prefixFreq;
         vector<int> toSearch(26);
-        for(int j=0;j<n;j++){
-            toSearch[word2[j]-'a']++;
+        prefixFreq.push_back(toSearch);
+        for(auto n:word1){
+            prefixFreq.push_back(prefixFreq.back());
+            prefixFreq.back()[n-'a']++;
+        }
+        for(auto n:word2){
+            toSearch[n-'a']++;
         }
 
         long long ans=0;
         for(int i=0;i<m;i++){
-            ans+=m-binarySearch(toSearch,i,prefixFreq,m);
+            ans+=m-binarySearch(toSearch,prefixFreq,i,m-1);
             toSearch[word1[i]-'a']++;
         }
         return ans;
     }
 
-    int binarySearch(vector<int>& toSearch,int start,vector<vector<int>>& prefix,int m){
-        int end = prefix.size()-2;
-        int ans=m;
+    int binarySearch(vector<int>& toSearch,vector<vector<int>>& prefixFreq,int start,int end){
+        int ans = end+1;
         while(start<=end){
-            int mid = (start+end)/2;
-            if(isValid(prefix[mid+1],toSearch)){
+            int mid=(start+end)/2;
+            if(isValid(toSearch,prefixFreq[mid+1])){
                 ans=mid;
                 end=mid-1;
             } else {
@@ -37,9 +36,9 @@ public:
         return ans;
     }
 
-    bool isValid(vector<int>& prefix,vector<int>& toSearch){
+    bool isValid(vector<int>& toSearch,vector<int>& prefix){
         for(int i=0;i<26;i++){
-            if(prefix[i]<toSearch[i]){
+            if(toSearch[i]>prefix[i]){
                 return false;
             }
         }
@@ -47,13 +46,28 @@ public:
     }
 };
 
+
 /*
 
-    a   b   c   a   b   c
+a   1
+b   1
+c   1
 
-a   1   1   1   2   2   2
-b   0   1   1   1   2   2 
-c   0   0   1   1   1   2
+        5-2+1 =4
+        5-3+1 =3
+        5-2+1 =2 
+        5-5+1 =1
+-------------------
+                10
+
+        0   1   2   3   4   5
+        a   b   c   a   b   c
+
+a   0   1   1   1   2   2   2
+b   0   0   1   1   1   2   2 
+c   0   0   0   1   1   1   2
 
 
 */
+
+
