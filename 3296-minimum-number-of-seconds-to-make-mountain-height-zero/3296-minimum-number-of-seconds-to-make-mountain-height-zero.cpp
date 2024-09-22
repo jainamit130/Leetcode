688664ac5900a01127fa6083f0b019afcfb9bf9f
Amit Choraria <jainamit130@gmail.com>
2024-09-22 16:09:04 +0530

    Time: 26 ms (100%), Space: 27.8 MB (100%) - LeetHub

--- 3296-minimum-number-of-seconds-to-make-mountain-height-zero/3296-minimum-number-of-seconds-to-make-mountain-height-zero.cpp
+++ 3296-minimum-number-of-seconds-to-make-mountain-height-zero/3296-minimum-number-of-seconds-to-make-mountain-height-zero.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     long long minNumberOfSeconds(int mountainHeight, vector<int>& wt) {
-    long long l = 1, r = 1e18;
+    long long l = 1, r = 100000000000LL * mountainHeight / wt.size();
 
     while (l < r) {
         long long m = (l + r) / 2;
-        long long h = 0; 
+        long long h = 0; // Total height reduction
 
+        // Accumulate height reductions using a loop
         for (int t : wt) {
-            h += (long long)(sqrt(m / (double)(t) * 2 + 0.25) - 0.5);
+            h += static_cast<long long>(sqrt(m / static_cast<double>(t) * 2 + 0.25) - 0.5);
         }
 
         if (h < mountainHeight)
