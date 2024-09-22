cf054a2f395947c6388ccd30f095ec5219a503e8
Amit Choraria <jainamit130@gmail.com>
2024-09-22 23:43:10 +0530

    Time: 561 ms (50%), Space: 112.2 MB (100%) - LeetHub

--- 3296-minimum-number-of-seconds-to-make-mountain-height-zero/3296-minimum-number-of-seconds-to-make-mountain-height-zero.cpp
+++ 3296-minimum-number-of-seconds-to-make-mountain-height-zero/3296-minimum-number-of-seconds-to-make-mountain-height-zero.cpp
@@ -1,65 +1,36 @@
 class Solution {
 public:
     using ll=long long;
-    ll minNumberOfSeconds(int mountainHeight, vector<int>& workerTimes) {
-        ll start=1;
-        ll end=1e17;
-        ll ans=end;
-        while(start<=end){
-            ll mid=start+(end-start)/2;
-            if(isValid(mid,workerTimes,mountainHeight)){
-                ans=mid;
-                end=mid-1;
-            } else {
-                start=mid+1;
-            }
-        } 
-        return ans;
-    }
-
-    bool isValid(ll maxTime,vector<int>& rate,int height){
-        long long totalReduction=0;
-        for(auto n: rate){
-            totalReduction+=binarySearchOnHeight(maxTime,n,height);
-            if(totalReduction>=height){
-                return true;
-            }
+    long long minNumberOfSeconds(int mountainHeight, vector<int>& workerTimes) {
+        priority_queue<vector<ll>,vector<vector<ll>>,greater<vector<ll>>> pq;
+        for(auto n: workerTimes){
+            pq.push({n,n,1});
         }
-        return false;
-    }
 
-    int binarySearchOnHeight(ll maxTime,int n,int height){
-        int start=0;
-        int end=height;
-        int ans=0;
-        while(start<=end){
-            int mid=start+(end-start)/2;
-            ll maxTimeForMidHeight = (ll)n*((ll)mid*(ll)(mid+1)/2);
-            if(maxTimeForMidHeight<=maxTime){
-                ans=mid;
-                start=mid+1;
-            } else {
-                end=mid-1;
-            }
+        ll ans=0;
+        while(mountainHeight){
+            ll currRate = pq.top()[0];
+            int original = pq.top()[1];
+            int usage = pq.top()[2];
+            pq.pop();
+            ans=currRate;
+            usage++;
+            currRate += (ll)original * usage;
+            pq.push({currRate,original,usage});
+            mountainHeight--;
         }
         return ans;
     }
 };
 
+
 /*
 
-[10^6]
+MinHeap: 
 
-[10^5]
+1 => use => 1+1*2 => use => 3+1*3
 
-1 - 10^17
+heap: <currRate,original,usage>
 
-mid
-
-((10^6*10^6)/2)*10^5 = 10^12+5 = 10^17
-
-workerTimes[i] + workerTimes[i] * 2 + ... + workerTimes[i] * x
-
-n*(mid*(mid+1)/2);
 
 */
