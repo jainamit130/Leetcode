634cb1dce7c64c4f1c9c249db30bbde72321f6c2
Amit Choraria <jainamit130@gmail.com>
2024-10-03 11:49:01 +0530

    Time: 1198 ms (41.32%), Space: 152.1 MB (66.12%) - LeetHub

--- 0488-zuma-game/0488-zuma-game.cpp
+++ 0488-zuma-game/0488-zuma-game.cpp
@@ -1,55 +1,80 @@
 class Solution {
-    unordered_map<char,int> mp;
-    int mins=10000;
-    string boa;
 public:
-    void addb(int i,int j,char c){
-        boa.insert(i,j,c);
+   int findMinStep(string board, string hand) {
+	vector<int> freq(26, 0);
+	for(char c: hand)
+		freq[c - 'A']++;
+	unordered_map<string, int> cache;
+	return dfs(board, freq, cache);
+}
+
+int dfs(string board, vector<int>& freq, unordered_map<string, int>& cache) {
+    string key = board + "#" + serialize(freq);
+	if(cache.count(key)) {
+        return cache[key];
     }
-    
-    void del(int i,int j){
-        boa.erase(boa.begin()+i,boa.begin()+j);
+	int r = INT_MAX;
+	if(board.length() == 0) {           
+		r = 0;
+	} else {
+		for(int i = 0; i < board.length(); i++) {    
+			for(int j = 0; j < freq.size(); j++) {     
+                bool worthTrying = false;
+                if(board[i] - 'A' == j)
+                    worthTrying = true;
+                else if(0 < i && board[i] == board[i - 1] && board[i] - 'A' != j) 
+                    worthTrying = true;
+                    
+				if(freq[j] > 0 && worthTrying) {     
+					board.insert(board.begin() + i, j + 'A');  
+					freq[j]--;   
+
+					string newStr = updateBoard(board);  
+					int steps = dfs(newStr, freq, cache);   
+					if(steps != -1) {   
+						r = min(r, steps + 1);
+					}
+
+					freq[j]++; 
+					board.erase(board.begin() + i);   
+				}
+			}
+		}
+	}
+	if(r == INT_MAX) r = -1; 
+	cache[key] = r;
+	return r;
+}
+
+string updateBoard(string board) {
+	 int start = 0;
+	 int end = 0; 
+	 int boardLen = board.length();
+	 while(start < boardLen) {
+		 while(end < boardLen && board[start] == board[end]) {
+			 end++;  
+		 }
+		
+		 if(end - start >= 3) {  
+			 string newBoard = board.substr(0, start) + board.substr(end); 
+			 return updateBoard(newBoard);    
+		 } else {
+			 start = end;
+		 }
+	 }
+	 return board;
+}
+
+string serialize(vector<int>& freq) {
+    string key = "";
+    for(int i = 0; i < freq.size(); i++) {
+        if(freq[i] > 0)
+        key += to_string((char) i + 'A') + to_string(freq[i]);
     }
-    
-    void dfs(int used){
-        if(boa.empty()){
-            mins=min(mins,used);
-            return;
-        }
-        for(int i=0;i<boa.size();){
-            int j=i;
-            char c=boa[i];
-            while(j<boa.size() && boa[j]==c){
-                j++;
-            }
-            if((j-i)<3){
-                int req=3-(j-i);
-                if(mp[c]>=req){
-                    del(i,j);
-                    mp[c]-=req;
-                    dfs(used+req);
-                    mp[c]+=req;
-                    addb(i,j-i,c);
-                }
-            }
-            else{
-                del(i,j);
-                dfs(used);
-                addb(i,j-i,c);
-            }
-            i=j;
-        }
-    }
-    
-    int findMinStep(string board, string hand) {
-        for(auto h:hand){
-            mp[h]++;
-        }
-        boa=board;
-        dfs(0);
-        return mins==10000 ? -1 : mins;
-    }
+    return key;
+}  
 };
+
 /*
 
 WRBRW
