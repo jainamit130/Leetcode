091129469d086394159136c05660ffa517ef28a2
Amit Choraria <jainamit130@gmail.com>
2024-10-15 00:29:56 +0530

    Time: 39 ms (5.26%), Space: 41.7 MB (15.79%) - LeetHub

--- 3318-find-x-sum-of-all-k-long-subarrays-i/3318-find-x-sum-of-all-k-long-subarrays-i.cpp
+++ 3318-find-x-sum-of-all-k-long-subarrays-i/3318-find-x-sum-of-all-k-long-subarrays-i.cpp
@@ -1,28 +1,45 @@
 class Solution {
 public:
     vector<int> findXSum(vector<int>& nums, int k, int x) {
-        int n=nums.size();
         vector<int> ans;
+        int n=nums.size();
         for(int j=k-1;j<n;j++){
-            int sum = 0;
-            unordered_map<int,int> mp;
+            int sum=0;
+            unordered_map<int,int> freq;
             set<pair<int,int>> st;
             for(int i=j-k+1;i<=j;i++){
                 sum+=nums[i];
-                if(mp.find(nums[i])!=mp.end()){
-                    st.erase({mp[nums[i]],nums[i]});
+                if(st.find( {freq[nums[i]],nums[i]} )!=st.end()){
+                    st.erase({freq[nums[i]],nums[i]});
                 }
-                mp[nums[i]]++;
-                st.insert({mp[nums[i]],nums[i]});
+                freq[nums[i]]++;
+                st.insert({freq[nums[i]],nums[i]});
             }
-            
+
             while(st.size()>x){
                 auto [smallestFreq,smallestNum] = *st.begin();
                 sum-=smallestFreq*smallestNum;
                 st.erase({smallestFreq,smallestNum});
             }
+
             ans.push_back(sum);
         }
         return ans;
     }
 };
+
+
+/*
+
+Traversing the window
+track sum
+track freq
+set<pair<int,int>> => pair<count,num> 
+
+2 -> 2
+2 -> 1
+1 -> 4
+1 -> 3
+
+
+*/
