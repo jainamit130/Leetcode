09e890dc43ae1adfaa5565cd0a9658d89a9bc48c
Amit Choraria <jainamit130@gmail.com>
2024-10-18 21:31:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0675-cut-off-trees-for-golf-event/0675-cut-off-trees-for-golf-event.cpp
+++ 0675-cut-off-trees-for-golf-event/0675-cut-off-trees-for-golf-event.cpp
@@ -1,56 +1,57 @@
 class Solution {
 public:
-    vector<int> dir = {-1, 0, 1, 0, -1};  // Simplified direction array
-
+    vector<vector<int>> dir = {{0,1},{0,-1},{1,0},{-1,0}};
+    using pii = pair<int,int>;
     int cutOffTree(vector<vector<int>>& forest) {
         vector<vector<int>> indexes;
-        for(int i = 0; i < forest.size(); i++) {
-            for(int j = 0; j < forest[0].size(); j++) {
-                if(forest[i][j] > 1) {
-                    indexes.push_back({forest[i][j], i, j});
+        for(int i=0;i<forest.size();i++){
+            for(int j=0;j<forest[0].size();j++){
+                if(forest[i][j]>1){
+                    indexes.push_back({forest[i][j],i,j});
                 }
             }
         }
-        sort(indexes.begin(), indexes.end());
-        pair<int, int> start = {0, 0};
-        int ans = 0;
-
-        for (int i = 0; i < indexes.size(); i++) {
-            pair<int, int> end = {indexes[i][1], indexes[i][2]};
-            int steps = shortestPath(forest, start, end);
-            if (steps == -1) {
+        sort(indexes.begin(),indexes.end());
+        pii start={0,0};
+        int ans=0;
+        int i=0;
+        while(i<indexes.size()){
+            pii end={indexes[i][1],indexes[i][2]};
+            int steps = shortestPath(forest,start,end);
+            if(steps==-1){
                 return -1;
             }
-            ans += steps;
-            start = end;
+            ans+=steps;
+            start=end;
+            i++;
         }
         return ans;
     }
 
-    int shortestPath(vector<vector<int>>& forest, pair<int, int>& start, pair<int, int>& end) {
-        queue<pair<int, int>> q;
+
+    int shortestPath(vector<vector<int>>& forest,pii& start, pii& end){
+        queue<pii> q;
         q.push(start);
         int level = 0;
-        vector<vector<int>> visited(forest.size(), vector<int>(forest[0].size()));
-        visited[start.first][start.second] = 1;  // Mark start as visited
+        vector<vector<int>> visited(forest.size(),vector<int>(forest[0].size()));
+        visited[start.first][start.second]=1;
+        while(!q.empty()){
+            int size= q.size();
+            while(size){
+                pii index = q.front();
 
-        while (!q.empty()) {
-            int size = q.size();
-            while (size--) {
-                auto index = q.front();
                 q.pop();
+                size--;
 
-                if (index == end) {
+                if(index==end){
                     return level;
                 }
 
-                for (int i = 0; i < 4; i++) {
-                    int newRow = index.first + dir[i];
-                    int newCol = index.second + dir[i + 1];
-                    if (isValid(newRow, newCol, forest.size(), forest[0].size()) && 
-                        forest[newRow][newCol] != 0 && visited[newRow][newCol] == 0) {
-                        visited[newRow][newCol] = 1;
-                        q.push({newRow, newCol});
+                for(auto d:dir){
+                    pii newIndex = {index.first+d[0],index.second+d[1]};
+                    if(isValid(newIndex,forest.size(),forest[0].size()) && forest[newIndex.first][newIndex.second]!=0 && visited[newIndex.first][newIndex.second]==0){
+                        visited[newIndex.first][newIndex.second]=1;
+                        q.push(newIndex);
                     }
                 }
             }
@@ -59,7 +60,60 @@ public:
         return -1;
     }
 
-    bool isValid(int row, int col, int m, int n) {
-        return row >= 0 && col >= 0 && row < m && col < n;
+    bool isValid(pii& index,int m,int n){
+        if(index.first>=0 && index.second>=0 && index.first<m && index.second<n){
+            return true;
+        }
+        return false;
     }
 };
+
+
+/*
+
+
+54581641    64080174    24346381    69107959
+86374198    61363882    68783324    79706116
+0
+
+
+1           64080174    24346381    69107959
+86374198    61363882    68783324    79706116
+2
+
+
+1           64080174    1           69107959
+86374198    61363882    68783324    79706116
+4
+
+
+1           64080174    1           69107959
+86374198    1           68783324    79706116
+4
+
+
+1           64080174    1           69107959
+86374198    1           1           79706116
+5
+
+
+1           64080174    1           1
+86374198    1           1           79706116
+7
+
+
+1           64080174    1           1
+86374198    1           1           1
+8
+
+
+1           64080174    1           1
+1           1           1           1
+11
+
+
+1           1           1           1
+1           1           1           1
+13
+
+*/
