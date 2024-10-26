d0535ad66dbb6f3b996fcd215a2b4427e6152b5c
Amit Choraria <jainamit130@gmail.com>
2024-10-26 16:09:13 +0530

    Time: 2 ms (38.87%), Space: 8 MB (50.89%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m+1,vector<int>(n+1));
-        dp[m-1][n-1]=1;
-        for(int i=m-1;i>=0;i--){
-            for(int j=n-1;j>=0;j--){
-                if(i==m-1 && j==n-1){
-                    continue;
-                }
-                dp[i][j]=dp[i+1][j]+dp[i][j+1];
+        vector<vector<int>> dp(m,vector<int>(n));
+        dp[0]=vector<int>(n,1);
+        for(int i=0;i<m;i++){
+            dp[i][0]=1;
+        }
+
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
             }
         }
-        return dp[0][0];
+        return dp[m-1][n-1];
     }
 };
