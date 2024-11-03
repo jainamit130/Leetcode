9cf1c06daef5b0904f35de8375bd2547b34a8791
Amit Choraria <jainamit130@gmail.com>
2024-11-04 00:52:59 +0530

    Time: 36 ms (100%), Space: 32.1 MB (100%) - LeetHub

--- 3341-find-minimum-time-to-reach-last-room-i/3341-find-minimum-time-to-reach-last-room-i.cpp
+++ 3341-find-minimum-time-to-reach-last-room-i/3341-find-minimum-time-to-reach-last-room-i.cpp
@@ -5,24 +5,26 @@ public:
         priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>> pq;
         int m = moveTime.size();
         int n = moveTime[0].size();
-        vector<vector<int>> visited(m, vector<int>(n));
+        vector<vector<int>> shortestTime(m,vector<int>(n,INT_MAX));
         pq.push({0, 0, 0});
-        int ans = INT_MAX;
+        shortestTime[0][0] = 0;
         while (!pq.empty()) {
             int row = pq.top()[1];
             int col = pq.top()[2];
             int time = pq.top()[0];
             pq.pop();
-            visited[row][col] = 1;
             if (row == m - 1 && col == n - 1) {
                 return time;
             }
             for (int i = 0; i < dir.size(); i++) {
                 int newRow = row + dir[i][0];
                 int newCol = col + dir[i][1];
-                if (newRow < m && newRow >= 0 && newCol < n && newCol >= 0 &&
-                    visited[newRow][newCol] == 0) {
-                    pq.push({max(moveTime[newRow][newCol], time) + 1,newRow, newCol});
+                if (newRow < m && newRow >= 0 && newCol < n && newCol >= 0) {
+                    int timeToArrive = max(moveTime[newRow][newCol], time) + 1;
+                    if(timeToArrive<shortestTime[newRow][newCol]) {
+                        shortestTime[newRow][newCol]= timeToArrive;
+                        pq.push({timeToArrive,newRow, newCol});
+                    }
                 }
             }
         }
