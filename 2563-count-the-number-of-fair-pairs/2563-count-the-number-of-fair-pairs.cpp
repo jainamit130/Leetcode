d67331b1429d618bb837d118ac434adacec2d15d
Amit Choraria <jainamit130@gmail.com>
2024-11-13 17:55:35 +0530

    Time: 82 ms (8.93%), Space: 60.2 MB (80.83%) - LeetHub

--- 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.cpp
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.cpp
@@ -1,27 +1,13 @@
 class Solution {
 public:
     long long countFairPairs(vector<int>& nums, int lower, int upper) {
-        vector<int> sortedNums;
-        int ans = 0;
-        for(auto n:nums){
-            int lowerLimit = lower-n;
-            int upperLimit = upper-n;
-            int index1 = lower_bound(sortedNums.begin(),sortedNums.end(),lowerLimit)-sortedNums.begin();
-            int index2 = upper_bound(sortedNums.begin(),sortedNums.end(),upperLimit)-sortedNums.begin();
+        sort(nums.begin(),nums.end());
+        long long ans = 0;
+        for(int i=0;i<nums.size();i++){
+            int index1 = lower_bound(nums.begin(),nums.begin()+i,lower-nums[i])-nums.begin();
+            int index2 = upper_bound(nums.begin(),nums.begin()+i,upper-nums[i])-nums.begin();
             ans+=index2-index1;
-            sortedNums.push_back(n);
-            sort(sortedNums.begin(),sortedNums.end());
         }
         return ans;
     }
 };
-
-/*
-nums[j] = 4
-
-then i want 
--1<=nums[i]<=2
-
-
-
-*/
