f6762cc1f369ad52c446e1bf6679270abada350f
Amit Choraria <jainamit130@gmail.com>
2024-11-24 00:57:23 +0530

    Time: 134 ms (100%), Space: 228.1 MB (62.5%) - LeetHub

--- 3362-zero-array-transformation-iii/3362-zero-array-transformation-iii.cpp
+++ 3362-zero-array-transformation-iii/3362-zero-array-transformation-iii.cpp
@@ -1,27 +1,69 @@
 class Solution {
 public:
     int maxRemoval(vector<int>& nums, vector<vector<int>>& queries) {
-      priority_queue<pair<int,int>> pq;
-      int n = queries.size();
-      sort(queries.begin(), queries.end());
-      vector<int> v(nums.size() + 1, 0);
-      int i = 0, k = 0;
-      while (k < nums.size()) {
-        if (k > 0)
-          v[k] += v[k - 1];
-        while (i < n && queries[i][0] <= k && queries[i][1]>=k)
-            pq.push({queries[i][1], queries[i][0]}), i++;
-        while (k < nums.size() && !pq.empty() && nums[k] + v[k] > 0 && pq.top().first >= k) {
-          v[k] -= 1;
-          v[pq.top().first + 1] += 1;
-          pq.pop();
-        }
-        if (nums[k] + v[k] > 0)
-          return -1;
-        
-        k++;
-      }
+        int n=queries.size();
+        int m = nums.size();
+        sort(queries.begin(),queries.end());
+        priority_queue<pair<int,int>> pq;
+        vector<int> prefix(m+1);
+        int i=0;  // nums index
+        int j=0; // queries index
+        while(i<m) {
+            if(i>0){
+                prefix[i]+=prefix[i-1];
+            }
 
-      return pq.size();
+            // Load the queries covering index i 
+            while(j<n && queries[j][0]<=i && i<=queries[j][1]) {
+                pq.push({queries[j][1],queries[j][0]});
+                j++;
+            }
+
+            while(!pq.empty() && nums[i]+prefix[i]>0 && pq.top().first>=i) {
+                auto [u,l]= pq.top();
+                prefix[i]-=1;
+                prefix[u+1]+=1;
+                pq.pop();
+            }
+            
+            if(prefix[i]+nums[i]>0){
+                return -1;
+            }
+
+            i++;
+        }
+
+        return pq.size();
     }
 };
+
+
+/*
+
+0   1   2   3   4   5   6
+1   2   3   4   5   6   7
+-1  0   0   0   0   0   +1
+
+-1  -1  -1  -1  -1  -1  0
+
+
+queries= [0,5],[0,2]
+
+
+[[1,3],[0,2],[1,3],[1,2]]
+
+0   2
+1   3
+1   3
+1   2
+
+
+
+1. Sort the queries
+2. Iterate nums
+3. for each nums index i => load all the queries which cover index i into priority queue (such that the upper bound is in descending order)
+4. update the query on prefix array until either the pq is exhausted or the nums[i]+prefix[i]==0
+5. if pq exhausted and still this is not true => nums[i]+prefix[i]==0 then return -1
+6. at the end return pq.size();
+
+*/
