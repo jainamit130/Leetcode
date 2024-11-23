ea3ea7be2dfbd48604903b8c5241f1927e8b8760
Amit Choraria <jainamit130@gmail.com>
2024-11-23 14:46:26 +0530

    Time: 142 ms (5.49%), Space: 60.1 MB (47.76%) - LeetHub

---
+++ 1072-flip-columns-for-maximum-number-of-equal-rows/1072-flip-columns-for-maximum-number-of-equal-rows.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int maxEqualRowsAfterFlips(int[][] matrix) {
+        Map<String,Integer> mp = new HashMap<>();
+        for(int i=0;i<matrix.length;i++) {
+            String str = new String();
+            String strrev = new String();
+            for(int j=0;j<matrix[0].length;j++) {
+                str+=matrix[i][j];
+                if(matrix[i][j]==0){
+                    strrev+=1;
+                } else {
+                    strrev+=0;
+                }
+            }
+
+            if(str.charAt(0)=='0'){
+                mp.put(str,mp.getOrDefault(str,0)+1);
+            } else {
+                mp.put(strrev,mp.getOrDefault(strrev,0)+1);
+            }
+        }
+
+        int ans = 0;
+        for(Integer val:mp.values()) {
+            ans = Math.max(ans,val);
+        }
+        return ans;
+    }
+}
