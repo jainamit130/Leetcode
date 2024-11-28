9489b54efcbe2396bf683ddc005f39249fa4a16d
Amit Choraria <jainamit130@gmail.com>
2024-11-28 22:13:03 +0530

    Time: 717 ms (30.25%), Space: 172.4 MB (38.96%) - LeetHub

--- 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
@@ -4,6 +4,8 @@ public:
         int m = grid.size();
         int n = grid[0].size();
         vector<vector<int>> dir = { {1,0}, {0,1}, {-1,0}, {0,-1} };
+        vector<vector<int>> visited(m,vector<int>(n,0));
+        visited[0][0]=1;
         priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>> pq;
         pq.push({0,0,0});
         int ans = INT_MAX;
@@ -14,15 +16,14 @@ public:
             pq.pop();
             if(row == m-1 && col == n-1){
                 ans = min(ans,obsCount);
-                break;
             }
 
-            grid[row][col]=2;
             for(int i=0;i<4;i++) {
                 int newRow = row + dir[i][0];
                 int newCol = col + dir[i][1];
-                if(newRow>=0 && newRow<m && newCol>=0 && newCol<n && grid[newRow][newCol]!=2) {
+                if(newRow>=0 && newRow<m && newCol>=0 && newCol<n && visited[newRow][newCol]==0) {
                     pq.push({obsCount + grid[newRow][newCol], newRow, newCol});
+                    visited[newRow][newCol]=1;
                 }
             }
         }
