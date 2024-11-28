3f8a4f9ca8116589652b23cc04f23197ba7d996e
Amit Choraria <jainamit130@gmail.com>
2024-11-28 22:12:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minimumObstacles(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> dir = { {1,0}, {0,1}, {-1,0}, {0,-1} };
+        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>> pq;
+        pq.push({0,0,0});
+        int ans = INT_MAX;
+        while(!pq.empty()) {
+            int row = pq.top()[1];
+            int col = pq.top()[2];
+            int obsCount = pq.top()[0];
+            pq.pop();
+            if(row == m-1 && col == n-1){
+                ans = min(ans,obsCount);
+                break;
+            }
+
+            grid[row][col]=2;
+            for(int i=0;i<4;i++) {
+                int newRow = row + dir[i][0];
+                int newCol = col + dir[i][1];
+                if(newRow>=0 && newRow<m && newCol>=0 && newCol<n && grid[newRow][newCol]!=2) {
+                    pq.push({obsCount + grid[newRow][newCol], newRow, newCol});
+                }
+            }
+        }
+        return ans;
+    }
+};
