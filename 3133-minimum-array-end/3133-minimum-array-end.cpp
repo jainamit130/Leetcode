d6f9fe8b6b31447ea666bf7a4c254687bc8176a6
Amit Choraria <jainamit130@gmail.com>
2024-12-01 11:13:24 +0530

    Time: 1764 ms (5.33%), Space: 8.6 MB (37.41%) - LeetHub

--- 3133-minimum-array-end/3133-minimum-array-end.cpp
+++ 3133-minimum-array-end/3133-minimum-array-end.cpp
@@ -1,32 +1,3 @@
-// class Solution {
-// public:
-//     using ll = long long;
-//     ll minEnd(int n, int x){
-//         ll  num = x , one = 1;
-//         vector<int> unsetBits;
-//         for(int bit = 0 ; bit < 63 ; bit++){
-//             if(num >> bit & one ^ one){
-//                 unsetBits.push_back(bit);
-//             }
-//         }
-//         for(int i=0;i<unsetBits.size();i++)
-//             cout<<unsetBits[i]<<endl;
-//         int m = unsetBits.size();
-//         for(ll mask = 0 ; mask < (one << m) ; mask++){
-//             if(--n == 0){
-//                 for(int index = 0 ; index < m ; index++){
-//                     if(mask >> index & one){
-//                         num |= one << unsetBits[index];
-//                     }
-//                 }
-//                 return num;
-//             }
-//         }
-//         return num;
-//     }
-// };
-
-
 class Solution {
 public:
     long long minEnd(int n, int x) {
@@ -41,19 +12,19 @@ public:
             }
         }
         vector<int> unsetBits1=unsetBits;
-        long long numberOfZeros=1;
+        long long res=1;
         while(n){
             if(n==1){
                 unsetBits1=unsetBits;
                 int k=63;
                 for(int i=63;i>=0;i--){
                     if(unsetBits1[i]==0){
-                        unsetBits1[i]=unsetBits1[i]|((numberOfZeros>>(63-k)) & 1);
+                        unsetBits1[i]=unsetBits1[i]|((res>>(63-k)) & 1);
                         k--;
                     }
                 }
             }
-           numberOfZeros++;
+           res++;
             n--;
         }
         long long ans=0;
