96b7d2a0fe220be588d8f45826ec5264c237c832
Amit Choraria <jainamit130@gmail.com>
2024-12-02 13:20:56 +0530

    Time: 627 ms (31.04%), Space: 362.4 MB (23.93%) - LeetHub

--- 3203-find-minimum-diameter-after-merging-two-trees/3203-find-minimum-diameter-after-merging-two-trees.cpp
+++ 3203-find-minimum-diameter-after-merging-two-trees/3203-find-minimum-diameter-after-merging-two-trees.cpp
@@ -3,23 +3,14 @@ public:
     int minimumDiameterAfterMerge(vector<vector<int>>& edges1, vector<vector<int>>& edges2) {
         int n = edges1.size()+1;
         int m = edges2.size()+1;
+        vector<vector<int>> adj1 = constructAdj(edges1);
+        vector<vector<int>> adj2 = constructAdj(edges2);
+
         int maxD1=0,maxD2=0;
-        vector<vector<int>> adj1(n);
-        vector<vector<int>> adj2(m);
-        for(auto edge:edges1) {
-            adj1[edge[0]].push_back(edge[1]);
-            adj1[edge[1]].push_back(edge[0]);
-        }
-
-        for(auto edge:edges2) {
-            adj2[edge[0]].push_back(edge[1]);
-            adj2[edge[1]].push_back(edge[0]);
-        }
-
-        vector<int> visited1(n);
+        vector<int> visited1(n),visited2(m);
         getMaxDiameter(adj1,0,visited1,maxD1);
-        vector<int> visited2(m);
         getMaxDiameter(adj2,0,visited2,maxD2);
+
         return max({maxD1,maxD2,(maxD1+1)/2+(maxD2+1)/2 + 1});
     }
 
@@ -32,9 +23,7 @@ public:
                 int childDepth = getMaxDiameter(adj,neigh,visited,maxDiameter);
                 maxDepth = max(childDepth,maxDepth);
                 pq.push(childDepth);
-                if(pq.size()>2) {
-                    pq.pop();
-                }
+                if(pq.size()>2) pq.pop();
             }
         }
         int subDiameter = 0;
@@ -44,6 +33,15 @@ public:
         }
         maxDiameter = max(maxDiameter,subDiameter);
         return maxDepth+1;
+    }
+
+    vector<vector<int>> constructAdj(vector<vector<int>>& edges) {
+        vector<vector<int>> adj(edges.size()+1);
+        for(auto edge:edges) {
+            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]);
+        }
+        return adj;
     }
 };
 
