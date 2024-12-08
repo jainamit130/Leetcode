7a94ebaae288ac6972e0976ca8f6672e448fd6e4
Amit Choraria <jainamit130@gmail.com>
2024-12-09 00:03:22 +0530

    Time: 13 ms (69.23%), Space: 32.9 MB (92.31%) - LeetHub

---
+++ 3380-maximum-area-rectangle-with-point-constraints-i/3380-maximum-area-rectangle-with-point-constraints-i.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int maxRectangleArea(vector<vector<int>>& points) {
+        set<pair<int,int>> st;
+        for(auto point:points){
+            st.insert({point[0],point[1]});
+        }
+
+        int ans = INT_MIN;
+        for(int i=0;i<points.size()-1;i++) {
+            int x1 = points[i][0];
+            int y1 = points[i][1];
+            for(int j=i+1;j<points.size();j++) {
+                int x2 = points[j][0];
+                int y2 = points[j][1];
+                if(x1!=x2 && y1!=y2 && st.find({x1,y2})!=st.end() && st.find({x2,y1})!=st.end()) {
+                    bool isValid = true;
+                    for(int k=0;k<points.size();k++) { 
+                        if(k==i || k==j){
+                            continue;
+                        }
+                        if ((points[k][0] > min(x1, x2) && points[k][0] < max(x1, x2) &&
+     points[k][1] > min(y1, y2) && points[k][1] < max(y1, y2)) ||
+    ((points[k][0] == min(x1, x2) || points[k][0] == max(x1, x2)) &&
+     (points[k][1] > min(y1, y2) && points[k][1] < max(y1, y2))) ||
+    ((points[k][1] == min(y1, y2) || points[k][1] == max(y1, y2)) &&
+     (points[k][0] > min(x1, x2) && points[k][0] < max(x1, x2)))) {
+                            isValid = false;
+                            break;
+                        }
+                    }
+                    if(isValid) {
+                        cout<<x1<<","<<y1<<" and "<<x2<<","<<y2<<endl;
+                        ans = max(ans,abs(y2-y1)*abs(x2-x1));
+                    }
+                }
+            }
+        }
+        if(ans==INT_MIN){
+            return -1;
+        }
+        return ans;
+    }
+};
