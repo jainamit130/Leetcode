e065e57e3d9faf89877f25e400098677bce8f8bc
Amit Choraria <jainamit130@gmail.com>
2024-12-11 21:27:27 +0530

    Time: 120 ms (49.5%), Space: 140.5 MB (27.73%) - LeetHub

--- 3243-shortest-distance-after-road-addition-queries-i/3243-shortest-distance-after-road-addition-queries-i.cpp
+++ 3243-shortest-distance-after-road-addition-queries-i/3243-shortest-distance-after-road-addition-queries-i.cpp
@@ -1,46 +1,55 @@
 class Solution {
 public:
     vector<int> shortestDistanceAfterQueries(int n, vector<vector<int>>& queries) {
-        vector<vector<pair<int, int>>> graph(n);
-        
-        for (int i = 0; i < n - 1; ++i) {
-            graph[i].emplace_back(i + 1, 1); 
+        vector<vector<int>> adj(n);
+        for(int i=0;i<n-1;i++) {
+            adj[i].push_back(i+1);
         }
-        
-        auto dijkstra = [&](int n) {
-            vector<int> dist(n, INT_MAX);
-            dist[0] = 0;
-            priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-            pq.emplace(0, 0); 
-            
-            while (!pq.empty()) {
-                int d = pq.top().first;
-                int u = pq.top().second;
-                pq.pop();
-                
-                if (d > dist[u]) continue;
-                
-                for (const auto& [v, w] : graph[u]) {
-                    if (dist[u] + w < dist[v]) {
-                        dist[v] = dist[u] + w;
-                        pq.emplace(dist[v], v);
+
+        vector<int> ans;
+        for(auto q:queries) {
+            adj[q[0]].push_back(q[1]);
+            ans.push_back(bfs(adj));
+        }
+
+        return ans;
+    }
+
+    int bfs(vector<vector<int>>& adj) {
+        vector<int> visited(adj.size());
+        queue<int> q;
+        q.push(0);
+        visited[0]=1;
+        int dist = 0;
+        while(!q.empty()) {
+            int size = q.size();
+            while(size) {
+                int node = q.front();
+                size--;
+                q.pop();
+                if(node==adj.size()-1) {
+                    return dist;
+                }
+                for(auto neigh:adj[node]) {
+                    if(visited[neigh]==0) {
+                        q.push(neigh);
+                        visited[neigh] = 1;
                     }
                 }
             }
-            
-            return dist[n - 1];
-        };
-        
-        vector<int> answer;
-        
-        for (const auto& query : queries) {
-            int u = query[0];
-            int v = query[1];
-            graph[u].emplace_back(v, 1); 
-            
-            answer.push_back(dijkstra(n));
+            dist++;
         }
-        
-        return answer;
+        return -1;
     }
 };
+
+
+/*
+
+    0   1   2   3   4
+1                   
+2
+3
+
+
+*/
