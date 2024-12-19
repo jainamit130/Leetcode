ceb85907002b7fb3bb2df0d212511c6a0ddcd1f3
Amit Choraria <jainamit130@gmail.com>
2024-12-19 23:49:05 +0530

    Time: 1800 ms (5.03%), Space: 533.8 MB (5.03%) - LeetHub

--- 3389-minimum-operations-to-make-character-frequencies-equal/3389-minimum-operations-to-make-character-frequencies-equal.cpp
+++ 3389-minimum-operations-to-make-character-frequencies-equal/3389-minimum-operations-to-make-character-frequencies-equal.cpp
@@ -1,49 +1,76 @@
 class Solution {
 public:
-    unordered_map<int,unordered_map<int,int>> dp;
+    unordered_map<int,unordered_map<int,int>> cache;
     int makeStringGood(string s) {
-        unordered_map<int, int> mp;
-        for (auto ch : s) {
-            mp[ch - 'a']++;
-        }
-
+        unordered_map<int,int> mp;
         int maxC = 0;
-        for (auto [_, count] : mp) {
-            maxC = max(maxC, count);
+        for(auto ch:s) {
+            mp[ch-'a']++;
+            maxC = max(maxC,mp[ch-'a']);
         }
 
         int ans = INT_MAX;
-
-        for (int i = 0; i <= maxC; i++) {
-            dp.clear();
-            ans = min(ans, solve(0, 0, i, mp));
+        for(int i=0;i<=maxC;i++) {
+            cache.clear();
+            ans = min(ans,solve(mp,i,0,0));
         }
+
         return ans;
     }
 
-    int solve(int index, int leftOver, int target, unordered_map<int, int> &mp) {
-        if (index >= 26) {
+    int solve(unordered_map<int,int>& mp,int targetFreq, int index, int leftOvers) {
+        if(index>=26) {
             return 0;
-        }
-        if (dp[index].count(leftOver)) {
-            return dp[index][leftOver];
         }
 
         int ans = INT_MAX;
         int current = mp[index];
 
-        if (current == target) {
-            ans = min(ans, solve(index + 1, 0, target, mp));
-        } else if (current > target) {
-            ans = min(ans, solve(index + 1, current - target, target, mp) + current - target);
-        } else {
-            ans = min({
-                ans,
-                solve(index + 1, 0, target, mp) + max(target - (current + leftOver), 0),
-                solve(index + 1, current, target, mp) + current
-            });
+        if(cache[index].count(leftOvers)) {
+            return cache[index][leftOvers];
         }
 
-        return dp[index][leftOver] = ans;
+        if(current==targetFreq) {
+            ans = min(ans,solve(mp,targetFreq,index+1,0));
+        } else if(current<targetFreq) {
+            ans = min({ans,
+                       max(targetFreq-(current+leftOvers),0) +solve(mp,targetFreq,index+1,0),
+                       current+ solve(mp,targetFreq,index+1,current)});
+        } else {
+            ans = min(ans , current-targetFreq + solve(mp,targetFreq,index+1,current-targetFreq));
+        }
+
+        return cache[index][leftOvers]=ans;
     }
 };
+
+/*
+
+
+a   a   a   b   c
+
+
+Map
+
+a -> 3
+b -> 1
+c -> 1
+
+index, leftOvers
+
+x frequency =>
+
+ax > x
+=> should not just delete
+=> but convert => cost = ax-x
+
+ax == x
+=> Move forward => cost = 0
+
+ax < x
+=>  go towards x => cost = x-ax ; leftover = 0;
+=>  go towards 0 => cost = ax; leftover = ax
+
+0 -> 2*10^4
+
+*/
