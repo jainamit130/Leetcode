33f04a0434cc5a83293983eef23f2a18ae956c92
Amit Choraria <jainamit130@gmail.com>
2024-12-19 15:14:30 +0530

    Time: 0 ms (100%), Space: 8.9 MB (7.14%) - LeetHub

--- 2162-minimum-cost-to-set-cooking-time/2162-minimum-cost-to-set-cooking-time.cpp
+++ 2162-minimum-cost-to-set-cooking-time/2162-minimum-cost-to-set-cooking-time.cpp
@@ -8,23 +8,20 @@ public:
         char curr = to_string(startAt)[0];
         if(minutes!=0) {
             ans+=calcCost(to_string(minutes),curr,moveCost,pushCost);
+            ans+=calcCost("0"+to_string(seconds),curr,moveCost,pushCost);
+        } else {
+            ans+=calcCost(to_string(seconds),curr,moveCost,pushCost);
         }
-        string strSec = to_string(seconds);
-        if(strSec.size()==1 && minutes!=0) {
-            strSec="0"+strSec;
-        }
-        ans+=calcCost(strSec,curr,moveCost,pushCost);
 
         curr = to_string(startAt)[0];
         if(minutes!=0 && seconds<=39) {
             ans2=0;
-            if(minutes>1)
+            if(minutes>1) {
                 ans2+=calcCost(to_string(minutes-1),curr,moveCost,pushCost);
-            strSec = to_string(seconds + 60);
-            if(strSec.size()==1) {
-                strSec="0"+strSec;
+                ans2+=calcCost("0"+to_string(seconds + 60),curr,moveCost,pushCost);
+            } else {
+                ans2+=calcCost(to_string(seconds + 60),curr,moveCost,pushCost);
             }
-            ans2+=calcCost(strSec,curr,moveCost,pushCost);
         }
         if(ans2!=-1) {
             ans = min(ans,ans2);
@@ -34,7 +31,7 @@ public:
 
     int calcCost(string nums,char& curr,int moveCost,int pushCost) {
         int cost = 0;
-        int index = 0;
+        int index = nums.size()>2?1:0;
         while(index<nums.size()) {
             if(curr!=nums[index]) {
                 cost+=moveCost;
