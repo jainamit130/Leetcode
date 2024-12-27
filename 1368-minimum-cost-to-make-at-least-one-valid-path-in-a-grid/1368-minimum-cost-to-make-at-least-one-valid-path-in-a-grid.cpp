dfc2f5caf984a02f2edde1a1c1e9680633d11bbe
Amit Choraria <jainamit130@gmail.com>
2024-12-27 21:23:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -4,27 +4,24 @@ public:
         int m = grid.size();
         int n = grid[0].size();
         vector<vector<int>> dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+        queue<vector<int>> q;
         vector<vector<int>> dist(m,vector<int>(n,INT_MAX));
-        pq.push({0, 0, 0});
-        while (!pq.empty()) {
-            int cost = pq.top()[0];
-            int row = pq.top()[1];
-            int col = pq.top()[2];
+        q.push({0, 0, 0});
+        while (!q.empty()) {
+            int cost = q.front()[0];
+            int row = q.front()[1];
+            int col = q.front()[2];
             int sign = grid[row][col];
-            pq.pop();
-            if(row==m-1 && col==n-1) {
-                return cost;
-            }
+            q.pop();
             for (int i = 1; i <= 4; i++) {
                 int newRow = row + dir[i - 1][0];
                 int newCol = col + dir[i - 1][1];
                 if (newRow >= 0 && newRow < m && newCol >= 0 && newCol < n && dist[newRow][newCol]>(i!=sign)+cost) {
                     dist[newRow][newCol]=(i!=sign)+cost;
-                    pq.push({(i!=sign)+cost, newRow, newCol});
+                    q.push({(i!=sign)+cost, newRow, newCol});
                 }
             }
         }
-        return 0;
+        return dist[m-1][n-1];
     }
 };
