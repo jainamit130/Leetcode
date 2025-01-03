83ca0c19dd10e8ff6a9526fa3f9abc13720d541e
Amit Choraria <jainamit130@gmail.com>
2025-01-03 17:51:32 +0530

    Time: 32 ms (54.31%), Space: 60.6 MB (66.43%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.cpp
+++ 0706-design-hashmap/0706-design-hashmap.cpp
@@ -1,22 +1,74 @@
-class MyHashMap {
+class Node {
 public:
-    vector<int> nums;
+    int key;
+    int val;
+    Node* next;
+
+    Node(int k = -1, int v = -1, Node* n = nullptr) : key(k), val(v), next(n) {}
+};
+
+class MyHashMap {
+private:
+    vector<Node*> map;
+
+public:
     MyHashMap() {
-        nums.resize(1e6+1,-1);
+        map.resize(1000);
+        for (int i = 0; i < 1000; ++i) {
+            map[i] = new Node();
+        }
     }
-    
+
+    int hash(int key) {
+        return key % 1000;
+    }
+
     void put(int key, int value) {
-        nums[key]=value;
+        int hash_key = hash(key);
+        Node* cur = map[hash_key];
+
+        while (cur->next) {
+            if (cur->next->key == key) {
+                cur->next->val = value;
+                return;
+            }
+            cur = cur->next;
+        }
+
+        cur->next = new Node(key, value);
     }
-    
+
     int get(int key) {
-        return nums[key];
+        int hash_key = hash(key);
+        Node* cur = map[hash_key];
+
+        while (cur->next) {
+            if (cur->next->key == key) {
+                return cur->next->val;
+            }
+            cur = cur->next;
+        }
+
+        return -1;
     }
-    
+
     void remove(int key) {
-        nums[key] = -1;
+        int hash_key = hash(key);
+        Node* cur = map[hash_key];
+
+        while (cur->next) {
+            if (cur->next->key == key) {
+                Node* temp = cur->next;
+                cur->next = cur->next->next;
+                delete temp;
+                return;
+            }
+            cur = cur->next;
+        }
     }
 };
+
+
 
 /**
  * Your MyHashMap object will be instantiated and called as such:
