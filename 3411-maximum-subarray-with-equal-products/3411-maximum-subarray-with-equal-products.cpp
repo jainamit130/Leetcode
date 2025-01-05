fc2a50ea4c7e52e5b2218dab52d43ab86f7fd72a
Amit Choraria <jainamit130@gmail.com>
2025-01-05 12:02:41 +0530

    Time: 0 ms (100%), Space: 28 MB (46.15%) - LeetHub

--- 3411-maximum-subarray-with-equal-products/3411-maximum-subarray-with-equal-products.cpp
+++ 3411-maximum-subarray-with-equal-products/3411-maximum-subarray-with-equal-products.cpp
@@ -1,70 +1,19 @@
 class Solution {
 public:
     int maxLength(vector<int>& nums) {
-        int ans = 0;
-        for(int i=0;i<nums.size();i++) {
-            for(int k=1; k<=nums.size()-i; k++) { 
-                vector<int> arr;
-                for(int j=i;j<k+i;j++) {
-                    arr.push_back(nums[j]);
-                }
-                if(prod(arr)==gcd(arr)*1LL*lcm(arr)) {
-                    ans = max(ans,k);
-                }
-            }
+    int ans = 0;
+    for(int i = 0; i < nums.size(); ++i){
+        long long gcdVal = nums[i], lcmVal = nums[i], prod = 1, maxLcm = 100000000000;
+        for(int j = i; j < nums.size(); ++j){
+            prod = prod*(long long)nums[j];
+            if(prod > maxLcm) break;
+            gcdVal = gcd(gcdVal, (long long)nums[j]);
+            lcmVal = lcm(lcmVal, (long long)nums[j]);
+            if(prod == gcdVal * lcmVal) ans = max(ans, j - i + 1);
         }
-        return ans;
     }
-
-    long long prod(vector<int> arr) {
-        long long prod = 1;
-        for(auto n:arr) {
-            prod=prod*n;
-            if(prod>2520) {
-                return prod;
-            }
-        }
-        return prod;
-    }
-
-    long long lcm(vector<int> arr) {
-        vector<int> nums(11);
-        for(int i=0;i<arr.size();i++) {
-            for(int j=2;j<=arr[i];j++) {
-                int count = 0;
-                while(arr[i]%j==0) {
-                    count++;
-                    arr[i]/=j;
-                }
-                nums[j] = max(count,nums[j]);
-            }   
-        }
-        long long lcm = 1;
-        for(int i=2;i<nums.size();i++){
-            if(nums[i]>0) {
-                lcm*=pow(i,nums[i]);
-            }
-        }
-        return lcm;
-    }
-
-    long long gcd(vector<int> arr) {
-        vector<int> nums(11);
-        for(int i=0;i<arr.size();i++) {
-            for(int j=2;j<=arr[i];j++) {
-                if(arr[i]%j==0){
-                    nums[j]++;
-                }
-            }
-        }
-        
-        long long gcd = 1;
-        for(int i=2;i<nums.size();i++){
-            if(nums[i]==arr.size())
-                gcd=i; 
-        }
-        return gcd;
-    }
+    return ans;
+}
 };
 
 
