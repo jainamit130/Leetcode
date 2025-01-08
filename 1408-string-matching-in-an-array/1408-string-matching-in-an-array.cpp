8959fafb6a8dbb5c3e33701994b5f91392fade7a
Amit Choraria <jainamit130@gmail.com>
2025-01-08 23:14:52 +0530

    Time: 15 ms (9.7%), Space: 14.5 MB (9.88%) - LeetHub

--- 1408-string-matching-in-an-array/1408-string-matching-in-an-array.cpp
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.cpp
@@ -5,19 +5,20 @@ public:
             return lhs.length()<rhs.length();
         });
 
-        vector<string> ans;
+        unordered_set<string> ansSt;
         for(int i=0;i<words.size()-1;i++) {
             string word1 = words[i];
             for(int j=i+1;j<words.size();j++) {
                 string word2 = words[j];
                 for(int k=0;k<=word2.length()-word1.length();k++) {
                     if(word1==word2.substr(k,word1.length())) {
-                        ans.push_back(word1);
+                        ansSt.insert(word1);
                         break;
                     }
                 }
             }
         }
+        vector<string> ans(ansSt.begin(),ansSt.end());
         return ans;
     }
 };
