8ee56e03c82156a10af2be8d4303dbd801e80445
Amit Choraria <jainamit130@gmail.com>
2025-01-12 18:15:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3419-minimize-the-maximum-edge-weight-of-graph/3419-minimize-the-maximum-edge-weight-of-graph.cpp
+++ 3419-minimize-the-maximum-edge-weight-of-graph/3419-minimize-the-maximum-edge-weight-of-graph.cpp
@@ -7,27 +7,26 @@ public:
         }
 
         int start = 1;
-        int end = 1e6+1;
-        int ans = end;
+        int end = 1e6;
+        int ans = 1e6+1;
         while(start<=end) {
             int mid = (start+end)/2;
             vector<int> visited(n);
-            visited[0]=1;
             if(dfs(adj,visited,0,mid)==n) {
                 ans = mid;
-                end=mid-1;
+                end = mid-1;
             } else {
                 start = mid+1;
             }
         }
-        return (ans==(1e6+1))?-1:ans;
+        return (ans==1e6+1)?-1:ans;
     }
 
     int dfs(vector<vector<vector<int>>>& adj,vector<int>& visited,int node,int maxWt) {
         int totalNodes = 1;
         for(int i=0;i<adj[node].size();i++) {
             int nextNode = adj[node][i][0];
-            if(maxWt>=adj[node][i][1] && visited[nextNode]==0) {
+            if(maxWt>=adj[node][i][1] && !visited[nextNode]) {
                 visited[nextNode]=1;
                 totalNodes += dfs(adj,visited,nextNode,maxWt);
             }
@@ -38,11 +37,6 @@ public:
 
 
 /*
-
-1 - 1e6
-
-
-
 
 
 
