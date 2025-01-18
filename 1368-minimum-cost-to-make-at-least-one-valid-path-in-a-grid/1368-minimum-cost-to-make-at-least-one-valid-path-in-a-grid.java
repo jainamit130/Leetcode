d9064befd9485f23d77b7196b414601c6515082b
Amit Choraria <jainamit130@gmail.com>
2025-01-18 19:14:36 +0530

    Time: 117 ms (5.11%), Space: 47.1 MB (5.11%) - LeetHub

--- 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
+++ 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
@@ -3,19 +3,28 @@ class Solution {
         int[][] dir = { {0,1}, {0,-1}, {1,0}, {-1,0} }; 
         int m = grid.length;
         int n = grid[0].length;
+        int[][][] adj = new int[m][n][4];
 
         BiPredicate<Integer, Integer> isValid = (row, col) -> row >= 0 && row < m && col >= 0 && col < n;
+
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+                for(int k=1;k<=4;k++) {
+                    adj[i][j][k-1]=(grid[i][j]!=k)?1:0;
+                }
+            }
+        }
 
         PriorityQueue<List<Integer>> minHeap = new PriorityQueue<>(
             Comparator.comparingInt(a -> a.get(0))
         ); 
+        int[][][] visited = new int[m][n][4];
+        visited[0][0][0]=1;
+        visited[0][0][1]=1;
+        visited[0][0][2]=1;
+        visited[0][0][3]=1;
 
         minHeap.add(Arrays.asList(0, 0, 0));
-
-        int[][] distance = new int[m][n];
-        for (int[] row : distance) Arrays.fill(row, Integer.MAX_VALUE);
-        distance[0][0] = 0;
-
         while(!minHeap.isEmpty()) {
             List<Integer> minList = minHeap.poll(); 
             int weight = minList.get(0);
@@ -26,21 +35,16 @@ class Solution {
                 return weight;
             }
 
-            if(distance[row][col]<weight) continue;
-
-            for(int i=0;i<4;i++) {
+            for(int i=0;i<=3;i++) {
                 int newRow = row+dir[i][0];
                 int newCol = col+dir[i][1];
-                if(isValid.test(newRow,newCol)) {
-                    int newWeight = weight + (grid[row][col] != i + 1 ? 1 : 0);
-                    if (newWeight < distance[newRow][newCol]) {
-                        distance[newRow][newCol] = newWeight;
-                        minHeap.add(Arrays.asList(newWeight, newRow, newCol));
-                    }
+                if(isValid.test(newRow,newCol) && visited[newRow][newCol][i]==0) {
+                    visited[newRow][newCol][i]=1;
+                    minHeap.add(Arrays.asList(weight+adj[row][col][i],newRow,newCol));
                 }
             }
         }
-        return -1;
+        return 0;
     }
 
 }
