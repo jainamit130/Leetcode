9afaaab0760ce88a10a05489f7f6d6e67dd744f1
Amit Choraria <jainamit130@gmail.com>
2025-01-26 23:55:24 +0530

    Time: 62 ms (72.22%), Space: 96.5 MB (50%) - LeetHub

--- 3434-maximum-frequency-after-subarray-operation/3434-maximum-frequency-after-subarray-operation.cpp
+++ 3434-maximum-frequency-after-subarray-operation/3434-maximum-frequency-after-subarray-operation.cpp
@@ -1,17 +1,51 @@
 class Solution {
 public:
     int maxFrequency(vector<int>& nums, int k) {
-        int orig = count(nums.begin(), nums.end(), k), mx = 0;
-        for (int m = 1; m <= 50; ++m) {
-            if (m == k) continue;
-            int cur = 0, mxCur = 0;
-            for (int n : nums) {
-                cur += n == m ? 1 : n == k ? -1 : 0;
-                cur = max(cur, 0);
-                mxCur = max(mxCur, cur);
+        int totalK = count(nums.begin(),nums.end(),k);
+        int extraK =0;
+        for(int i=0;i<51;i++) {
+            if(i==k) continue;
+            int iToKCount = 0;
+            int count = 0;
+            for(int j=0;j<nums.size();j++) {
+                if(nums[j]==i) {
+                    count++;
+                } else if(nums[j]==k) {
+                    count--;
+                }
+                count = max(count,0);
+                iToKCount = max(iToKCount,count);
             }
-            mx = max(mx, mxCur);
+            extraK = max(iToKCount,extraK);
         }
-        return orig + mx;
+        return totalK+extraK;
     }
 };
+
+
+/*
+
+Some elements might already be k
+
+
+Some elements need to be converted to k
+
+
+k ...   5   k   6   5   5   .....   k
+
+
+choose a element i from 0-50 as the element to be converted to k
+if(i==k) loose a element => Hence consider it as -1
+if(i==nums[i]) gain a element => Hence consider it as 1
+else do nothing
+
+
+
+
+k = 3
+ans = 4
+
+
+
+
+*/
