36dc9c8ec62e10e7669186611f95e85c26770ff1
Amit Choraria <jainamit130@gmail.com>
2025-01-27 13:14:40 +0530

    Time: 213 ms (16.3%), Space: 120.1 MB (10.74%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.cpp
+++ 1462-course-schedule-iv/1462-course-schedule-iv.cpp
@@ -1,163 +1,28 @@
-//Topological Sort
-
-//BFS
 class Solution {
 public:
     vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
-        vector<vector<int>> adjacencyList(numCourses);
-        vector<bool> result(queries.size());
-        //Constructing Adjacency List
-        for(int i=0;i<prerequisites.size();i++)
-            adjacencyList[prerequisites[i][0]].push_back(prerequisites[i][1]);
-        
-        //Topological Sort
-        unordered_map<int,unordered_set<int>> prereqMap;
-        for(int i=0;i<numCourses;i++)
-            bfs(adjacencyList,i,prereqMap);
-
-        for(int i=0;i<queries.size();i++){
-            int start=queries[i][0];
-            int dst=queries[i][1];
-            result[i]=prereqMap[start].find(dst)!=prereqMap[start].end();
+        vector<vector<int>> adj(numCourses);
+        for(auto prerequisite:prerequisites) {
+            adj[prerequisite[0]].push_back(prerequisite[1]);
         }
-        return result;
+        vector<bool> ans;
+        for(auto query:queries) {
+            vector<int> visited(numCourses);
+            visited[query[0]]=1;
+            ans.push_back(dfs(adj,query[0],query[1],visited));
+        }
+        return ans;
     }
 
-    void bfs(vector<vector<int>>& adj,int course,unordered_map<int,unordered_set<int>>& prereqMap){
-        queue<int> q;
-        q.push(course);
-        while(!q.empty()){
-            int size=q.size();
-            while(size){
-                int top=q.front();
-                q.pop();
-                size--;
-                if(prereqMap[course].find(top)!=prereqMap[course].end())
-                    continue;
-                prereqMap[course].insert(top);
-                for(int i=0;i<adj[top].size();i++){
-                    if(prereqMap[course].find(adj[top][i])==prereqMap[course].end())
-                        q.push(adj[top][i]);
-                }
+    bool dfs(vector<vector<int>>& adj,int node,int dst,vector<int>& visited) {
+        if(node==dst) return true;
+        bool ans = false;
+        for(auto neigh:adj[node]) {
+            if(visited[neigh]==0) {
+                visited[neigh]=1;
+                ans = ans || dfs(adj,neigh,dst,visited);
             }
         }
-        return;
+        return ans;
     }
 };
-
-//DFS
-// class Solution {
-// public:
-//     vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
-//         vector<vector<int>> adjacencyList(numCourses);
-//         vector<bool> result(queries.size());
-//         //Constructing Adjacency List
-//         for(int i=0;i<prerequisites.size();i++)
-//             adjacencyList[prerequisites[i][0]].push_back(prerequisites[i][1]);
-        
-//         //Topological Sort
-//         unordered_map<int,unordered_set<int>> prereqMap;
-//         for(int i=0;i<numCourses;i++)
-//             dfs(adjacencyList,i,prereqMap);
-
-//         for(int i=0;i<queries.size();i++){
-//             int start=queries[i][0];
-//             int dst=queries[i][1];
-//             result[i]=prereqMap[start].find(dst)!=prereqMap[start].end();
-//         }
-//         return result;
-//     }
-
-//     unordered_set<int> dfs(vector<vector<int>>& adj,int course,unordered_map<int,unordered_set<int>>& prereqMap){
-//         if(prereqMap.find(course)!=prereqMap.end())
-//             return prereqMap[course];
-
-//         unordered_set<int> topoSet;
-//         for(int i=0;i<adj[course].size();i++){
-//             unordered_set<int> temp=dfs(adj,adj[course][i],prereqMap);
-//             prereqMap[course].insert(temp.begin(),temp.end());
-//         }
-//         prereqMap[course].insert(course);
-//         return prereqMap[course];
-//     }
-// };
-
-//Brute Force
-
-//BFS
-// class Solution {
-// public:
-//     vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
-//         vector<vector<int>> adjacencyList(numCourses);
-//         vector<bool> result(queries.size());
-//         //Constructing Adjacency List
-//         for(int i=0;i<prerequisites.size();i++)
-//             adjacencyList[prerequisites[i][0]].push_back(prerequisites[i][1]);
-  
-//         for(int i=0;i<queries.size();i++){
-//             int start=queries[i][0];
-//             int dst=queries[i][1];
-//             result[i]=bfs(adjacencyList,start,dst,numCourses);
-//         }
-//         return result;
-//     }
-
-//     bool bfs(vector<vector<int>>& adj,int start,int dst,int numCourses){
-//         queue<int> q;
-//         vector<bool> visited(numCourses);
-//         q.push(start);
-//         while(!q.empty()){
-//             int size=q.size();
-//             while(size){
-//                 int top=q.front();
-//                 if(top==dst)
-//                     return true;
-//                 q.pop();
-//                 size--;
-//                 if(visited[top])
-//                     continue;
-//                 visited[top]=true;
-//                 for(int i=0;i<adj[top].size();i++){
-//                     if(!visited[adj[top][i]])
-//                         q.push(adj[top][i]);
-//                 }
-//             }
-//         }
-//         return false;
-//     }
-// };
-
-//DFS
-// class Solution {
-// public:
-//     vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
-//         vector<vector<int>> adjacencyList(numCourses);
-//         vector<bool> result(queries.size());
-//         //Constructing Adjacency List
-//         for(int i=0;i<prerequisites.size();i++)
-//             adjacencyList[prerequisites[i][0]].push_back(prerequisites[i][1]);
-  
-//         for(int i=0;i<queries.size();i++){
-//             int start=queries[i][0];
-//             int dst=queries[i][1];
-//             vector<bool> visited(numCourses);
-//             result[i]=dfs(adjacencyList,start,dst,visited);
-//         }
-//         return result;
-//     }
-
-//     bool dfs(vector<vector<int>>& adj,int start,int dst,vector<bool>& visited){
-//         if(visited[start])
-//             return false;
-
-//         if(start==dst)
-//             return true;
-
-//         visited[start]=true;
-//         bool ans=false;
-//         for(int i=0;i<adj[start].size();i++){
-//             ans=ans || dfs(adj,adj[start][i],dst,visited);
-//         }
-//         return ans;
-//     }
-// };
