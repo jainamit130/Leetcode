d2e9e77445f8bb11471b63dab60f843477040b96
Amit Choraria <jainamit130@gmail.com>
2025-01-30 22:08:50 +0530

    Time: 0 ms (100%), Space: 8 MB (46.05%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
     int minSteps(int n) {
-        vector<vector<int>> cache(n+1,vector<int>(n+1,-1));
-        return solve(n,1,0,cache);
-    }
-
-    int solve(int& n,int count,int copyCount,vector<vector<int>>& cache) {
-        if(n==count) return 0;
-        if(count>n) return 1001;
-        if(cache[count][copyCount]!=-1) return cache[count][copyCount]; 
-        int ans = 1001;
-        if(copyCount==0) ans = min(ans,1+solve(n,count,count,cache));
-        else {
-            ans = min(ans,1+solve(n,count+copyCount,0,cache));
-            ans = min(ans,1+solve(n,count+copyCount,copyCount,cache));
+        if(n==1) return 0;
+        int ans = 0;        
+        for(int i=2;i*i<=n;i++) {
+            while(n%i==0) {
+                n/=i;
+                ans+=i;
+            }
         }
-        return cache[count][copyCount]=ans;
-    } 
+        if(n>1) ans+=n; 
+        return ans;
+    }
 };
 
 
 /*
+
+9 -> 3*3
 
 
 
