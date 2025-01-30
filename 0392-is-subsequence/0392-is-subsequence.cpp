6102a7f1249c12a4167e91d37194ce265585f1ad
Amit Choraria <jainamit130@gmail.com>
2025-01-30 16:17:40 +0530

    Time: 0 ms (100%), Space: 7.4 MB (100%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,32 +1,15 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        if(t.length()<s.length()) return false;
-        if(t==s) return true; 
-        vector<vector<int>> nextEarliestAlphaIndex(t.length()+1,vector<int>(26,-1));
-        for(int i=t.length()-2;i>=0;i--) {
-            nextEarliestAlphaIndex[i+1]=nextEarliestAlphaIndex[i+2];
-            nextEarliestAlphaIndex[i+1][t[i+1]-'a']=i+2;
+        if(s.length()==0)
+            return true;
+        for(int i=t.length()-1;i>=0;i--){
+            if(t[i]==s[s.length()-1]){
+                s.pop_back();
+            }
+            if(s.length()==0)
+                return true;
         }
-        nextEarliestAlphaIndex[0]=nextEarliestAlphaIndex[1];
-        nextEarliestAlphaIndex[0][t[0]-'a']=1;
-
-        int hopIndex = 0;
-        int i=0;
-        while(i<s.length()) {
-            int potentialNextHopIndex = nextEarliestAlphaIndex[hopIndex][s[i]-'a'];
-            if(potentialNextHopIndex==-1) return false;
-            hopIndex=potentialNextHopIndex;
-            i++;
-        }
-        return true;
+        return false;
     }
 };
-
-
-/*
-
-length of the string
-
-
-*/
