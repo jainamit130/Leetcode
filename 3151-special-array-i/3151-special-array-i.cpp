0c006180a8f2ae67d92ade88d70b09e7a7567d25
Amit Choraria <jainamit130@gmail.com>
2025-02-01 23:21:12 +0530

    Time: 0 ms (100%), Space: 27.7 MB (25%) - LeetHub

--- 3151-special-array-i/3151-special-array-i.cpp
+++ 3151-special-array-i/3151-special-array-i.cpp
@@ -1,10 +1,18 @@
 class Solution {
 public:
     bool isArraySpecial(vector<int>& nums) {
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i]%2==nums[i+1]%2)
+        // Iterate through indexes 0 to n - 1
+        for (int index = 0; index < nums.size() - 1; index++) {
+            // Compare the parities of the current and next number
+            if (nums[index] % 2 == nums[index + 1] % 2) {
+                // If the two adjacent numbers have the same parity, return
+                // false
                 return false;
+            }
         }
+
+        // Return true if no pair of adjacent numbers with the same parity are
+        // found
         return true;
     }
 };
