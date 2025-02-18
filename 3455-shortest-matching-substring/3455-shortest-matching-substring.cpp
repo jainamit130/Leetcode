4013449a5ba71b7986fb4d37e840e3855abd84f4
Amit Choraria <jainamit130@gmail.com>
2025-02-18 22:40:01 +0530

    Time: 99 ms (65.23%), Space: 105.4 MB (22.62%) - LeetHub

--- 3455-shortest-matching-substring/3455-shortest-matching-substring.cpp
+++ 3455-shortest-matching-substring/3455-shortest-matching-substring.cpp
@@ -1,65 +1,93 @@
 class Solution {
 public:
-    vector<int> computeLPS(const string &pattern) {
-        int n = pattern.size();
-        vector<int> lps(n, 0);
-        for (int i = 1; i < n; i++) {
-            int prevIdx = lps[i - 1];
-            while (prevIdx > 0 && pattern[i] != pattern[prevIdx]) {
-                prevIdx = lps[prevIdx - 1];
-            }
-            lps[i] = prevIdx + (pattern[i] == pattern[prevIdx] ? 1 : 0);
-        }
-        return lps;
-    }
-
-    int shortestMatchingSubstring(string text, string pattern) {
-        int textLength = text.length();
-        int patternLength = pattern.length();
-        if (patternLength == 2) {
-            return 0;
-        }
-
-        vector<int> starPositions;
-        for (int i = 0; i < patternLength; i++) {
-            if (pattern[i] == '*') {
-                starPositions.push_back(i);
+    int shortestMatchingSubstring(string s, string p) {
+        string group1="";
+        string group2="";
+        string group3="";
+        int count = 0;
+        for(int i=0;i<p.length();i++) {
+            if(p[i]=='*') {
+                count++;
+            } else {
+                if(count==0) {
+                    group1+=p[i];
+                } else if(count==1){
+                    group2+=p[i];
+                } else {
+                    group3+=p[i];
+                }
             }
         }
 
-        string prefix = pattern.substr(0, starPositions[0]);
-        string middle = pattern.substr(starPositions[0] + 1, starPositions[1] - starPositions[0] - 1);
-        string suffix = pattern.substr(starPositions[1] + 1, patternLength - starPositions[1] - 1);
+        vector<int> matchIndexes1 = getMatchingIndexes(group1, s);
+        vector<int> matchIndexes2 = getMatchingIndexes(group2, s);
+        vector<int> matchIndexes3 = getMatchingIndexes(group3, s);
 
-        int prefixLen = prefix.size();
-        int middleLen = middle.size();
-        int suffixLen = suffix.size();
+        int res = INT_MAX;
+        int i=0,j=0,k=0;
 
-        vector<int> prefixLPS = computeLPS(prefix + '#' + text);
-        vector<int> middleLPS = computeLPS(middle + '#' + text);
-        vector<int> suffixLPS = computeLPS(suffix + '#' + text);
+        if(group1.empty() && group2.empty() && group3.empty()) return 0;
 
-        prefixLPS = vector<int>(prefixLPS.begin() + prefixLen + 1, prefixLPS.end());
-        middleLPS = vector<int>(middleLPS.begin() + middleLen + 1, middleLPS.end());
-        suffixLPS = vector<int>(suffixLPS.begin() + suffixLen + 1, suffixLPS.end());
+        while((i+group2.length()+group3.length())<s.length()) {
+            while(i<s.length() && matchIndexes1[i]!=group1.length()) i++;
+            if(i>=s.length()) break;
 
-        int minLength = INT_MAX;
-        int i = 0, j = 0, k = 0;
+            while(j<s.length() && (j<i+group2.length() || matchIndexes2[j]!=group2.length())) j++;
+            if(j>=s.length()) break;
 
-        while (i + middleLen + suffixLen < textLength) {
-            while (i < textLength && prefixLPS[i] != prefixLen) i++;
-            if (i >= textLength) break;
+            while(k<s.length() && (k<j+group3.length() || matchIndexes3[k]!=group3.length())) k++;
+            if(k>=s.length()) break;
 
-            while (j < textLength && (j < i + middleLen || middleLPS[j] != middleLen)) j++;
-            if (j >= textLength) break;
-
-            while (k < textLength && (k < j + suffixLen || suffixLPS[k] != suffixLen)) k++;
-            if (k >= textLength) break;
-
-            minLength = min(minLength, k - i + prefixLen);
+            res = min(res,k-i+(int)group1.size());
             i++;
         }
 
-        return minLength == INT_MAX ? -1 : minLength;
+
+        return res == INT_MAX ? -1 : res;
+    } 
+
+    vector<int> getMatchingIndexes(string pattern,string s) {
+        vector<int> matchIndexes(s.length(),0);
+        if(pattern.length()==0) return matchIndexes;
+        vector<int> lps = getLPS(pattern);
+        int i=0,j=0;
+        while(i<s.length()) {
+            if(s[i]==pattern[j]) {
+                i++;
+                j++;
+                if(j==pattern.length()) {
+                    matchIndexes[i-1]=pattern.length();
+                    j = lps[j-1];
+                }
+            } else {
+                if(j!=0) j = lps[j-1];
+                else i++;
+            }
+        }
+        return matchIndexes;
+    }
+
+    vector<int> getLPS(string pattern) {
+        vector<int> lps(pattern.length());
+        int i=1;
+        int length=0;
+        while(i<pattern.length()) {
+            if(pattern[length]==pattern[i]) {
+                length++;
+                lps[i]=length;
+                i++;
+            } else {
+                if(length!=0) length = lps[length-1];
+                else i++;
+            }
+        }
+        return lps;
     }
 };
+
+
+/*
+
+    
+
+*/
