6e29aa7d1ca2cfbabde50722a86b143e005f27b4
Amit Choraria <jainamit130@gmail.com>
2025-02-22 16:14:37 +0530

    Time: 832 ms (44.97%), Space: 112.9 MB (68.54%) - LeetHub

--- 3459-length-of-longest-v-shaped-diagonal-segment/3459-length-of-longest-v-shaped-diagonal-segment.cpp
+++ 3459-length-of-longest-v-shaped-diagonal-segment/3459-length-of-longest-v-shaped-diagonal-segment.cpp
@@ -1,76 +1,59 @@
 class Solution {
 public:
-    unordered_map<int, vector<int>> dirMp = {
-        {1, {-1, 1}}, {2, {1, 1}}, {3, {1, -1}}, {4, {-1, -1}}};
-    
-    unordered_map<int, int> clockwise = {{1, 2}, {2, 3}, {3, 4}, {4, 1}};
-    int dp[501][501][2][4][2];
-    int m, n;
-
+    int m,n;
+    int dp[501][501][4][2][2];
+    unordered_map<int,vector<int>> dirMp = { {0,{-1,1}}, {1,{1,1}}, {2,{1,-1}}, {3,{-1,-1}} };
     int lenOfVDiagonal(vector<vector<int>>& grid) {
         int ans = 0;
         m = grid.size();
         n = grid[0].size();
-        memset(dp , -1 ,sizeof(dp));
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1) {  
-                    for (int k = 1; k <= 4; k++) { 
-                        ans = max(ans,1+ solve(grid, i+dirMp[k][0], j+dirMp[k][1], 0, k, 1));  
+        memset(dp,-1,sizeof(dp));
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+                if(grid[i][j]==1) {
+                    for(int k = 0;k<=3;k++) {
+                        ans = max(ans,1+solve(grid, i + dirMp[k][0], j+dirMp[k][1],k,0,1));
                     }
                 }
             }
         }
-        
         return ans;
     }
 
-    bool isValid(int row, int col) {
-        return row >= 0 && row < m && col >= 0 && col < n;
+    bool isValid(int row,int col) {
+        return row>=0 && row<m && col>=0 && col<n;
     }
 
-    int solve(vector<vector<int>>& grid, int row, int col, int turn, int dir, int shouldBe2) {
+    int solve(vector<vector<int>>& grid,int row,int col,int dir,int turn,int order) {
         if(!isValid(row,col)) return 0;
-        if(shouldBe2 && grid[row][col]!=2) return 0;
-        if(!shouldBe2 && grid[row][col]!=0) return 0;
-        if(dp[row][col][turn][dir-1][shouldBe2]!=-1) return dp[row][col][turn][dir-1][shouldBe2];
-        int ans = 0; 
+        if((order && grid[row][col]!=2) || (!order && grid[row][col]!=0)) return 0;
+        if(dp[row][col][dir][turn][order]!=-1) return dp[row][col][dir][turn][order];
+        int length = 0;
         
-        int newRow = row + dirMp[dir][0];
-        int newCol = col + dirMp[dir][1];
-        
-        ans = max(ans, 1 + solve(grid, newRow, newCol, turn, dir, !shouldBe2));
-
-        if (!turn) {
-            vector<int> newDir = dirMp[clockwise[dir]];
-            ans = max(ans, 1 + solve(grid, row + newDir[0], col + newDir[1], 1, clockwise[dir], !shouldBe2));
+        // either to go forward
+        length = max(length,1+solve(grid,row+dirMp[dir][0],col+dirMp[dir][1],dir,turn,!order));
+        // or to change direction
+        if(!turn) {
+            int newDir = (dir+1)%4;
+            length = max(length,1+solve(grid,row+dirMp[newDir][0],col+dirMp[newDir][1],newDir,1,!order));
         }
-        
-        return dp[row][col][turn][dir-1][shouldBe2]=ans;
+        return dp[row][col][dir][turn][order]=length;
     }
 };
 
 
 /*
-
-2,1,2,2],[2,0,1,0],[2,2,2,2],[0,0,0,0],[2,2,2,2
-
-
-2   1   2   2
-2   0   1   0
-2   2   2   2
-0   0   0   0
-2   2   2   2
+diagonal directions
+1   1   
+-1  1
 
 
-2,2,0,2,0,2,0],[1,2,2,1,0,2,0
+States
 
-2   2   0   2   0   2   0
-1   2   2   1   0   2   0
-
-4,1 -> 5,0 => 6,-1 => 6%5,-1%4 => 1,3
-
-4,3 -> 5,4 => 1,0
-
+row
+col
+dir
+turn
+order
 
 */
