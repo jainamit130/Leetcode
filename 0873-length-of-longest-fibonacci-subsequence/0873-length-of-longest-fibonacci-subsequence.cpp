aa5cfd123f66217e4932254566fef6b462829257
Amit Choraria <jainamit130@gmail.com>
2025-02-27 17:08:54 +0530

    Time: 267 ms (81.84%), Space: 15.8 MB (74.57%) - LeetHub

---
+++ 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int lenLongestFibSubseq(vector<int>& arr) {
+        int ans = 2;
+        unordered_set<int> st(arr.begin(),arr.end());
+        for(int i=0;i<arr.size()-2;i++) {
+            for(int j=i+1;j<arr.size()-1;j++) {
+                int num1 = arr[i];
+                int num2 = arr[j];
+                int subAns = 2;
+                // generate as long as we can
+                while(st.find(num1+num2)!=st.end()) {
+                    int temp = num2;
+                    num2 = num1+num2;
+                    num1 = temp;
+                    subAns++;
+                }
+                ans = max(ans,subAns);
+            }
+        }
+        return ans<=2?0:ans;
+    }
+};
