cdee45c8ea40d9d6635430765d93d42f04feb13e
Amit Choraria <jainamit130@gmail.com>
2025-03-10 21:45:17 +0530

    Time: 2665 ms (5.2%), Space: 412.1 MB (5.48%) - LeetHub

--- 3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii.cpp
+++ 3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii.cpp
@@ -1,39 +1,41 @@
 class Solution {
 public:
-    unordered_map<char,int> vowelIndex = {{'a',0} , {'e',1} , {'i',2} , {'o',3} , {'u',4} };
+    unordered_map<char,int> vowelIndex = { {'a',0}, {'e',1},{'o',2},{'i',3},{'u',4} };
     long long countOfSubstrings(string word, int k) {
         int n=word.length();
         vector<vector<int>> vowelPrefix(n+1,vector<int>(6));
+
         for(int i=0;i<n;i++){
             vowelPrefix[i+1]=vowelPrefix[i];
             if(vowelIndex.find(word[i])!=vowelIndex.end()){
                 vowelPrefix[i+1][vowelIndex[word[i]]]++;
                 vowelPrefix[i+1][5]++;
-            } 
+            }
         }
 
         long long ans=0;
         for(int i=0;i<n;i++){
-            int firstVowelValidIndex = getValidIndex(word,i,vowelPrefix,0,k);
-            int lastKValidIndex = getValidIndex(word,i,vowelPrefix,1,k);
-            if(firstVowelValidIndex<=lastKValidIndex){
-                ans+=(long long)lastKValidIndex-firstVowelValidIndex+1;
+            int start=binarySearch(i,vowelPrefix,k,0);
+            int end=binarySearch(i,vowelPrefix,k,1);
+            if(start<=end){
+                ans+=(long long)end-start+1;
             }
         }
         return ans;
     }
 
-    int getValidIndex(string& word,int index,vector<vector<int>>& vowelPrefix,int choice,int k){
+    int binarySearch(int index,vector<vector<int>>& vowelPrefix,int k,int choice){
+        int n=vowelPrefix.size()-1;
         int start=index;
-        int end=word.length()-1;
-        int ans=(choice==1)?start-1:word.length();
+        int end=n-1;
+        int ans=(choice==0)?n:start-1;
         while(start<=end){
-            int mid = (start+end)/2;
-
+            int mid=(start+end)/2;
             if(choice==0){
                 bool valid=true;
                 for(int i=0;i<5;i++){
-                    if(vowelPrefix[mid+1][i]-vowelPrefix[index][i]==0){
+                    int vowelCount = vowelPrefix[mid+1][i]-vowelPrefix[index][i];
+                    if(vowelCount==0){
                         valid=false;
                         break;
                     }
@@ -61,12 +63,11 @@ public:
 
 
 /*
-k=1;
-0   1   2   3   4   5   6   7   8   9   10  11  12  13
-i   e   a   o   u   q   q   i   e   a   o   u   q   q
-0   0   0   0   0   1   2   2   2   2   2   2   3   4
 
+consonentCount = total Char - totalVowels 
 
+BS1: vowelCondition + consonentCount>=k
+BS2: consonentCount<=k
 
 
 
