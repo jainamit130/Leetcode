828d67c5b4660645b3d1ec60f9f3b42a3a26957c
Amit Choraria <jainamit130@gmail.com>
2025-03-13 23:48:00 +0530

    Time: 62 ms (56.16%), Space: 318.7 MB (100%) - LeetHub

--- 3356-zero-array-transformation-ii/3356-zero-array-transformation-ii.cpp
+++ 3356-zero-array-transformation-ii/3356-zero-array-transformation-ii.cpp
@@ -1,33 +1,38 @@
 class Solution {
 public:
     int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
-        int n = nums.size();
         int start=0;
-        int end = queries.size()-1;
+        int end= queries.size()-1;
+        int n = nums.size();
         int ans = -1;
         while(start<=end) {
             int mid = (start+end)/2;
             vector<int> prefix(n+1);
-            for(int i=0;i<=mid;i++){
+            for(int i=0;i<=mid;i++) {
                 prefix[queries[i][0]]+=queries[i][2];
                 prefix[queries[i][1]+1]-=queries[i][2];
             }
 
             int flag = 0;
+            int flag1 = 0;
             for(int i=1;i<=n;i++){
                 prefix[i]+=prefix[i-1];
+                if(nums[i-1]!=0) {
+                    flag1=1;
+                }
                 if(prefix[i-1]<nums[i-1]){
                     flag=1;
                     break;
                 }
             }
 
-            if(!flag){
-                ans = mid+1;
-                end=mid-1;
+            if(!flag) {
+                ans = flag1?mid+1:mid;
+                end = mid-1;
             } else {
-                start=mid+1;
+                start = mid+1;
             }
+
         }
         return ans;
     }
