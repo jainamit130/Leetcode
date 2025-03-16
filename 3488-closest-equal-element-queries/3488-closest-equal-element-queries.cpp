4457f1248c21e30cb4153b4d543c40b04bccca90
Amit Choraria <jainamit130@gmail.com>
2025-03-16 23:05:59 +0530

    Time: 291 ms (100%), Space: 216.9 MB (100%) - LeetHub

--- 3488-closest-equal-element-queries/3488-closest-equal-element-queries.cpp
+++ 3488-closest-equal-element-queries/3488-closest-equal-element-queries.cpp
@@ -10,25 +10,23 @@ public:
         for(auto index:queries) {
             int dist = INT_MAX;
             auto mpIndex = lower_bound(indexMp[nums[index]].begin(),indexMp[nums[index]].end(),index)-indexMp[nums[index]].begin();
+            // check 1 index before
             int beforeIndex = mpIndex-1;
-            if(beforeIndex>=0) {
-                dist = min(dist,(int)abs(index-indexMp[nums[index]][beforeIndex]));
-            }
-
+            if(beforeIndex>=0) dist = min(dist,abs(index-indexMp[nums[index]][beforeIndex]));
+            // check 1 index after
             int afterIndex = mpIndex+1;
-            if(afterIndex!=indexMp[nums[index]].size()) {
-                dist = min(dist,(int)abs(index-indexMp[nums[index]][afterIndex]));
-            }
-
-            // wrap around cases
+            if(afterIndex<indexMp[nums[index]].size()) dist = min(dist,abs(index-indexMp[nums[index]][afterIndex]));
+            // extreme left check
             if(!indexMp[nums[index]].empty() && indexMp[nums[index]][0]!=index) {
-                dist = min({dist,(int)nums.size()-(int)abs(indexMp[nums[index]][0]-index)});
+                int diff = abs(indexMp[nums[index]][0]-index);
+                dist = min({dist,diff,(int)nums.size()-diff});
             }
-
+            // extreme right check
             if(!indexMp[nums[index]].empty() && indexMp[nums[index]].back()!=index) {
-                dist = min({dist,(int)nums.size()-(int)abs(indexMp[nums[index]].back()-index)});
+                int diff = abs(indexMp[nums[index]].back()-index);
+                dist = min({dist,diff,(int)nums.size()-diff});
             }
-            ans.push_back(dist==INT_MAX?-1:dist);
+            ans.push_back(dist==INT_MAX?-1:dist); 
         }
         return ans;
     }
@@ -36,14 +34,21 @@ public:
 
 
 /*
-
-1   3   1   4   1   3   1
 0   1   2   3   4   5   6
+1   3   1   4   1   3   2
 i
 
-1 -> 0,2,4
-2 -> 6
+     0   1   2
+1 -> 0   2   4
+2 -> 6 
 3 -> 1,5
 4 -> 3
+
+
+0   1   2   3   4   5
+
+0   3   5
+
+
 
 */
