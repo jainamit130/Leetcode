0ead14767a12a923393f5f20b26566e28062bdef
Amit Choraria <jainamit130@gmail.com>
2025-03-19 17:56:43 +0530

    Time: 1575 ms (10.64%), Space: 591.8 MB (11.79%) - LeetHub

--- 3485-longest-common-prefix-of-k-strings-after-removal/3485-longest-common-prefix-of-k-strings-after-removal.cpp
+++ 3485-longest-common-prefix-of-k-strings-after-removal/3485-longest-common-prefix-of-k-strings-after-removal.cpp
@@ -1,30 +1,32 @@
 class Trie {
-    public:
+    public: 
         unordered_map<char,Trie*> children;
         int count;
         Trie() {
             count = 0;
         }
-        
-        void addWord(Trie* root,string& word, map<int,unordered_set<Trie*>>& mp,int k){
-            for(int i=0;i<word.length();i++){
+
+        void addWord(string& word,map<int,unordered_set<Trie*>>& mp,int k) {
+            Trie* root = this;
+            for(int i=0;i<word.length();i++) {
                 char ch = word[i];
-                if(root->children.find(ch-'a')==root->children.end()){
-                    root->children[ch-'a']=new Trie();
+                if(root->children.find(ch)==root->children.end()) {
+                    root->children[ch] = new Trie();
                 }
-                root=root->children[ch-'a'];
+                root=root->children[ch];
                 root->count++;
                 if(root->count>=k) {
                     mp[i+1].insert(root);
                 }
             }
             return;
-        } 
+        }
 
-        void removeWord(Trie* root,string word,map<int,unordered_set<Trie*>>& mp,int k) {
-            for(int i=0;i<word.length();i++){
+        void removeWord(string& word,map<int,unordered_set<Trie*>>& mp,int k) {
+            Trie* root = this;
+            for(int i=0;i<word.length();i++) {
                 char ch = word[i];
-                root=root->children[ch-'a'];
+                root=root->children[ch];
                 root->count--;
                 if(root->count+1==k) {
                     mp[i+1].erase(root);
@@ -40,38 +42,33 @@ class Trie {
 class Solution {
 public:
     vector<int> longestCommonPrefix(vector<string>& words, int k) {
-        Trie* root=new Trie();
+        Trie* root = new Trie();
         map<int,unordered_set<Trie*>> mp;
-        for(int i=0;i<words.size();i++){
-            root->addWord(root,words[i],mp,k);
+        for(auto word:words) {
+            root->addWord(word,mp,k);
         }
-        vector<int> ans(words.size());
-        for(int i=0;i<words.size();i++){
-            root->removeWord(root,words[i],mp,k);
-            if(mp.empty()) {
-                root->addWord(root,words[i],mp,k);
-                continue;    
-            }
-            ans[i] = mp.rbegin()->first;
 
-            root->addWord(root,words[i],mp,k);
+        vector<int> ans(words.size());
+        for(int i=0;i<words.size();i++) {
+            root->removeWord(words[i],mp,k);
+            if(!mp.empty()) ans[i]=mp.rbegin()->first;
+            root->addWord(words[i],mp,k);
         }
         return ans;
     }
 };
 
 /*
+        l,k
+jump -> 4,2
+run ->  3,2
 
+Ex:
 
+jum
 jump
+jummer
 run
-run
-jump
-run
-
-k=2
-
-
-
+runner
 
 */
