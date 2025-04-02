078d088afede373a54a0b5f76fc60d42eed38159
Amit Choraria <jainamit130@gmail.com>
2025-04-03 00:17:04 +0530

    Time: 2 ms (13.11%), Space: 9.1 MB (12.04%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,27 +1,40 @@
 class Solution {
 public:
+    unordered_map<char,char> openEquivalentMp = {
+        {'}','{'},
+        {')','('},
+        {']','['},
+    };
+    unordered_set<char> openBrackets = {'[','{','('};
     bool isValid(string s) {
-        stack<int> q;
-        int c=0;
-        map<char,char> mp={{'}','{'},{')','('},{']','['}};
-        for(int i=0;i<s.length();i++){
-            if(s[i]=='{' || s[i]=='(' || s[i]=='['){
-                q.push(s[i]);
-                c++;
-            }
-            else if(s[i]=='}' || s[i]==')' || s[i]==']'){
-                c--;
-                if(c<0)
-                    return false;
-                if(q.top()!=mp[s[i]])
-                    return false;
-                else{
-                    q.pop();
-                }
+        stack<char> st;
+        for(auto ch:s) {
+            if(openBrackets.find(ch)!=openBrackets.end()) st.push(ch);
+            else {
+                if(st.empty() || st.top()!=openEquivalentMp[ch]) return false;
+                st.pop();
             }
         }
-        if(c!=0)
-            return false;
-        return true;
+        return st.empty();
     }
 };
+
+
+/*
+Clarifications
+
+string is given ? yes
+only brackets => {},[],()? yes 
+if empty string return true ? yes
+
+
+Approach
+initializa a stack
+start iterating the string s 
+if open brackets encountered then push in the stack
+if close brackets encountered then check top of stack if equivalent open bracket present then pop if not return false from there
+if iteration completed then check if stack is empty or not
+
+Time => O(n)
+Space => O(n)
+*/
