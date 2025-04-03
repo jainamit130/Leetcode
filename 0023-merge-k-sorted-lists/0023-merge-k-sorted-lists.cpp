61efed01f3949cd2f360ff4a74a7707285157486
Amit Choraria <jainamit130@gmail.com>
2025-04-03 17:52:36 +0530

    Time: 4 ms (48.75%), Space: 19.6 MB (9.64%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,46 +10,35 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoSortedLists(ListNode* l1, ListNode* l2) {
-        if(!l1)
-            return l2;
-        if(!l2)
-            return l1;
+    using pil = pair<int, ListNode*>;
 
-        if(l1->val <= l2->val) {
-            l1->next = mergeTwoSortedLists(l1->next, l2);
-            return l1;
-        } else {
-            l2->next = mergeTwoSortedLists(l1, l2->next);
-            return l2;
-        }
-        
-        return NULL;
-    }
-    
-    ListNode* partitionAndMerge(int start, int end, vector<ListNode*>& lists) {
-        if(start == end)
-            return lists[start];
-        
-        if(start > end)
-            return NULL;
-        
-        int mid = start + (end-start)/2;
-        
-        ListNode* l1 = partitionAndMerge(start, mid, lists);
-        ListNode* l2 = partitionAndMerge(mid+1, end, lists);
-        
-        return mergeTwoSortedLists(l1, l2);
-    }
-    
+public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        
-        int n = lists.size();
-        
-        if(n == 0)
-            return NULL;
-        
-        return partitionAndMerge(0, n-1, lists);
-        
+        int k = lists.size();
+        vector<ListNode*> ptrs(k, NULL);
+        priority_queue<pil, vector<pil>, greater<pil>> minHeap;
+        for (int i = 0; i < k; i++) {
+            ptrs[i] = lists[i];
+            if(ptrs[i]) minHeap.push({ptrs[i]->val, ptrs[i]});
+        }
+        ListNode* ans = NULL;
+        ListNode* ansPtr = NULL;
+        while (!minHeap.empty()) {
+            auto [val, ptr] = minHeap.top();
+            minHeap.pop();
+            if (ans == NULL) {
+                ans = new ListNode(val);
+                ansPtr = ans;
+            } else {
+                ansPtr->next = new ListNode(val);
+                ansPtr = ansPtr->next;
+            }
+
+            ptr = ptr->next;
+            if (ptr) {
+                minHeap.push({ptr->val, ptr});
+            }
+        }
+        return ans;
     }
 };
