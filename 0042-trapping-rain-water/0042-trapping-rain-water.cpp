75725171ad437eecc8830cb1dbf4e16db85f1c51
Amit Choraria <jainamit130@gmail.com>
2025-04-04 00:14:08 +0530

    Time: 0 ms (100%), Space: 26.6 MB (46.67%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,47 +1,25 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        stack<vector<int>> st;
-        int n=height.size();
-        vector<vector<int>> nextHeight(n,{-1,-1});
-        for(int i=n-1;i>=0;i--){
-            vector<int> nextGreater={-1,-1};
-            while(!st.empty() && st.top()[0]<height[i]){
-                if(nextGreater[0]<st.top()[0]){
-                    nextGreater=st.top();
-                }
-                st.pop();
-            }
-            if(!st.empty()){
-                nextGreater=st.top();
-            }
-            nextHeight[i]=nextGreater;
-            st.push({height[i],i});
-        }
-
-        bool noSnowMode=false;
-        int endActivation=-1;
-        int ans=0;
-        for(int i=0;i<n;){
-            if(nextHeight[i][0]==-1){
-                i++;
-                continue;
-            }
-            if(noSnowMode){
-                if(i==endActivation){
-                    noSnowMode=false;
-                    endActivation=-1;
-                } else {
-                    ans-=height[i];
-                    i++;
-                }
-            } else {
-                ans+=abs(nextHeight[i][1]-i-1)*min(height[i],nextHeight[i][0]);
-                noSnowMode=true;
-                endActivation=nextHeight[i][1];
-                i++;
-            }
+        int n = height.size();
+        vector<int> rightMax(n,0);
+        for(int i=n-2;i>=0;i--) rightMax[i]=max(rightMax[i+1],height[i+1]);
+        int leftMax = 0;
+        int ans = 0;
+        for(int i=1;i<n;i++) {
+            leftMax = max(leftMax,height[i-1]);
+            if(leftMax && rightMax[i]) ans += max(min(rightMax[i],leftMax)-height[i],0);
         }
         return ans;
     }
 };
+
+/*
+
+
+0   1   0   2   1   0   1   3   2   1   2   1
+0   1   2   3   4   5   6   7   8   9   10  11
+
+
+
+*/
