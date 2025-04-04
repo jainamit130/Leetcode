59f5dcfe822e99dfde2314b0d66a9fdf6afda649
Amit Choraria <jainamit130@gmail.com>
2025-04-04 14:33:23 +0530

    Time: 17 ms (23.33%), Space: 30.6 MB (5.9%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -8,37 +8,101 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+
+ /*
+ 
+ 
+[]   []   []  []  []  []  []
+[]   []   []  [] => []  []     []   []
+[]   []   [] 
+ 
+ */
+
 class Solution {
 public:
-    using pil = pair<int, ListNode*>;
-
-public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        int k = lists.size();
-        vector<ListNode*> ptrs(k, NULL);
-        priority_queue<pil, vector<pil>, greater<pil>> minHeap;
-        for (int i = 0; i < k; i++) {
-            ptrs[i] = lists[i];
-            if(ptrs[i]) minHeap.push({ptrs[i]->val, ptrs[i]});
-        }
+        return partition(0,lists.size()-1,lists);     
+    }
+
+    ListNode* partition(int start,int end,vector<ListNode*>& lists) {
+        if(start>end) return NULL;
+        if(start==end) return lists[start];
+        int mid = (start+end)/2;
+        ListNode* list1 = partition(start,mid,lists);
+        ListNode* list2 = partition(mid+1,end,lists);
+        return merge(list1,list2);
+    }
+
+    ListNode* merge(ListNode* list1,ListNode* list2) {
         ListNode* ans = NULL;
-        ListNode* ansPtr = NULL;
-        while (!minHeap.empty()) {
-            auto [val, ptr] = minHeap.top();
-            minHeap.pop();
-            if (ans == NULL) {
-                ans = new ListNode(val);
-                ansPtr = ans;
+        ListNode* ptr = NULL;
+        ListNode* ptr1 = list1;
+        ListNode* ptr2 = list2;
+        while(ptr1 || ptr2) {
+            int val = INT_MAX;
+            if(ptr1) {
+                if(ptr2) {
+                    if(ptr1->val<=ptr2->val) {
+                        val = min(val,ptr1->val);
+                        ptr1 = ptr1->next;
+                    } else {
+                        val = min(val,ptr2->val);
+                        ptr2 = ptr2->next;
+                    }
+                } else {
+                    val = min(val,ptr1->val);
+                    ptr1 = ptr1->next;
+                }
             } else {
-                ansPtr->next = new ListNode(val);
-                ansPtr = ansPtr->next;
+                val = min(val,ptr2->val);
+                ptr2 = ptr2->next;
             }
 
-            ptr = ptr->next;
-            if (ptr) {
-                minHeap.push({ptr->val, ptr});
+            ListNode* node = new ListNode(val);
+            if(!ans) {
+                ans = node;
+                ptr = node;
+            } else {
+                ptr->next = node;
+                ptr=ptr->next;
             }
         }
         return ans;
     }
 };
+
+// class Solution {
+// public:
+//     using pil = pair<int, ListNode*>;
+
+// public:
+//     ListNode* mergeKLists(vector<ListNode*>& lists) {
+//         int k = lists.size();
+//         vector<ListNode*> ptrs(k, NULL);
+//         priority_queue<pil, vector<pil>, greater<pil>> minHeap;
+//         for (int i = 0; i < k; i++) {
+//             ptrs[i] = lists[i];
+//             if(ptrs[i]) minHeap.push({ptrs[i]->val, ptrs[i]});
+//         }
+//         ListNode* ans = NULL;
+//         ListNode* ansPtr = NULL;
+//         while (!minHeap.empty()) {
+//             auto [val, ptr] = minHeap.top();
+//             minHeap.pop();
+//             if (ans == NULL) {
+//                 ans = new ListNode(val);
+//                 ansPtr = ans;
+//             } else {
+//                 ansPtr->next = new ListNode(val);
+//                 ansPtr = ansPtr->next;
+//             }
+
+//             ptr = ptr->next;
+//             if (ptr) {
+//                 minHeap.push({ptr->val, ptr});
+//             }
+//         }
+//         return ans;
+//     }
+// };
