316e98fb93106db7c0258fc3bde5dc7244461fd3
Amit Choraria <jainamit130@gmail.com>
2025-04-08 15:57:07 +0530

    Time: 0 ms (100%), Space: 7.5 MB (100%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,80 +1,32 @@
 class Solution {
 public:
-    vector<int> ans;
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        solve(matrix,1,1,0,matrix.size()-1,0,matrix[0].size()-1); 
-        return ans;
-    }
+        int left=0,right=matrix[0].size()-1;
+        int top=0,bottom=matrix.size()-1;
+        vector<int> res;
 
-    /*
-    
-    isHor = true
-    isFor = true
-    
-    isHor = false
-    isFor = true
-
-    isHor = true
-    isFor = false
-    
-    isHor = false
-    isFor = false
-    */
-
-    void solve(vector<vector<int>>& matrix,int isHor,int isFor,int rowStart,int rowEnd,int colStart,int colEnd) {
-        if(ans.size()==(matrix[0].size()*matrix.size())) return;
-        if(isFor) {
-            if(isHor) {
-                int row = rowStart;
-                int col = colStart;
-                while(col<=colEnd) {
-                    ans.push_back(matrix[row][col]);
-                    col++;
-                }
-                solve(matrix,!isHor,isFor,rowStart+1,rowEnd,colStart,colEnd);
-            } else {
-                int row = rowStart;
-                int col = colEnd;
-                while(row<=rowEnd) {
-                    ans.push_back(matrix[row][col]);
-                    row++;
-                }
-                solve(matrix,!isHor,!isFor,rowStart,rowEnd,colStart,colEnd-1);
+        while(top<=bottom && left<=right){
+            for(int i=left;i<=right;i++){
+                res.push_back(matrix[top][i]);
             }
-        } else {
-            if(isHor) {
-                int row = rowEnd;
-                int col = colEnd;
-                while(col>=colStart) {
-                    ans.push_back(matrix[row][col]);
-                    col--;
+            top++;
+            for(int i=top;i<=bottom;i++){
+                res.push_back(matrix[i][right]);
+            }
+            right--;
+            if(top<=bottom){
+                for(int i=right;i>=left;i--){
+                    res.push_back(matrix[bottom][i]);
                 }
-                solve(matrix,!isHor,isFor,rowStart,rowEnd-1,colStart,colEnd);
-            } else {
-                int row = rowEnd;
-                int col = colStart;
-                while(row>=rowStart) {
-                    ans.push_back(matrix[row][col]);
-                    row--;
+                bottom--;
+            }
+            if(left<=right){
+                for(int i=bottom;i>=top;i--){
+                    res.push_back(matrix[i][left]);
                 }
-                solve(matrix,!isHor,!isFor,rowStart,rowEnd,colStart+1,colEnd);
+                left++;
             }
         }
+        return res;
     }
 };
-
-/*
-
-rowStart ------ rowEnd
-colStart ------ colEnd
-
-
-when isHor,isFor then rowEnd--;
-when isHor,!isFor then rowStart++;
-when isVer,!isFor then colStart++;
-when isVer,isFor then colEnd--;
-
-
-
-
-*/
