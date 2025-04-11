1e2e9f1d23da4d91932838c56e830c2b4d149085
Amit Choraria <jainamit130@gmail.com>
2025-04-11 22:59:04 +0530

    Time: 97 ms (24.48%), Space: 88.7 MB (50.79%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,44 +1,73 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        unordered_map<int,int> startToEnd;
-        unordered_map<int,int> endToStart;
+        unordered_set<int> st(nums.begin(),nums.end());
         int ans = 0;
-        for(auto n:nums) {
-            int nextNum = n+1;
-            int prevNum = n-1;
-            if(startToEnd.find(nextNum)!=startToEnd.end() && endToStart.find(prevNum)!=endToStart.end()) {
-                int start = endToStart[prevNum];
-                int end = startToEnd[nextNum];
-                endToStart.erase(prevNum);
-                endToStart[end]=start;
-                startToEnd[start]=end;
-                ans = max(ans,end-start+1);
-            } else if(startToEnd.find(nextNum)!=startToEnd.end()) {
-                int end = startToEnd[nextNum];
-                endToStart[end] = n;
-                startToEnd[n] = end;
-                startToEnd.erase(nextNum);
-                ans = max(ans,end-n+1);  
-            } else if(endToStart.find(prevNum)!=endToStart.end()) {
-                int start = endToStart[prevNum];
-                startToEnd[start] = n;
-                endToStart[n] = start;
-                endToStart.erase(prevNum);
-                ans = max(ans,n-start+1);  
-            } else {
-                startToEnd[n]=n;
-                endToStart[n]=n;
-                ans = max(ans,1);
+        for(auto n:st) {
+            if(st.find(n-1)==st.end()) {
+                int nextNum = n+1;
+                int subAns = 1;
+                while(st.find(nextNum)!=st.end()) {
+                    subAns++;
+                    nextNum++;
+                }
+                ans = max(subAns,ans);
             }
         }
         return ans;
     }   
-
 };
+
+// class Solution {
+// public:
+//     int longestConsecutive(vector<int>& nums) {
+//         unordered_map<int,int> startToEnd;
+//         unordered_map<int,int> endToStart;
+//         unordered_set<int> st;
+//         int ans = 0;
+//         for(auto n:nums) {
+//             if(st.find(n)!=st.end()) continue;
+//             int nextNum = n+1;
+//             int prevNum = n-1;
+//             if(startToEnd.find(nextNum)!=startToEnd.end() && endToStart.find(prevNum)!=endToStart.end()) {
+//                 int start = endToStart[prevNum];
+//                 int end = startToEnd[nextNum];
+//                 endToStart.erase(prevNum);
+//                 endToStart[end]=start;
+//                 startToEnd[start]=end;
+//                 ans = max(ans,end-start+1);
+//             } else if(startToEnd.find(nextNum)!=startToEnd.end()) {
+//                 int end = startToEnd[nextNum];
+//                 endToStart[end] = n;
+//                 startToEnd[n] = end;
+//                 startToEnd.erase(nextNum);
+//                 ans = max(ans,end-n+1);  
+//             } else if(endToStart.find(prevNum)!=endToStart.end()) {
+//                 int start = endToStart[prevNum];
+//                 startToEnd[start] = n;
+//                 endToStart[n] = start;
+//                 endToStart.erase(prevNum);
+//                 ans = max(ans,n-start+1);  
+//             } else {
+//                 startToEnd[n]=n;
+//                 endToStart[n]=n;
+//                 ans = max(ans,1);
+//             }
+//             st.insert(n);
+//         }
+//         return ans;
+//     }   
+
+// };
 
 
 /*
+
+97  100 102 105 101 104 98  103 99
+
+97          100         102         105                  
+
+
 End
 105 -> 97
 
