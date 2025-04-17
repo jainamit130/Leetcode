de5c1afb4dd87ae6d60fdb352cefa1be422f0734
Amit Choraria <jainamit130@gmail.com>
2025-04-17 21:58:41 +0530

    Time: 16 ms (2.6%), Space: 27.4 MB (100%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,40 +1,12 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        if(k==0) return;
-        int n = nums.size();
-        int swaps = 0;
-        int index = 0;
-        int val = nums[index];
-        int count = 0;
-        while(swaps<n) {
-            int newIndex = (index + k)%n;
-            int temp = val;
-            val = nums[newIndex];
-            nums[newIndex] = temp;
-            index = newIndex;
-            count+=k;
-            if(count%n==0) {
-                count=0;
-                index=(index+1)%n;
-                val=nums[index];
-            }
-            swaps++;
-        }
+        if(k==nums.size() || k==0)
+            return;
+        k=k%nums.size();
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
+        return;
     }
 };
-
-/*
-1   2   3   4   5   6
-
-
-1   2   3   4   1   6
-
-1   2   5   4   1   6
-
-
-
-
-
-
-*/
