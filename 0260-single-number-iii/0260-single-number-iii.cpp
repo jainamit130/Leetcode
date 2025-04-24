97bfe5af63f911982d73a0ef3b951380fb2592d8
Amit Choraria <jainamit130@gmail.com>
2025-04-24 16:48:45 +0530

    Time: 7 ms (9.84%), Space: 12.3 MB (100%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        int xors = 0;
-        for(auto n:nums) {
-            xors^=n;
+        int r=nums[0];
+        for(int i=1;i<nums.size();i++){
+            r^=nums[i];
         }
-
-        int index = 0;
-        while(xors) {
-            if(xors&1) break;
-            index++;
-            xors=xors>>1;
+        int i=0;
+        while(1){
+            if((r>>i)&1)
+                break;
+            i++;
         }
-
-        int a=0,b=0;
-        for(auto n:nums) {
-            if((n>>index)&1) a^=n;
-            else b^=n;
+        int a=0;
+        int b=0;
+        for(int j=0;j<nums.size();j++){
+            if((nums[j]>>i)&1)
+                a^=nums[j];
+            else
+                b^=nums[j];
         }
         return {a,b};
     }
@@ -24,21 +25,20 @@ public:
 
 
 
-/*
-1   1   2   2   3   5
+// 1   2   1   3   2   5
 
+// 3   ^   5
 
-0   0   0   1
-0   0   0   1
-0   0   1   0
-0   0   1   0
-0   0   1   1
-0   1   0   1
---------------
-0   1   1   0
+// 011 ^   101 =   110
 
+// 0   0   0   0   1
+// 0   0   0   1   0
+// 0   0   0   0   1
+// 0   0   0   1   0
 
+// 0   0   0   1   1       
 
+// 0   0   1   0   1
 
+// 0   0   1   1   0
 
-*/
