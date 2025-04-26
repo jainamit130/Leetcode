18716018d670d2132cfd426e9c039e052fe95ca8
Amit Choraria <jainamit130@gmail.com>
2025-04-26 13:56:29 +0530

    Time: 3 ms (34.25%), Space: 14 MB (76.06%) - LeetHub

---
+++ 0833-find-and-replace-in-string/0833-find-and-replace-in-string.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    string findReplaceString(string s, vector<int>& indices, vector<string>& sources, vector<string>& targets) {
+        unordered_map<int,pair<string,int>> changeMap;
+        for(int i=0;i<indices.size();i++) {
+            int index = indices[i];
+            string source = sources[i];
+            if(source.length()<=s.length()-index && s.substr(index,(int)source.length())==source) {
+                changeMap[index]={targets[i],source.length()};
+            }
+        }
+
+        string ans = "";
+        int i = 0;
+        while(i<s.length()) {
+            if(changeMap.find(i)!=changeMap.end()) {
+                ans += changeMap[i].first;
+                i += changeMap[i].second;
+            } else {
+                ans += s[i];
+                i++;
+            }
+        }
+        return ans;
+    }
+};
+
+
+/*
+0   1   2   3   4   5
+a   b   c   d   e   f   g   h
+        j   k   l   m
+2
+cde
+
+Given
+
+string => s - all small case english alphabets? yes, can it be empty? no
+all 3 below of length k => >0
+indices => vector<int> => size k => always within <s.length()
+sources => vector<string> => size k
+targets => vector<string> => size k
+
+Ex:
+0   1   2   3   4   5   6   7   8   9
+a   b   d   h   g   y   c   b   d   a
+
+[3,0,8]
+["hgy","abh","da"]
+["g","sdz","xyz"]
+
+
+Approach:
+
+1. string ans = "" 
+2. iterate the indices array with it get the substring from sources => check if sources[indices[i]]==s.substr(s.begin()+indices[i],sources[indices[i]].length()) if yes then add it to changeMap (index->new substring) mapping
+O(k) time and O(k*max(target[i].length()))
+3. basically now iterate the original string and form ans O(n) time and O(max(n,k*max(target[i].length()))) space
+
+
+*/
