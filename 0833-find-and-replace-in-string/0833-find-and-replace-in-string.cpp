3239284f0667a0bf547a5521395014a42977ceaf
Amit Choraria <jainamit130@gmail.com>
2025-04-26 14:53:45 +0530

    Time: 0 ms (100%), Space: 14 MB (76.06%) - LeetHub

--- 0833-find-and-replace-in-string/0833-find-and-replace-in-string.cpp
+++ 0833-find-and-replace-in-string/0833-find-and-replace-in-string.cpp
@@ -1,29 +1,66 @@
 class Solution {
 public:
     string findReplaceString(string s, vector<int>& indices, vector<string>& sources, vector<string>& targets) {
-        unordered_map<int,pair<string,int>> changeMap;
-        for(int i=0;i<indices.size();i++) {
-            int index = indices[i];
-            string source = sources[i];
-            if(source.length()<=s.length()-index && s.substr(index,(int)source.length())==source) {
-                changeMap[index]={targets[i],source.length()};
-            }
-        }
+        int k = sources.size();
+        int n = s.length();
+        vector<int> indexes(k);
+        iota(indexes.begin(),indexes.end(),0);
+        sort(indexes.begin(),indexes.end(),[&](auto lhs,auto rhs) {
+            return indices[lhs]<indices[rhs];
+        });
 
         string ans = "";
         int i = 0;
-        while(i<s.length()) {
-            if(changeMap.find(i)!=changeMap.end()) {
-                ans += changeMap[i].first;
-                i += changeMap[i].second;
-            } else {
-                ans += s[i];
-                i++;
+        int sIndex = 0;
+        while(sIndex<n) {
+            if(i<k) {
+                int index = indexes[i];
+                int indicesIndex = indices[index];
+                if(indicesIndex == sIndex) {
+                    string source = sources[index];
+                    if(source.length()<=n-sIndex && s.substr(sIndex,source.length())==source) {
+                        ans += targets[index];
+                        sIndex += source.length();
+                        i++;
+                    } else { 
+                        i++;
+                    }
+                    continue;
+                }
             }
+            ans += s[sIndex];
+            sIndex++;
         }
         return ans;
     }
 };
+
+// class Solution {
+// public:
+//     string findReplaceString(string s, vector<int>& indices, vector<string>& sources, vector<string>& targets) {
+//         unordered_map<int,pair<string,int>> changeMap;
+//         for(int i=0;i<indices.size();i++) {
+//             int index = indices[i];
+//             string source = sources[i];
+//             if(source.length()<=s.length()-index && s.substr(index,(int)source.length())==source) {
+//                 changeMap[index]={targets[i],source.length()};
+//             }
+//         }
+
+//         string ans = "";
+//         int i = 0;
+//         while(i<s.length()) {
+//             if(changeMap.find(i)!=changeMap.end()) {
+//                 ans += changeMap[i].first;
+//                 i += changeMap[i].second;
+//             } else {
+//                 ans += s[i];
+//                 i++;
+//             }
+//         }
+//         return ans;
+//     }
+// };
 
 
 /*
@@ -56,6 +93,11 @@ Approach:
 2. iterate the indices array with it get the substring from sources => check if sources[indices[i]]==s.substr(s.begin()+indices[i],sources[indices[i]].length()) if yes then add it to changeMap (index->new substring) mapping
 O(k) time and O(k*max(target[i].length()))
 3. basically now iterate the original string and form ans O(n) time and O(max(n,k*max(target[i].length()))) space
+
+Time
+
+O(n+k)
+O(n + k*max(target[i].length()))
 
 
 */
