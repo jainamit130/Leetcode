58782d5eaea55e0b61e0a038dc0e5b6b84bce5e6
Amit Choraria <jainamit130@gmail.com>
2025-04-27 23:37:16 +0530

    Time: 36 ms (100%), Space: 71.1 MB (77.78%) - LeetHub

--- 3529-count-cells-in-overlapping-horizontal-and-vertical-substrings/3529-count-cells-in-overlapping-horizontal-and-vertical-substrings.cpp
+++ 3529-count-cells-in-overlapping-horizontal-and-vertical-substrings/3529-count-cells-in-overlapping-horizontal-and-vertical-substrings.cpp
@@ -1,74 +1,56 @@
 class Solution {
 public:
+    int m,n;
     int countCells(vector<vector<char>>& grid, string pattern) {
         vector<int> lis = getLIS(pattern);
-        int m = grid.size();
-        int n = grid[0].size();
+        m = grid.size();
+        n = grid[0].size();
         int k = 0;
         vector<vector<int>> hor(m,vector<int>(n));
         vector<vector<int>> ver(m,vector<int>(n));
+
+        matchPattern(grid,pattern,lis,hor,1);
+        matchPattern(grid,pattern,lis,ver,0);
+
+        int ans = 0;
         for(int i=0;i<m;i++) {
             for(int j=0;j<n;j++) {
-                if(grid[i][j]==pattern[k]) {
-                    k++;
-                    if(k==pattern.length()) {hor[i][j]=pattern.length();k=lis[k-1];}
-                } else {
-                    while(k-1>=0 && grid[i][j]!=pattern[k]) k=lis[k-1];
-                    if(grid[i][j]==pattern[k]) k++;
-                }
-            }
-        }
-        k = 0;
-        for(int j=0;j<n;j++) {
-            for(int i=0;i<m;i++) {
-                if(grid[i][j]==pattern[k]) {
-                    k++;
-                    if(k==pattern.length()) {ver[i][j]=pattern.length();k=lis[k-1];}
-                } else {
-                    while(k-1>=0 && grid[i][j]!=pattern[k]) k=lis[k-1];
-                    if(grid[i][j]==pattern[k]) k++;
-                }
-            }
-        }
-
-        int count = 0;
-        for(int i=m-1;i>=0;i--) {
-            for(int j=n-1;j>=0;j--) {
-                if(hor[i][j]!=0) count=hor[i][j];
-                if(count>0) hor[i][j]=1;
-                count--;
-            }
-        }
-
-        count = 0;
-        int ans = 0;
-        for(int j=n-1;j>=0;j--) {
-            for(int i=m-1;i>=0;i--) {
-                if(ver[i][j]!=0) count=ver[i][j];
-                if(count>0) ver[i][j]=1;
                 if(ver[i][j]+hor[i][j]>=2) ans++;
-                count--;
             }
         }
-
-                // for(int i=0;i<m;i++) {
-        //     for(int j=0;j<n;j++) {
-        //         cout<<hor[i][j]<<" ";
-        //     }
-        //     cout<<endl;
-        // }
-
-        // cout<<endl;
-
-        // for(int i=0;i<m;i++) {
-        //     for(int j=0;j<n;j++) {
-        //         cout<<ver[i][j]<<" ";
-        //     }
-        //     cout<<endl;
-        // }
-
         return ans;
     }
+
+    void matchPattern(vector<vector<char>>& grid,string pattern,vector<int>& lis,vector<vector<int>>& container,bool isHor) {
+        int k = 0;
+        for(int i=0; isHor? i<m: i<n ;i++) {
+            for(int j=0; isHor? j<n: j<m; j++) {
+                char ch = isHor ? grid[i][j]:grid[j][i];
+                if(ch==pattern[k]) {
+                    k++;
+                    int& con = isHor? container[i][j] : container[j][i];
+                    if(k==pattern.length()) { con=pattern.length(); k=lis[k-1]; }
+                } else {
+                    while(k-1>=0 && ch!=pattern[k]) k=lis[k-1];
+                    if(ch==pattern[k]) k++;
+                }
+            }
+        }
+        normalizeContainer(container,isHor);
+    }
+
+    void normalizeContainer(vector<vector<int>>& container,bool isHor) {
+        int count = 0;
+        for(int i=isHor?m-1:n-1;i>=0;i--) {
+            for(int j=isHor?n-1:m-1;j>=0;j--) {
+                int& val = isHor?container[i][j]:container[j][i];
+                if(val!=0) count=val;
+                if(count>0) val=1;
+                count--;
+            }
+        }
+    }
+
 
     vector<int> getLIS(string pattern) {
         int n = pattern.length();
