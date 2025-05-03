e7b7a658572899f1bdcaf1d7cf11739c4161fa8c
Amit Choraria <jainamit130@gmail.com>
2025-05-03 21:09:54 +0530

    Time: 160 ms (25.7%), Space: 112.9 MB (10.32%) - LeetHub

--- 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.cpp
+++ 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.cpp
@@ -1,38 +1,52 @@
 class Solution {
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& nums) {
-        vector<int> ans;
-        int m = nums.size();
-        int n = 1;
-        for(int i=0;i<m;i++) {
-            // i -> 0
-            int col = 0;
-            int row = i;
-            while(row>=0) {
-                if(row+col==i && col<nums[row].size()) {
-                    ans.push_back(nums[row][col]);
-                }
-                n=max(n,(int)nums[row].size());
-                col++;
-                row--;
+        map<int,vector<int>> diagonal;
+        int maxKey = 0;
+        for(int i=0;i<nums.size();i++) {
+            for(int j=0;j<nums[i].size();j++) {
+                diagonal[i+j].push_back(nums[i][j]);
+                maxKey = max(maxKey,i+j);
             }
         }
-
-        for(int i=1;i<n;i++) {
-            // i -> 0
-            int row = m-1;
-            int col = i;
-            while(row>=0) {
-                if(row+col==i+m-1 && col<nums[row].size()) {
-                    ans.push_back(nums[row][col]);
-                }
-                row--;
-                col++;
+        vector<int> ans;
+        for(auto [key,arr]:diagonal) {
+            while(!arr.empty()) {
+                ans.push_back(arr.back());
+                arr.pop_back();
             }
         }
         return ans;
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> findDiagonalOrder(vector<vector<int>>& nums) {
+//         vector<int> ans;
+//         int m = nums.size();
+//         int n = 1;
+//         for(int i=0;i<m;i++) {
+//             int col = 0;
+//             int row = i;
+//             while(row>=0) {
+//                 if(row+col==i && col<nums[row].size()) ans.push_back(nums[row][col]);
+//                 n=max(n,(int)nums[row].size());
+//                 col++; row--;
+//             }
+//         }
+
+//         for(int i=1;i<n;i++) {
+//             int row = m-1;
+//             int col = i;
+//             while(row>=0) {
+//                 if(row+col==i+m-1 && col<nums[row].size()) ans.push_back(nums[row][col]);
+//                 row--; col++;
+//             }
+//         }
+//         return ans;
+//     }
+// };
 
 /*
 
