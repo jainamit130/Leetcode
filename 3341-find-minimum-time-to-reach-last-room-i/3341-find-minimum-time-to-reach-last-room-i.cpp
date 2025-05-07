83a88e6b2c5a298f3a49e979205ad5627246be4a
Amit Choraria <jainamit130@gmail.com>
2025-05-07 13:45:54 +0530

    Time: 58 ms (10.86%), Space: 37.5 MB (8.15%) - LeetHub

--- 3341-find-minimum-time-to-reach-last-room-i/3341-find-minimum-time-to-reach-last-room-i.cpp
+++ 3341-find-minimum-time-to-reach-last-room-i/3341-find-minimum-time-to-reach-last-room-i.cpp
@@ -1,33 +1,27 @@
 class Solution {
 public:
     int minTimeToReach(vector<vector<int>>& moveTime) {
-        vector<vector<int>> dir = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
+        vector<vector<int>> dir = { {0,1}, {0,-1}, {-1,0}, {1,0} };
         priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>> pq;
+        pq.push({0,0,0});
         int m = moveTime.size();
         int n = moveTime[0].size();
-        vector<vector<int>> shortestTime(m,vector<int>(n,INT_MAX));
-        pq.push({0, 0, 0});
-        shortestTime[0][0] = 0;
-        while (!pq.empty()) {
+        moveTime[0][0]=-1;
+        while(!pq.empty()) {
+            int time = pq.top()[0];
             int row = pq.top()[1];
             int col = pq.top()[2];
-            int time = pq.top()[0];
             pq.pop();
-            if (row == m - 1 && col == n - 1) {
-                return time;
-            }
-            for (int i = 0; i < dir.size(); i++) {
-                int newRow = row + dir[i][0];
-                int newCol = col + dir[i][1];
-                if (newRow < m && newRow >= 0 && newCol < n && newCol >= 0) {
-                    int timeToArrive = max(moveTime[newRow][newCol], time) + 1;
-                    if(timeToArrive<shortestTime[newRow][newCol]) {
-                        shortestTime[newRow][newCol]= timeToArrive;
-                        pq.push({timeToArrive,newRow, newCol});
-                    }
+            if(row==m-1 && col==n-1) return time;
+            for(auto d:dir) {
+                int newRow = d[0]+row;
+                int newCol = d[1]+col; 
+                if(newRow>=0 && newCol>=0 && newRow<m && newCol<n && moveTime[newRow][newCol]!=-1) {
+                    pq.push({max(time,moveTime[newRow][newCol])+1,newRow,newCol});
+                    moveTime[newRow][newCol]=-1;
                 }
             }
         }
-        return 0;
+        return -1;
     }
 };
