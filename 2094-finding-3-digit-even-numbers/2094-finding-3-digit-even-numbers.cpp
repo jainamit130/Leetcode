322478cb87c5bece3f1ff1b88a02a7396d0bc8c9
Amit Choraria <jainamit130@gmail.com>
2025-05-12 20:00:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.cpp
+++ 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.cpp
@@ -3,35 +3,39 @@ public:
     vector<int> ans;
     vector<int> findEvenNumbers(vector<int>& digits) {
         unordered_map<int,int> mp;
+
+        // O(n)
         for(auto digit:digits) {
             mp[digit]++;
         }
-        for(int i=0;i<=8;i+=2) {
+
+        // O(10)
+        for(int i=1;i<=9;i++) {
             if(mp.find(i)!=mp.end()) {
                 mp[i]--;
                 if(mp[i]==0) mp.erase(i);
-                solve(mp,string(1,char(i+'0')));
+                solve(mp,i);
                 mp[i]++;
             }
         }
-        sort(ans.begin(),ans.end());
         return ans;
     }
 
-    void solve(unordered_map<int,int>& mp,string numStr) {
-        if(numStr.length()==3) {
-            ans.push_back(stoi(numStr)); 
+    void solve(unordered_map<int,int>& mp,int num) {
+        if(num>99) {
+            ans.push_back(num); 
             return;
         }
-        int digit = numStr.length()==2 ? 1 : 0;
+        int digit = 0;
+        int inc = num>9.length()==2 ? 2 : 1; 
         while(digit<10) {
             if(mp.find(digit)!=mp.end()) {
                 mp[digit]--;
                 if(mp[digit]==0) mp.erase(digit);
-                solve(mp,string(1, char(digit + '0'))+numStr);
+                solve(mp,num*10+digit);
                 mp[digit]++;
             }
-            digit++;
+            digit+=inc;
         }
         return;
     }
