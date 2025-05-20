673c285a0b4a6fc299825d842a7cba26a9640a0c
Amit Choraria <jainamit130@gmail.com>
2025-05-20 13:34:52 +0530

    Time: 1259 ms (26.59%), Space: 452.4 MB (18.1%) - LeetHub

--- 3548-equal-sum-grid-partition-ii/3548-equal-sum-grid-partition-ii.cpp
+++ 3548-equal-sum-grid-partition-ii/3548-equal-sum-grid-partition-ii.cpp
@@ -6,15 +6,26 @@ public:
         vector<long long> row(m);
         vector<long long> col(n);
 
-        unordered_map<int,pair<int,int>> rowSec;
-        unordered_map<int,pair<int,int>> rowOtherSec;
+        unordered_map<long long,pair<int,int>> rowSec;
+        unordered_map<long long,pair<int,int>> rowOtherSec;
 
-        unordered_map<int,pair<int,int>> colSec;
-        unordered_map<int,pair<int,int>> colOtherSec;
+        unordered_map<long long,pair<int,int>> colSec;
+        unordered_map<long long,pair<int,int>> colOtherSec;
+
+        // just a single row when i==0
+        unordered_map<long long,pair<int,int>> rowSecJust1;
+        // just a single row when i==m-1
+        unordered_map<long long,pair<int,int>> rowOtherSecJust1;
+
+        // just a single col when j==0
+        unordered_map<long long,pair<int,int>> colSecJust1;
+        // just a single col when j==n-1
+        unordered_map<long long,pair<int,int>> colOtherSecJust1;
 
         long long sum = 0;
         for(int i=0;i<m;i++) {
             for(int j=0;j<n;j++) {
+                if(i==21694) cout<<grid[i][j]<<" ";
                 sum +=grid[i][j];
                 row[i] +=grid[i][j];
                 col[j] +=grid[i][j];
@@ -24,61 +35,76 @@ public:
                 if(colSec.find(grid[i][j])==colSec.end()) colSec[grid[i][j]] = {INT_MAX,INT_MIN};
                 if(colOtherSec.find(grid[i][j])==colOtherSec.end()) colOtherSec[grid[i][j]] = {INT_MAX,INT_MIN};
 
-                if(!(i==0 && j!=0 && j!=n-1)) rowSec[grid[i][j]] = { min(rowSec[grid[i][j]].first,i), max(rowSec[grid[i][j]].second,i) };
-                if(!(i==m-1 && j!=0 && j!=n-1)) rowOtherSec[grid[i][j]] = { min(rowOtherSec[grid[i][j]].first,i), max(rowOtherSec[grid[i][j]].second,i) };
-                if(!(j==0 && i!=0 && i!=m-1)) colSec[grid[i][j]] = { min(colSec[grid[i][j]].first,j), max(colSec[grid[i][j]].second,j) };
-                if(!(j==n-1 && i!=0 && i!=m-1)) colOtherSec[grid[i][j]] = { min(colOtherSec[grid[i][j]].first,j), max(colOtherSec[grid[i][j]].second,j) };
+                rowSec[grid[i][j]] = { min(rowSec[grid[i][j]].first,i), max(rowSec[grid[i][j]].second,i) };
+                rowOtherSec[grid[i][j]] = { min(rowOtherSec[grid[i][j]].first,i), max(rowOtherSec[grid[i][j]].second,i) };
+                colSec[grid[i][j]] = { min(colSec[grid[i][j]].first,j), max(colSec[grid[i][j]].second,j) };
+                colOtherSec[grid[i][j]] = { min(colOtherSec[grid[i][j]].first,j), max(colOtherSec[grid[i][j]].second,j) };
+
+
+                if(i==0 && (j==0 || j==n-1)) rowSecJust1[grid[i][j]] = { min(rowSecJust1[grid[i][j]].first,i), max(rowSecJust1[grid[i][j]].second,i) };
+                if(i==m-1 && (j==0 || j==n-1)) rowOtherSecJust1[grid[i][j]] = { min(rowOtherSecJust1[grid[i][j]].first,i), max(rowOtherSecJust1[grid[i][j]].second,i) };
+                if(j==0 && (i==0 || i==m-1)) colSecJust1[grid[i][j]] = { min(colSecJust1[grid[i][j]].first,j), max(colSecJust1[grid[i][j]].second,j) };
+                if(j==n-1 && (i==0 || i==m-1)) colOtherSecJust1[grid[i][j]] = { min(colOtherSecJust1[grid[i][j]].first,j), max(colOtherSecJust1[grid[i][j]].second,j) };
+
             }
         }
-
+        cout<<endl;
         // horizontal cut
+        long long ans = LLONG_MAX;
         long long rowSum = 0;
         for(int i=0;i<row.size();i++) {
             rowSum += row[i];
             long long otherSec = sum - rowSum;
-            long long diff = abs(rowSum-otherSec);
+            long long diff = llabs(rowSum - otherSec);
             // rowSum is equal to other section sum
             if(otherSec==rowSum) return true;
             // other section sum is higher
             else if(otherSec>rowSum) {
                 if(rowOtherSec.find(diff)!=rowOtherSec.end() && rowOtherSec[diff].second>=i+1) {
+                    // check for single row in bottom section
+                    if(i==m-2 && rowOtherSecJust1.find(diff)==rowOtherSecJust1.end()) continue;  
                     // check for single column case
-                    if(n==1 && rowOtherSec[diff].second!=m-1 && rowOtherSec[diff].second!=i+1) return false;
+                    if(n==1 && rowOtherSec[diff].second!=m-1 && rowOtherSec[diff].second!=i+1) continue;
                     return true;
                 }
             }
             // rowSec is higher 
             else {
                 if(rowSec.find(diff)!=rowSec.end() && rowSec[diff].first<=i) {
+                    // check for single row in upper section
+                    if(i==0 && rowSecJust1.find(diff)==rowSecJust1.end()) continue;  
                     // check for single column case
-                    if(n==1 && rowSec[diff].first!=0 && rowSec[diff].second!=i) return false;
+                    if(n==1 && rowSec[diff].first!=0 && rowSec[diff].second!=i) continue;
                     return true;
                 }
             }
         }
-
 
         // vertical cut
         long long colSum = 0;
         for(int i=0;i<col.size();i++) {
             colSum += col[i];
             long long otherSec = sum - colSum;
-            long long diff = abs(colSum-otherSec);
+            long long diff = llabs(colSum-otherSec);
             // rowSum is equal to other section sum
             if(otherSec==colSum) return true;
             // other section sum is higher
             else if(otherSec>colSum) {
                 if(colOtherSec.find(diff)!=colOtherSec.end() && colOtherSec[diff].second>=i+1) {
+                    // check for single col in right section
+                    if(i==n-2 && colOtherSecJust1.find(diff)==colOtherSecJust1.end()) continue;  
                     // check for single row case
-                    if(m==1 && colOtherSec[diff].second!=n-1 && colOtherSec[diff].second!=i+1) return true;
+                    if(m==1 && colOtherSec[diff].second!=n-1 && colOtherSec[diff].second!=i+1) continue;
                     return true;
                 }
             }
             // rowSec is higher 
             else {
                 if(colSec.find(diff)!=colSec.end() && colSec[diff].first<=i) {
+                    // check for single col in left section
+                    if(i==0 && colSecJust1.find(diff)==colSecJust1.end()) continue;  
                     // check for single row case
-                    if(m==1 && colSec[diff].first!=0 && colSec[diff].second!=i) return true;
+                    if(m==1 && colSec[diff].first!=0 && colSec[diff].second!=i) continue;
                     return true;
                 }
             }
