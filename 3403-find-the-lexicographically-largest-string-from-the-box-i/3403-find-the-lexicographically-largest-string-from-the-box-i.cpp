65fe3b2c4d51f34d03b74c2eb6ed85975b3bc678
Amit Choraria <jainamit130@gmail.com>
2025-06-04 20:41:39 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3403-find-the-lexicographically-largest-string-from-the-box-i/3403-find-the-lexicographically-largest-string-from-the-box-i.cpp
+++ 3403-find-the-lexicographically-largest-string-from-the-box-i/3403-find-the-lexicographically-largest-string-from-the-box-i.cpp
@@ -1,29 +1,40 @@
 class Solution {
 public:
     string answerString(string word, int numFriends) {
-        if(numFriends==1) {
-            return word;
-        }
-
-        string ans = "a";
-        for(int i=0;i<word.length();i++) {
-            if(word[i]>=ans[0]) {
-                string subAns = word.substr(i,min(word.length() - i,word.length() - numFriends + 1));
-                if(subAns>ans) {
-                    ans=subAns;
+        if(numFriends==1) return word;
+        char ch = word[0];
+        int n =word.length();
+        int endIndex = n-numFriends;
+        int length = endIndex+1;
+        int index = 0;
+        for(int i = 1;i<word.length();i++) {
+            if(word[i]>=ch) {
+                int newEndIndex = i+n-numFriends;
+                if(newEndIndex<i && newEndIndex>=n) continue;
+                int newLen = newEndIndex - i + 1;
+                if(word[i]>ch || (word[i]==ch && length<newLen)) {
+                    ch = word[i];
+                    length = newLen;
+                    index = i;
                 }
             }
         }
-        return ans;
+
+        return word.substr(index,length);
     }
 };
 
 
 /*
 
+10
+0   1   2   3   4   5   6   7   8   9   10  11  12  13  14  15  16  17  18  19  20  21  22  23  24
+x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   x   x
 
-word.length() - largestSubString.length() = numFriends - 1
 
-largestSubString.length() = word.length() - numFriends + 1
+index + (n-length) >= numFriends 
+
+length = index + n - numFriends
+
 
 */
