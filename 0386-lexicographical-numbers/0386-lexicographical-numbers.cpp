61456e2877bd4fb776fa670fe4fdf3be5a1f46f7
Amit Choraria <jainamit130@gmail.com>
2025-06-08 21:31:45 +0530

    Time: 36 ms (21.76%), Space: 34.4 MB (5.05%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -1,8 +1,11 @@
 class TrieNode{
     public:
-        map<char,TrieNode*> children;
+        vector<TrieNode*> children;
         bool end;
-        TrieNode(){end=false;}
+        TrieNode(){
+            children.resize(10,NULL);
+            end=false;
+        }
 };
 
 class Trie{
@@ -14,9 +17,9 @@ class Trie{
         void insert(string s){
             TrieNode* current=root;
             for(auto ch:s){
-                if(current->children.find(ch)==current->children.end())
-                    current->children[ch]=new TrieNode();
-                current=current->children[ch];
+                if(!current->children[ch-48])
+                    current->children[ch-48]=new TrieNode();
+                current=current->children[ch-48];
             }
             current->end=true;
         }
@@ -39,9 +42,33 @@ public:
             ans.push_back(current);
 
         for(char ch = '0';ch <='9';ch++){
-            if(root->children.find(ch)!=root->children.end())
-                dfs(root->children[ch],(current*10 )+ (ch - '0'));
+            if(root->children[ch-48])
+                dfs(root->children[ch-48],(current*10 )+ (ch - '0'));
         }
         return;
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> ans;
+//     vector<int> lexicalOrder(int n) {
+//         int digits = min(n,9);
+//         for(int i=1;i<=digits;i++){
+//             solve(i,n);
+//         }
+//         return ans;
+//     }
+
+//     void solve(int num,int n){
+//         if(num>n){
+//             return;
+//         }
+
+//         ans.push_back(num);
+//         for(int i=0;i<=9;i++){
+//             solve(num*10+i,n);
+//         }
+//         return;
+//     }
+// };
