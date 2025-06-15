3d3a61567ef312410b6d496228faec6235d90263
Amit Choraria <jainamit130@gmail.com>
2025-06-15 23:10:59 +0530

    Time: 655 ms (26.81%), Space: 417.8 MB (25%) - LeetHub

--- 3585-find-weighted-median-node-in-tree/3585-find-weighted-median-node-in-tree.cpp
+++ 3585-find-weighted-median-node-in-tree/3585-find-weighted-median-node-in-tree.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    int n,maxPower;
     vector<vector<int>> ancestors;
     vector<long long> prefixWt;
     vector<int> levels;
-    vector<int> findMedian(int _n,vector<vector<int>>& edges, vector<vector<int>>& queries) {
+    int n,maxPower;
+    vector<int> findMedian(int _n, vector<vector<int>>& edges, vector<vector<int>>& queries) {
         n = _n;
         maxPower = log2(n)+1;
         vector<vector<vector<int>>> adj(n);
@@ -12,6 +12,7 @@ public:
             adj[edge[0]].push_back({edge[1],edge[2]});
             adj[edge[1]].push_back({edge[0],edge[2]});
         }
+
         ancestors.resize(n,vector<int>(maxPower,-1));
         prefixWt.resize(n,0);
         levels.resize(n,-1);
@@ -20,43 +21,52 @@ public:
         for(int i=0;i<queries.size();i++) {
             int u = queries[i][0];
             int v = queries[i][1];
-            ans[i]=v;
             int lca = getLca(u,v);
-            long long dist = getDist(u,v);
-            long long target = (dist+1)/2;
-            int nodeCount = levels[u]+levels[v]-2*levels[lca];
-            int start = 0,end = nodeCount;
-            int subAns = v;
+            ans[i]=v;
+            int start = 0;
+            int totalNodeCount = levels[u]+levels[v]-2*levels[lca];
+            int end = totalNodeCount;
+            long long target = (prefixWt[u] + prefixWt[v] - 2*prefixWt[lca]+1)/2;
             while(start<=end) {
                 int mid = (start+end)/2;
-                int guessedNode =  getMidNodeFromU(u,v,mid,lca);
-                if(guessedNode==-1) break;
+                int guessedNode = getMidNodeFromU(u,v,mid,lca,totalNodeCount);
                 if(getDist(u,guessedNode)>=target) {
-                    subAns = guessedNode;
+                    ans[i] = guessedNode;
                     end = mid-1;
-                } else {
-                    start = mid+1;
-                }
+                } else start = mid+1;
             }
-            ans[i]=subAns;
         }
         return ans;
     }
 
-    int getMidNodeFromU(int u,int v,int mid,int lca) {
-        int nodeCountBetweenUAndLCA = levels[u]-levels[lca];
-        if(mid<=nodeCountBetweenUAndLCA) {
+    int getMidNodeFromU(int u,int v,int mid,int lca,int totalNodeCount) {
+        int totalNodesBetweenUAndLCA = levels[u]-levels[lca];
+        if(totalNodesBetweenUAndLCA>=mid) {
             return lift(u,mid);
-        }       
-        int nodeCountBetweenVAndLCA = levels[v]-levels[lca];
-        int rem = nodeCountBetweenVAndLCA - (mid - nodeCountBetweenUAndLCA);
-        if(rem<0) return -1;
-        return lift(v,rem);
+        }
+        int requiredJump = totalNodeCount - mid;
+        return lift(v,requiredJump);
     }
 
-    long long getDist(int u, int v) {
-        int lca = getLca(u, v);
-        return prefixWt[u] + prefixWt[v] - 2 * prefixWt[lca];
+    long long getDist(int u,int v) {
+        int lca = getLca(u,v);
+        return prefixWt[u] + prefixWt[v] - 2*prefixWt[lca];
+    }
+
+    int getLca(int u,int v) {
+        if(levels[u]<levels[v]) swap(u,v);
+        int diff = levels[u] - levels[v];
+
+        u = lift(u,diff);
+        if(u==v) return u;
+
+        for(int i = maxPower-1; i>=0; i--) {
+            if(ancestors[u][i]!=-1 && ancestors[u][i]!=ancestors[v][i]) {
+                u = ancestors[u][i];
+                v = ancestors[v][i];
+            }
+        }
+        return ancestors[u][0];
     }
 
     int lift(int node,int lift) {
@@ -70,23 +80,6 @@ public:
         }
         return node;
     }
-
-    int getLca(int u, int v) {
-        if (levels[u] < levels[v]) swap(u, v);
-        int diff = levels[u] - levels[v];
-
-        u = lift(u, diff);
-        if (u == v) return u;
-
-        for (int i = maxPower - 1; i >= 0; i--) {
-            if (ancestors[u][i] != -1 && ancestors[u][i] != ancestors[v][i]) {
-                u = ancestors[u][i];
-                v = ancestors[v][i];
-            }
-        }
-        return ancestors[u][0];
-    }
-
 
     void bfsWithBinaryLifting(vector<vector<vector<int>>>& adj) {
         queue<int> q;
@@ -105,11 +98,11 @@ public:
                     if(levels[neigh]==-1) {
                         levels[neigh]=level;
                         ancestors[neigh][0]=node;
-                        prefixWt[neigh] = prefixWt[node]+adj[node][i][1];
-                        for(int level=1;level<maxPower;level++) {
-                            int parent = ancestors[neigh][level-1];
+                        prefixWt[neigh]=prefixWt[node]+adj[node][i][1]; 
+                        for(int jump=1;jump<maxPower;jump++) {
+                            int parent = ancestors[neigh][jump-1];
                             if(parent!=-1) {
-                                ancestors[neigh][level] = ancestors[parent][level-1];
+                                ancestors[neigh][jump] = ancestors[parent][jump-1];
                             }
                         }
                         q.push(neigh);
@@ -117,6 +110,13 @@ public:
                 }
             }
         }
-        return;
     }
 };
+
+
+/*
+
+
+
+
+*/
