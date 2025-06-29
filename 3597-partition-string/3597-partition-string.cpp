8533b7d5a6eabda3fbd0a113bd0b4966e93e172f
Amit Choraria <jainamit130@gmail.com>
2025-06-29 20:37:47 +0530

    Time: 233 ms (63.52%), Space: 111.7 MB (85.1%) - LeetHub

---
+++ 3597-partition-string/3597-partition-string.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<string> partitionString(string s) {
+        unordered_set<string> st;
+        vector<string> ans;
+        string seg = "";
+        for(int i=0;i<s.length();i++) {
+            if(st.find(seg)==st.end()) {
+                if(seg!="") {
+                    st.insert(seg);
+                    ans.push_back(seg);
+                } 
+                seg = s[i];
+            } else {
+                seg+=s[i];
+            }
+        }
+        if(st.find(seg)==st.end()) ans.push_back(seg);
+        return ans;
+    }
+};
