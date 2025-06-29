7a48c63f0f488f4ebaf50b4e71cb0f506904f03c
Amit Choraria <jainamit130@gmail.com>
2025-06-29 22:31:41 +0530

    Time: 287 ms (16.03%), Space: 239.4 MB (18.17%) - LeetHub

--- 3598-longest-common-prefix-between-adjacent-strings-after-removals/3598-longest-common-prefix-between-adjacent-strings-after-removals.cpp
+++ 3598-longest-common-prefix-between-adjacent-strings-after-removals/3598-longest-common-prefix-between-adjacent-strings-after-removals.cpp
@@ -6,58 +6,55 @@ public:
             pq.push({getCommonPrefix(words[i],words[i+1]),i});
             if(pq.size()>2) pq.pop();
         }
-
+        int secondMax = 0; int secondIndex = -2;
+        int maxi = 0; int maxIndex = -2;
         vector<int> ans;
-        int secondMax = 0;
-        int b = -2;
-        int maxi = 0;
-        int a = -2;
         if(pq.size()==2) {
             secondMax = pq.top()[0];
-            b = pq.top()[1];
+            secondIndex=pq.top()[1];
             pq.pop();
-        } 
+        }
+
         if(pq.size()==1) {
             maxi = pq.top()[0];
-            a = pq.top()[1];
+            maxIndex=pq.top()[1];
             pq.pop();
         }
         for(int i=0;i<words.size();i++) {
             int subAns = 0;
-            if(i!=a && i!=a+1) {
+            if(i!=maxIndex && i!=maxIndex+1) {
                 subAns = maxi;
-            } else if(i!=b && i!=b+1){
+            } else if(i!=secondIndex && i!=secondIndex+1) {
                 subAns = secondMax;
             }
+
             if(i>0 && i+1<words.size()) {
-                // find prefix between i-1 and i+1
                 subAns = max(subAns,getCommonPrefix(words[i-1],words[i+1]));
-            } 
+            }
+
             ans.push_back(subAns);
         }
         return ans;
     }
 
-    int getCommonPrefix(string word1,string word2) {
+    int getCommonPrefix(string& word1,string& word2) {
         int j = 0;
-        int k = 0;
-        while(j<word1.length() && k<word2.length()) {
+        while(j<word1.length() && j<word2.length()) {
             if(word1[j]==word2[j]) {
-                k++;j++;
-            } else {
-                break;
-            }
+                j++;
+            } else break;
         }
-        return k;
+        return j;
     }
 };
 
-
 /*
 
+Steps:
 
-jump run run jump run
-0    3   0   0
+
+1. precompute the max and second max along with the respective first index of the pair
+2. iterate the array and compute the prev and next word common prefix get the valid contribution
 
 
 
