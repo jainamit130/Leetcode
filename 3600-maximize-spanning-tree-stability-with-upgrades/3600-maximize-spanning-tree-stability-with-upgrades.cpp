9e48912f1148f2f833a78f19b78f55b8fefc36c0
Amit Choraria <jainamit130@gmail.com>
2025-06-29 23:20:22 +0530

    Time: 1269 ms (8.22%), Space: 443.1 MB (7%) - LeetHub

--- 3600-maximize-spanning-tree-stability-with-upgrades/3600-maximize-spanning-tree-stability-with-upgrades.cpp
+++ 3600-maximize-spanning-tree-stability-with-upgrades/3600-maximize-spanning-tree-stability-with-upgrades.cpp
@@ -1,44 +1,42 @@
 class Union {
     public:
-        vector<int> parent;
-        vector<int> rank;
-        int components;
-        Union(int n) {
-            components=n;
-            parent.resize(n);
-            iota(parent.begin(),parent.end(),0);
-            rank.resize(n);
-        }
+    vector<int> parent;
+    vector<int> rank;
+    int components;
+    Union(int n) {
+        components = n;
+        parent.resize(n);
+        rank.resize(n);
+        iota(begin(parent),end(parent),0);
+    } 
 
-        Union(Union* root) {
-            components=root->components;
-            parent = root->parent;
-            rank = root->rank;
-        }
+    Union(Union* root) {
+        components = root->components;
+        parent = root->parent;
+        rank = root->rank;
+    }
 
-        int findParent(int n) {
-            while(parent[n]!=n) n = parent[parent[n]];
-            return n;
-        }
+    int findParent(int n) {
+        while(n!=parent[n]) n=parent[parent[n]];
+        return n;
+    }
 
-        bool merge(int n1,int n2) {
-            int p1 = findParent(n1);
-            int p2 = findParent(n2);
-            if(p1==p2) return false;
-            if(rank[p1]>=rank[p2]) {
-                parent[p2]=p1;
-                rank[p1]++;
-            } else {
-                parent[p1]=p2;
-                rank[p2]++;
-            }
-            components--;
-            return true;
+    bool merge(int n1,int n2) {
+        int p1 = findParent(n1);
+        int p2 = findParent(n2);
+        if(p1==p2) return false;
+        if(rank[p1]>=rank[p2]) {
+            parent[p2]=p1; rank[p1]++;
+        } else {
+            parent[p1]=p2; rank[p2]++;
         }
+        components--;
+        return true;
+    }
 
-        bool allConnected() {
-            return components == 1;
-        }
+    bool isAllConnected() {
+        return components==1;
+    }    
 };
 
 class Solution {
@@ -51,13 +49,13 @@ public:
 
         Union* root = new Union(n);
         int minStart = getMinAfterTakingMustEdges(edges,root);
-        if(minStart == -1) return -1;
-        int ans = -1;
+        if(minStart==-1) return -1;
         int start = 0;
         int end = 2*1e5;
+        int ans = -1;
         while(start<=end) {
-            Union* unionFind = new Union(root);
             int mid = start + (end-start)/2;
+            Union* unionFind = new Union(root);
             if(isValid(edges,mid,k,unionFind)) {
                 ans = mid;
                 start = mid+1;
@@ -74,10 +72,11 @@ public:
             int v = edge[1];
             if(!root->merge(u,v)) continue;
             if(strength>=stability) continue;
-            if(k>0 && (strength*2)>=stability) k--;
+            if(k>0 && strength*2>=stability) k--;
             else return false;
         }
-        return root->allConnected();
+
+        return root->isAllConnected();
     }
 
     int getMinAfterTakingMustEdges(vector<vector<int>>& edges,Union* root) {
@@ -86,8 +85,24 @@ public:
             auto edge = edges[i];
             if(!edge[3]) break;
             if(!root->merge(edge[0],edge[1])) return -1;
-            mini = min(edge[2],mini); 
+            mini = min(edge[2],mini);
         }
         return mini;
     }
 };
+
+
+/*
+
+Union Find => connect (parent and rank) and components
+=> findParent()
+=> merge()
+=> isAllConnected()
+
+
+binary search => start = 0 and end = 2*1e5
+
+must => 0/1
+
+
+*/
