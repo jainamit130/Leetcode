32ad5ce5f06648c4c03e93892216441df50f284c
Amit Choraria <jainamit130@gmail.com>
2025-06-30 13:30:50 +0530

    Time: 11 ms (86.1%), Space: 36.4 MB (65.41%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.cpp
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.cpp
@@ -1,25 +1,43 @@
 class Solution {
 public:
+    // O(nlogn) and O(1)
     int findLHS(vector<int>& nums) {
-        unordered_map<int,int> mp;
-        for(auto n:nums) {
-            mp[n]++;
-        }
+        sort(nums.begin(),nums.end());
+        int i=0,j=0;
         int ans = 0;
-        for(auto [num,count]: mp) {
-            // considering it as maximum
-            int otherNum = num-1;
-            if(mp.find(otherNum)!=mp.end()) {
-                ans = max(ans,mp[otherNum]+count);
+        while(j<nums.size()) {
+            while(i<j && nums[j]-nums[i]>1) {
+                i++;
             }
-            // considering it as minimum
-            otherNum = num+1;
-            if(mp.find(otherNum)!=mp.end()) {
-                ans = max(ans,mp[otherNum]+count);
+            if(nums[j]-nums[i]==1) {
+                ans=max(ans,j-i+1);
             }
+            j++;
         }
         return ans;
     }
+
+    // // O(n) and O(n)
+    // int findLHS(vector<int>& nums) {
+    //     unordered_map<int,int> mp;
+    //     for(auto n:nums) {
+    //         mp[n]++;
+    //     }
+    //     int ans = 0;
+    //     for(auto [num,count]: mp) {
+    //         // considering it as maximum
+    //         int otherNum = num-1;
+    //         if(mp.find(otherNum)!=mp.end()) {
+    //             ans = max(ans,mp[otherNum]+count);
+    //         }
+    //         // considering it as minimum
+    //         otherNum = num+1;
+    //         if(mp.find(otherNum)!=mp.end()) {
+    //             ans = max(ans,mp[otherNum]+count);
+    //         }
+    //     }
+    //     return ans;
+    // }
 };
 
 /*
