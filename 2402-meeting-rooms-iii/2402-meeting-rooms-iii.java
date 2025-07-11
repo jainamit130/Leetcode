280a0702cf7ca131d8b0c305027989090b333112
Amit Choraria <jainamit130@gmail.com>
2025-07-11 21:09:22 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2402-meeting-rooms-iii/2402-meeting-rooms-iii.java
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.java
@@ -3,35 +3,33 @@ class Solution {
         Arrays.sort(meetings,(int[] l, int[] r) -> {
             return l[0]-r[0];
         });
-
-        PriorityQueue<Integer> freeRooms = new PriorityQueue<>();
-
         PriorityQueue<List<Integer>> pq = new PriorityQueue<>((List<Integer> lhs,List<Integer> rhs) -> {
             if(lhs.get(0)==rhs.get(0)) return lhs.get(1)-rhs.get(1);
             return lhs.get(0)-rhs.get(0);
         });
-
         int currTime = 0;
         Map<Integer,Integer> mp = new HashMap();
-        for(int i=0;i<n;i++) freeRooms.add(i);
-        
-        for(int i=0;i<meetings.length;i++) {
-            currTime = meetings[i][0];
-            int duration = meetings[i][1] - meetings[i][0];;
-            // transfer all rooms that are free
-            while(!pq.isEmpty() && pq.peek().get(0)<=currTime) freeRooms.add(pq.poll().get(1));    
-            if(!freeRooms.isEmpty()) {
-                mp.put(freeRooms.peek(),mp.getOrDefault(freeRooms.peek(),0)+1);
-                pq.add(new ArrayList<>(Arrays.asList(currTime + duration,freeRooms.poll())));
-            } else if(!pq.isEmpty()) {
-                int currEndTime = pq.peek().get(0);
-                int room = pq.peek().get(1);
-                pq.poll();
-                mp.put(room,mp.getOrDefault(room,0)+1);
-                pq.add(new ArrayList(Arrays.asList(currEndTime+duration,room)));
-            }
+        for(int i=0;i<n;i++) {
+            pq.add(Arrays.asList(0,i));
         }
 
+        for(int i=0;i<meetings.length;i++) {
+            List<List<Integer>> freeRooms = new ArrayList<>();
+            currTime = Math.max(pq.peek().get(0),meetings[i][0]);
+            while(!pq.isEmpty() && pq.peek().get(0)<=currTime) {
+                freeRooms.add(pq.poll());
+            }
+            for(int j=0;j<freeRooms.size();j++) {
+                freeRooms.get(j).set(0,0);
+            }
+            pq.addAll(freeRooms);
+            int currEndTime = pq.peek().get(0);
+            int room = pq.peek().get(1);
+            mp.put(room,mp.getOrDefault(room,0)+1);
+            pq.poll();
+            int duration = meetings[i][1] - meetings[i][0];
+            pq.add(new ArrayList(Arrays.asList(currTime+duration,room)));
+        }
         int count = 0;
         int ans = n-1;
         for(int i=n-1;i>=0;i--) {
