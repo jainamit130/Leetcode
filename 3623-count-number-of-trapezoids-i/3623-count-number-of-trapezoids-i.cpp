45a0e6e374eeecd12cba758214476f2086a1ba4f
Amit Choraria <jainamit130@gmail.com>
2025-07-21 00:37:10 +0530

    Time: 162 ms (27.52%), Space: 215.2 MB (0%) - LeetHub

--- 3623-count-number-of-trapezoids-i/3623-count-number-of-trapezoids-i.cpp
+++ 3623-count-number-of-trapezoids-i/3623-count-number-of-trapezoids-i.cpp
@@ -6,26 +6,14 @@ public:
             mp[p[1]]++;
         }
         int mod = 1e9+7;
-        long long ans = 0;
-
-        long long prefix = 0;
+        int ans = 0;
+        int prefix = 0;
         for(auto [y,count]:mp) {
             if(count==1) continue;
             int p = count*1LL*(count-1)/2;
-            ans = (ans + prefix*p%mod)%mod;
+            ans = (ans + prefix*1LL*p%mod)%mod;
             prefix = (prefix+p)%mod;
         }
-        return (int)ans;
+        return ans;
     }
 };
-
-
-/*
-
-p1
-p2
-p3
-
-p1 * p2 + p1 * p3 + p2 * p3
-
-*/
