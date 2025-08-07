3bd418e72dae6eba2bb858fad8e27d2d38996cf5
Amit Choraria <jainamit130@gmail.com>
2025-08-08 00:27:33 +0530

    Time: 313 ms (5%), Space: 212 MB (25%) - LeetHub

---
+++ 3363-find-the-maximum-number-of-fruits-collected/3363-find-the-maximum-number-of-fruits-collected.java
@@ -0,0 +1,38 @@
+class Solution {
+    Integer[][][] dp;
+    public Map<Integer,List<List<Integer>>> mp = Map.of(
+        0,List.of(List.of(1,1),List.of(1,-1),List.of(1,0)),
+        1,List.of(List.of(0,1),List.of(-1,1),List.of(1,1))
+    );
+    int n;
+    public int maxCollectedFruits(int[][] fruits) {
+        n = fruits.length;
+        dp = new Integer[n][n][2]; 
+        int ans = solve(0,n-1,0,fruits);
+        dp = new Integer[n][n][2]; 
+        int subAns = solve(n-1,0,1,fruits);
+        ans += subAns; 
+        for(int i=0;i<n;i++) ans+=fruits[i][i];
+        return ans;
+    }
+
+    boolean isValid(int row,int col) {
+        return row>=0 && row<n && col>=0 && col<n;
+    }
+
+    int solve(int row,int col,int child,int[][] fruits) {
+        if(row==n-1 && col==n-1) return 0;
+        if((child==0 && col<=row) || (child==1 && row<=col)) return Integer.MIN_VALUE;
+        if (dp[row][col][child] != null) return dp[row][col][child];
+
+        int ans = 0;
+        List<List<Integer>> dir = mp.get(child);
+        for(int i=0;i<dir.size();i++) {
+            int newRow = row + dir.get(i).get(0);
+            int newCol = col + dir.get(i).get(1);
+            if(!isValid(newRow,newCol)) continue;
+            ans = Math.max(ans , fruits[row][col] + solve(newRow,newCol,child,fruits));
+        }
+        return dp[row][col][child]=ans;
+    }
+}
