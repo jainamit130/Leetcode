ad5ffc1afdc85a2f922a0743b6ffcda537b9de1f
Amit Choraria <jainamit130@gmail.com>
2025-08-10 12:33:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0869-reordered-power-of-2/0869-reordered-power-of-2.java
+++ 0869-reordered-power-of-2/0869-reordered-power-of-2.java
@@ -1,44 +1,47 @@
 class Solution {
     Map<Integer, Integer> mp = new HashMap<>();
-    int len; // To store length of original number
 
     public boolean reorderedPowerOf2(int n) {
-        if (n == 1) return true;
-        
-        len = String.valueOf(n).length();
-        
+        if(n==1) return true;
+        int num = n;
         while (n > 0) {
             mp.put(n % 10, mp.getOrDefault(n % 10, 0) + 1);
             n /= 10;
         }
-
-        return solve(0); 
+        return solve(num);
     }
 
     boolean solve(int num) {
-        if (String.valueOf(num).length() == len && isValid(num)) {
-            return true;
+        if (mp.isEmpty()) {
+            return isValid(num);
         }
-
-        for (int i = (num == 0 ? 1 : 0); i <= 9; i++) {
-            if (mp.getOrDefault(i, 0) > 0) {
-                mp.put(i, mp.get(i) - 1);
-                if (mp.get(i) == 0) mp.remove(i);
-
-                if (solve(num * 10 + i)) return true;
-
-                mp.put(i, mp.getOrDefault(i, 0) + 1);
+        int start = num==0 ? 1 : 0;
+        boolean ans = false;
+        for(int i=start;i<=9;i++) {
+            if((mp.getOrDefault(i,0))>0) {
+                mp.put(i,mp.get(i)-1);
+                if(mp.get(i)==0) mp.remove(i);
+                ans = ans || solve(num*10+i);
+                mp.put(i,mp.getOrDefault(i,0)+1);
             }
         }
-
-        return false;
+        return ans;
     }
 
     boolean isValid(int num) {
-        return (num & (num - 1)) == 0;
+        num--;
+        if (num < 0) return false;
+        boolean flag = true;
+        while (num > 0) {
+            if ((num & 1) == 0) {
+                flag = false;
+                break;
+            }
+            num = num >> 1;
+        }
+        return flag;
     }
 }
-
 
 /*
 
