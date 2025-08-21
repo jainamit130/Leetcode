e1bb81c48c893e1064bee5fba2db6f3566f520c4
Amit Choraria <jainamit130@gmail.com>
2025-08-21 20:36:18 +0530

    Time: 8 ms (43.15%), Space: 45.3 MB (71.58%) - LeetHub

--- 1504-count-submatrices-with-all-ones/1504-count-submatrices-with-all-ones.java
+++ 1504-count-submatrices-with-all-ones/1504-count-submatrices-with-all-ones.java
@@ -1,50 +1,25 @@
 class Solution {
     public int numSubmat(int[][] mat) {
-        int m = mat.length, n = mat[0].length;
         int ans = 0;
-        int[][] width = new int[m][n];
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (mat[i][j] == 1) {
-                    width[i][j] = (j == 0 ? 1 : width[i][j-1] + 1);
-
-                    int minWidth = width[i][j];
-                    for (int k = i; k >= 0 && minWidth > 0; k--) {
-                        minWidth = Math.min(minWidth, width[k][j]);
-                        ans += minWidth;
+        int[][] dp = new int[mat.length][mat[0].length];
+        for(int i=0;i<mat.length;i++) {
+            int cons1 = 0;
+            for(int j=0;j<mat[0].length;j++) {
+                if(mat[i][j]==1) {
+                    cons1++;
+                    ans += cons1;
+                    dp[i][j]=cons1;
+                    int width = cons1;
+                    for(int k=i-1;k>=0;k--) {
+                        if(mat[k][j]==0) break;
+                        width = Math.min(width,dp[k][j]);
+                        ans += width;
                     }
+                } else {
+                    cons1=0;
                 }
             }
         }
         return ans;
     }
 }
-
-
-
-/*
-
-
-0   1   1   0
-0   1   1   1
-1   1   1   0
-
-ans = 0 + 1 + 2 + 3 + 1 + 3 + 5 + 0 + 0 + 3 + 6
-
-horizontal
-0   2   1   0   0
-0   0   2   1   0
-3   2   1   0   0
-0   0   0   0   0
-
-vertical
-0   0   1   0   0
-0   2   2   1   0
-1   1   1   0   0
-0   0   0   0   0
-
-
-
-
-*/
