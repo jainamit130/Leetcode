292721ae1afe50791d582e5641364e15189c1a1f
Amit Choraria <jainamit130@gmail.com>
2025-09-15 15:12:28 +0530

    Time: 4 ms (7.97%), Space: 9 MB (57.81%) - LeetHub

--- 1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.cpp
+++ 1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.cpp
@@ -4,20 +4,11 @@ public:
         unordered_set<char> st;
         for(auto& ch: brokenLetters) st.insert(ch);
         int ans = 0;
-        string word = "";
         int i = 0;
         text+=" ";
         while(i<text.length()) {
-            if(text[i]==' ') {
-                ans++;
-                word = "";
-            }
-            else if(st.find(text[i])!=st.end()) {
-                word = "";
-                while(i<text.length() && text[i]!=' ') i++;
-            } else {
-                word += text[i]; 
-            }
+            if(text[i]==' ') ans++;
+            else if(st.find(text[i])!=st.end()) while(i<text.length() && text[i]!=' ') i++;
             i++;
         }
         return ans;
