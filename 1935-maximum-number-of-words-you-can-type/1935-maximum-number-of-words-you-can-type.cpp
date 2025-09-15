94931e331e821448ab2e239ee9fd017d16774a76
Amit Choraria <jainamit130@gmail.com>
2025-09-15 15:13:30 +0530

    Time: 1 ms (33.89%), Space: 9.3 MB (39.2%) - LeetHub

--- 1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.cpp
+++ 1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public:
     int canBeTypedWords(string text, string brokenLetters) {
-        unordered_set<char> st;
-        for(auto& ch: brokenLetters) st.insert(ch);
-        int ans = 0;
-        int i = 0;
-        text+=" ";
+        unordered_set<char> st(brokenLetters.begin(),brokenLetters.end());
+        int ans = 0, i = 0; text+=" ";
         while(i<text.length()) {
             if(text[i]==' ') ans++;
             else if(st.find(text[i])!=st.end()) while(i<text.length() && text[i]!=' ') i++;
