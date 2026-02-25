7401b08b16761900e12280cf88563f32121d9b19
Amit Choraria <jainamit130@gmail.com>
2026-02-25 15:13:33 +0530

    Time: 0 ms (100%), Space: 14.3 MB (43.16%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     vector<int> sortByBits(vector<int>& arr) {
-        sort(arr.begin(),arr.end(),[](auto& lhs,auto & rhs) {
-            int count1 = __builtin_popcount(lhs);
-            int count2 = __builtin_popcount(rhs);
-            if(count1==count2) {
-                return lhs<rhs;
-            }
-            return count1<count2;
+        sort(arr.begin(),arr.end(),[](auto& lhs,auto& rhs) {
+            int a = __builtin_popcount(lhs);
+            int b = __builtin_popcount(rhs);
+            if(a==b) return lhs<rhs;
+            return a<b;
         });
         return arr;
     }
