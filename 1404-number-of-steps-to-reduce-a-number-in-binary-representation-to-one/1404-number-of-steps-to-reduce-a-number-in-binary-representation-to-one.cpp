cc5a671b5da7379041c45b5bbba49ee18b841f9e
Amit Choraria <jainamit130@gmail.com>
2026-02-26 15:52:34 +0530

    Time: 6 ms (10.88%), Space: 12 MB (6.48%) - LeetHub

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
@@ -1,57 +1,42 @@
 class Solution {
 public:
     int numSteps(string s) {
-        int ans=0;
-        while(s.length()>1){
-            if(s.back()=='0'){
-                ans++;
-                s.pop_back();
+        int steps = 0;
+        while(s.length()>0 && s!="1") {
+            if(s.back()=='1') {
+                string endStr = "";
+                while(s.length() && s.back()=='1') {
+                    endStr += '0';
+                    s.pop_back();
+                }
+                string midStr = "";
+                if(s.length()>0) {
+                    s.pop_back();
+                    midStr = "1"; 
+                }
+                s = s + midStr + endStr;
             } else {
-                int i=s.length()-1;
-                s[i]='0';
-                i--;
-                while(i>=0 && s[i]=='1'){
-                    s[i]='0';
-                    i--;
-                }
-                if(i==-1){
-                    s[0]='1';
-                    ans++;
-                    s.push_back('0');
-                } else {
-                    s[i]='1';
-                    ans++;
-                }
+                s.pop_back();
             }
+            steps++;
         }
-        return ans;
+        return steps;
     }
 };
 
-
 /*
-
 1   1   0   1
+
 1   1   1   0
 1   1   1
 1   0   0   0
 1   0   0
 1   0
-1
-
-
-
-
-0   1   1   1   1   
-
-1   0   0   0   0
-
-1   0   0   0
-
-1   0   0
-
 1   0
 
-1
+
+1   0   0   1   1   
+
+0   0
 
 */
