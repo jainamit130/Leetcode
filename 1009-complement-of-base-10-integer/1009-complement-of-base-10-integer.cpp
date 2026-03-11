08d692f544d2b83999c4140995e2bfd5d4652c0b
Amit Choraria <jainamit130@gmail.com>
2026-03-11 18:28:24 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int bitwiseComplement(int num) {
+        int ans=0;
+        int i=0;
+        if(num==0){
+            return 1;
+        }
+        while(num){
+            int bit = num&1;
+            if(bit==0){
+                ans|=(1<<i);
+            } 
+            i++;
+            num=num>>1;
+        }
+        return ans;
+    }
+};
