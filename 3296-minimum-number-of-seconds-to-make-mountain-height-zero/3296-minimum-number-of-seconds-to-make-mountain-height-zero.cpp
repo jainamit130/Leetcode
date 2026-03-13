1b0f054e413388632f1ae45dbc80592321a70169
Amit Choraria <jainamit130@gmail.com>
2026-03-13 23:18:50 +0530

    Time: 51 ms (50.32%), Space: 28.9 MB (65.81%) - LeetHub

--- 3296-minimum-number-of-seconds-to-make-mountain-height-zero/3296-minimum-number-of-seconds-to-make-mountain-height-zero.cpp
+++ 3296-minimum-number-of-seconds-to-make-mountain-height-zero/3296-minimum-number-of-seconds-to-make-mountain-height-zero.cpp
@@ -2,26 +2,87 @@ class Solution {
 public:
     using ll=long long;
     long long minNumberOfSeconds(int mountainHeight, vector<int>& workerTimes) {
-        priority_queue<vector<ll>,vector<vector<ll>>,greater<vector<ll>>> pq;
-        for(auto n: workerTimes){
-            pq.push({n,n,1});
+        ll start=1;
+        ll end=1e16;
+        ll ans=end;
+        while(start<=end){
+            ll mid = start + (end-start)/2;
+            if(isValid(mid,mountainHeight,workerTimes)){
+                ans=mid;
+                end=mid-1;
+            } else {
+                start=mid+1;
+            }
         }
+        return ans;
+    }
 
-        ll ans=0;
-        while(mountainHeight){
-            ll currRate = pq.top()[0];
-            int original = pq.top()[1];
-            int usage = pq.top()[2];
-            pq.pop();
-            ans=currRate;
-            usage++;
-            currRate += (ll)original * usage;
-            pq.push({currRate,original,usage});
-            mountainHeight--;
+    bool isValid(ll maxTime,int height,vector<int>& rate){
+        ll totalReduction = 0;
+        for(auto n:rate){
+            totalReduction += maxReductionForWorker(n,maxTime,height);
+            if(totalReduction>=height){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    int maxReductionForWorker(int n,ll maxTime,int height) {
+        int start = 0;
+        int end = height;
+        int ans=0;
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            ll maxPossibleTimeForWorker = (ll)n*((ll)mid*(mid+1)/2);
+            if(maxPossibleTimeForWorker<=maxTime){
+                ans=mid;
+                start=mid+1;
+            } else {
+                end=mid-1;
+            }
         }
         return ans;
     }
 };
+
+
+/*
+
+MID => fixed time => maxPossible Time
+
+start = 1
+end = ?
+
+10^6*(10^5 * 10^5)/2 = 10^16
+
+
+*/
+
+// class Solution {
+// public:
+//     using ll=long long;
+//     long long minNumberOfSeconds(int mountainHeight, vector<int>& workerTimes) {
+//         priority_queue<vector<ll>,vector<vector<ll>>,greater<vector<ll>>> pq;
+//         for(auto n: workerTimes){
+//             pq.push({n,n,1});
+//         }
+
+//         ll ans=0;
+//         while(mountainHeight){
+//             ll currRate = pq.top()[0];
+//             int original = pq.top()[1];
+//             int usage = pq.top()[2];
+//             pq.pop();
+//             ans=currRate;
+//             usage++;
+//             currRate += (ll)original * usage;
+//             pq.push({currRate,original,usage});
+//             mountainHeight--;
+//         }
+//         return ans;
+//     }
+// };
 
 
 /*
