2cb0eb971d58499ee3966ed0f7d85f14d9affb18
Amit Choraria <jainamit130@gmail.com>
2026-03-17 16:48:54 +0530

    Time: 31 ms (70.21%), Space: 90.2 MB (46.28%) - LeetHub

--- 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
@@ -7,10 +7,9 @@ public:
         int area = 0;
         for(int i=0;i<m;i++) {
             for(int j=0;j<n;j++) nextRow[j] = matrix[i][j] ? nextRow[j]+1 : 0;
-            sort(nextRow.begin(),nextRow.end(),greater<int>());
-            for(int j=0;j<n;j++) {
-                area = max(area,(j+1)*nextRow[j]);
-            }
+            vector<int> copyRow = nextRow;
+            sort(copyRow.begin(),copyRow.end(),greater<int>());
+            for(int j=0;j<n;j++) area = max(area,(j+1)*copyRow[j]);
         }
 
         return area;
