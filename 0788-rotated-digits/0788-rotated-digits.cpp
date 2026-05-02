8243c6c3bd95a34f06891cc9151c2b1f4b96bb29
Amit Choraria <jainamit130@gmail.com>
2026-05-02 17:54:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0788-rotated-digits/0788-rotated-digits.cpp
+++ 0788-rotated-digits/0788-rotated-digits.cpp
@@ -4,11 +4,17 @@ public:
         int ans = 0;
         unordered_set<int> unchanged = {0,1,8};
         unordered_set<int> changed = {2,5,6,9};
+        unordered_map<int,pair<bool,bool>> cache;
         for(int i=1;i<=n;i++) {
             int num = i;
             int eligible = 0;
             int flag = 1;
             while(num) {
+                if(cache.find(num)!=cache.end()) {
+                    eligible = cache[num].first;
+                    flag = cache[num].second;
+                    break;
+                }
                 int t = num % 10;
                 if(changed.find(t)!=changed.end()) {
                     eligible = 1;
@@ -17,6 +23,7 @@ public:
                     break;
                 }
                 num/=10;
+                cache[num] = {eligible,flag};
             }
             if(eligible) ans += flag;
         }
